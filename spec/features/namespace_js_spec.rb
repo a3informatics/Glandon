34f9f680e98e6d27195082c84891c4b632a1bbb6
Dave Iberson-Hurst <daveih1664@gmail.com>
2016-11-21 15:33:26 +0000

    IsoNamespace Unit Tests
    [1] Add in feature tests

---
+++ spec/features/namespace_js_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe "Namespace JS", :type => :feature do
+  
+  include PauseHelpers
+  include DataHelpers
+
+  before :all do
+    clear_triple_store
+    load_test_file_into_triple_store("IsoNamespace.ttl")
+  end 
+
+  before :all do
+    user = User.create :email => "sysadmin@example.com", :password => "12345678" 
+    user.add_role :sys_admin
+  end
+
+  after :all do
+    user = User.where(:email => "sysadmin@example.com").first
+    user.destroy
+  end
+
+  describe "valid user", :type => :feature, js: true do
+
+    it "deletes namespace" do
+      visit '/users/sign_in'
+      fill_in 'Email', with: 'sysadmin@example.com'
+      fill_in 'Password', with: '12345678'
+      click_button 'Log in'
+      expect(page).to have_content 'Signed in successfully'
+      click_link 'Namespace'
+      expect(page).to have_content 'Namespaces'
+      find(:xpath, "//tr[contains(.,'AAA')]/td/a", :text => 'Delete').click
+      page.accept_alert
+      sleep(1)
+      expect(page).to have_content 'BBB Pharma'
+      expect(page).to have_no_content 'AAA Long'
+      pause
+    end
+
+  end
+
+end
---
+++ spec/features/namespace_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe "Namespace", :type => :feature do
+  
+  include PauseHelpers
+  include DataHelpers
+
+  before :all do
+    clear_triple_store
+    load_test_file_into_triple_store("IsoNamespace.ttl")
+  end 
+
+  before :each do
+    user = FactoryGirl.create(:user)
+    user.add_role :sys_admin
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'example1234'
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully'  
+  end
+
+  describe "View", :type => :feature do
+  
+    it "allows all namespaces to be viewed" do
+      click_link 'Namespaces'
+      expect(page).to have_content 'Namespaces'
+      expect(page).to have_content 'BBB Pharma'
+      expect(page).to have_content 'AAA Long'      
+    end
+
+    it "allows a new namespace to be added" do
+      click_link 'Namespaces'
+      click_link 'New Namespace'
+      expect(page).to have_content 'New Namespace'
+      fill_in 'iso_namespace_shortName', with: 'NEWORG'
+      fill_in 'iso_namespace_name', with: 'New Organisation'
+      click_button 'Submit'
+      expect(page).to have_content 'Namespaces'
+      expect(page).to have_content 'New Organisation'  
+    end
+
+  end
+
+end
