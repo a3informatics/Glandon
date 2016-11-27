8e1286a8bfe346b585ffb6d0947183e5ef486a53
Dave Iberson-Hurst <daveih1664@gmail.com>
2016-11-27 13:47:56 +0000

    JIRA-918: Fix Issues Found
    [1] Markdown test needs amending due to update to application.js message

--- spec/features/markdown_js_spec.rb
+++ spec/features/markdown_js_spec.rb
@@ -31,7 +31,7 @@ describe "Markdown", :type => :feature do
       expect(div.text(:all)).to eq("Hello world I am here")
       fill_in "raw_markdown", with: "@@@@@"
       click_button "Validate"
-      expect(page).to have_content 'Please enter a valid markdown.'
+      expect(page).to have_content 'Please enter valid markdown.'
     end
 
   end
