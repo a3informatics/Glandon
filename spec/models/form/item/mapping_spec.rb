0d1c82bf06c1ef2f89f561a0a733291775b4f980
Dave Iberson-Hurst <daveih1664@gmail.com>
2016-12-14 10:01:17 +0000

    GLAN-120: Mapping feature
    [1] Initial class and unit test

---
+++ spec/models/form/item/mapping_spec.rb
@@ -0,0 +1,92 @@
+require 'rails_helper'
+
+describe Form::Item::Mapping do
+  
+  include DataHelpers
+
+  it "clears triple store and loads test data" do
+    clear_triple_store
+    load_schema_file_into_triple_store("ISO11179Types.ttl")
+    load_schema_file_into_triple_store("ISO11179Basic.ttl")
+    load_schema_file_into_triple_store("ISO11179Identification.ttl")
+    load_schema_file_into_triple_store("ISO11179Registration.ttl")
+    load_schema_file_into_triple_store("ISO11179Data.ttl")
+    load_schema_file_into_triple_store("ISO11179Concepts.ttl")
+    load_schema_file_into_triple_store("BusinessOperational.ttl")
+    load_schema_file_into_triple_store("BusinessForm.ttl")
+    clear_iso_concept_object
+  end
+
+  it "validates a valid object" do
+    result = Form::Item::Mapping.new
+    result.mapping = "EGMONKEY when XXTESTCD=HELLO"
+    expect(result.valid?).to eq(true)
+  end
+
+  it "does not validate an invalid object, text label" do
+    result = Form::Item::Mapping.new
+    result.mapping = "EGMONKEY when --TESTCD=HELLO"
+    expect(result.valid?).to eq(false)
+  end
+
+  it "allows object to be initialized from triples" do
+    result = 
+      {
+        :id => "F-ACME_PLACEHOLDERTEST_G1_I1", 
+        :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1", 
+        :mapping => "XXXXX",
+        :completion => "",
+        :extension_properties => [],
+        :label => "Text Label",
+        :note => "xxxxx",
+        :optional => false,
+        :ordinal => 1,
+        :type => "http://www.assero.co.uk/BusinessForm#Mapping"
+      }
+    triples = {}
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] = []
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] << { subject: "http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1_I1", predicate: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", object: "http://www.assero.co.uk/BusinessForm#Mapping" }
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] << { subject: "http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1_I1", predicate: "http://www.w3.org/2000/01/rdf-schema#label", object: "Text Label" }
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] << { subject: "http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1_I1", predicate: "http://www.assero.co.uk/BusinessForm#isItemOf", object: "<http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1>" }
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] << { subject: "http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1_I1", predicate: "http://www.assero.co.uk/BusinessForm#note", object: "xxxxx" }
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] << { subject: "http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1_I1", predicate: "http://www.assero.co.uk/BusinessForm#optional", object: "false" }
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] << { subject: "http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1_I1", predicate: "http://www.assero.co.uk/BusinessForm#mapping", object: "XXXXX" }
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] << { subject: "http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1_I1", predicate: "http://www.assero.co.uk/BusinessForm#ordinal", object: "1" }
+    triples ["F-ACME_PLACEHOLDERTEST_G1_I1"] << { subject: "http://www.assero.co.uk/MDRForms/ACME/V1#F-ACME_PLACEHOLDERTEST_G1_I1", predicate: "http://www.assero.co.uk/BusinessForm#completion", object: "" }
+    expect(Form::Item::Mapping.new(triples, "F-ACME_PLACEHOLDERTEST_G1_I1").to_json).to eq(result)    
+  end
+
+  it "allows an object to be found"
+
+  it "allows an object to be found from triples"
+
+  it "allows an object to be exported as SPARQL" do
+    sparql = SparqlUpdateV2.new
+    result = 
+      "PREFIX bf: <http://www.assero.co.uk/BusinessForm#>\n" +
+      "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+      "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+      "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+      "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
+      "INSERT DATA \n" +
+      "{ \n" + 
+      "<http://www.example.com/path#parent_I1> rdf:type <http://www.example.com/path#rdf_test_type> . \n" +
+      "<http://www.example.com/path#parent_I1> rdfs:label \"label\"^^xsd:string . \n" +
+      "<http://www.example.com/path#parent_I1> bf:ordinal \"1\"^^xsd:positiveInteger . \n" +
+      "<http://www.example.com/path#parent_I1> bf:note \"\"^^xsd:string . \n" +
+      "<http://www.example.com/path#parent_I1> bf:completion \"\"^^xsd:string . \n" + 
+      "<http://www.example.com/path#parent_I1> bf:optional \"false\"^^xsd:boolean . \n" +
+      "<http://www.example.com/path#parent_I1> bf:mapping \"XXX=YYY\"^^xsd:string . \n" +
+      "}"
+    item = Form::Item::Mapping.new
+    item.rdf_type = "http://www.example.com/path#rdf_test_type"
+    item.label = "label"
+    item.mapping = "XXX=YYY"
+    item.to_sparql_v2(UriV2.new({:id => "parent", :namespace => "http://www.example.com/path"}), sparql)
+    expect(sparql.to_s).to eq(result)
+  end
+  
+  it "allows an object to be exported as XML"
+
+end
+  
