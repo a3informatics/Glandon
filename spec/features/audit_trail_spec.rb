1ee88de3f897f20fd866407b48d9cce8b41ff438
Dave Iberson-Hurst <daveih1664@gmail.com>
2017-01-24 12:03:29 +0000

    GLAN-230: Fix bug from testing
    [1] Fix and improve tests further

--- spec/features/audit_trail_spec.rb
+++ spec/features/audit_trail_spec.rb
@@ -11,29 +11,33 @@ describe "Audit Trail", :type => :feature do
     clear_triple_store
     load_test_file_into_triple_store("iso_namespace_real.ttl")
     ua_create
-    user1 = User.create :email => "user1@example.com", :password => "changeme" 
-    user2 = User.create :email => "user2@example.com", :password => "changeme" 
+    user1 = User.create :email => "audit_trail_user_1@example.com", :password => "changeme" 
+    user2 = User.create :email => "audit_trail_user_2@example.com", :password => "changeme" 
     AuditTrail.delete_all
     @now = Time.now
-    ar = AuditTrail.create(date_time: @now, user: "user1@example.com", owner: "CDISC", identifier: "I1", version: "1", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: @now - 10, user: "user1@example.com", owner: "CDISC", identifier: "I2", version: "1", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 20, user: "user1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 30, user: "user1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 40, user: "user1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 50, user: "user1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 2, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 60, user: "user1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 2, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 70, user: "user1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 2, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 80, user: "user1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 3, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 90, user: "user1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 3, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 100, user: "user1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 3, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 110, user: "user1@example.com", owner: "", identifier: "", version: "", event: 4, description: "Login")
-    ar = AuditTrail.create(date_time: Time.now - 120, user: "user1@example.com", owner: "", identifier: "", version: "", event: 4, description: "Logout")
-    ar = AuditTrail.create(date_time: Time.now - 130, user: "user2@example.com", owner: "", identifier: "", version: "", event: 4, description: "Login")
-    ar = AuditTrail.create(date_time: Time.now - 140, user: "user2@example.com", owner: "", identifier: "", version: "", event: 4, description: "Logout")
+    ar = AuditTrail.create(date_time: @now, user: "audit_trail_user_1@example.com", owner: "CDISC", identifier: "I1", version: "1", event: 1, description: "description")
+    ar = AuditTrail.create(date_time: @now - 10, user: "audit_trail_user_1@example.com", owner: "CDISC", identifier: "I2", version: "1", event: 1, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 20, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 1, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 30, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 1, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 40, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 1, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 50, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 2, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 60, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 2, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 70, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 2, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 80, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 3, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 90, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 3, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 100, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 3, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 110, user: "audit_trail_user_1@example.com", owner: "", identifier: "", version: "", event: 4, description: "Login")
+    ar = AuditTrail.create(date_time: Time.now - 120, user: "audit_trail_user_1@example.com", owner: "", identifier: "", version: "", event: 4, description: "Logout")
+    ar = AuditTrail.create(date_time: Time.now - 130, user: "audit_trail_user_2@example.com", owner: "", identifier: "", version: "", event: 4, description: "Login")
+    ar = AuditTrail.create(date_time: Time.now - 140, user: "audit_trail_user_2@example.com", owner: "", identifier: "", version: "", event: 4, description: "Logout")
   end
 
   after :all do
     ua_destroy
+    user = User.where(:email => "audit_trail_user_1@example.com").first
+    user.destroy
+    user = User.where(:email => "audit_trail_user_2@example.com").first
+    user.destroy
   end
 
   describe "curator allowed access to audit", :type => :feature do
@@ -57,8 +61,8 @@ describe "Audit Trail", :type => :feature do
       expect(page).to have_content 'Signed in successfully'
       click_link 'Audit Trail'
       expect(page).to have_content 'Index: Audit Trail'
-      ui_check_table_row("main", 1, [Timestamp.new(@now).to_datetime, "user1@example.com", "CDISC", "I1", "1", "Create"])
-      ui_check_table_row("main", 2, [Timestamp.new(@now - 10).to_datetime, "user1@example.com", "CDISC", "I2", "1", "Create"])
+      ui_check_table_row("main", 1, [Timestamp.new(@now).to_datetime, "audit_trail_user_1@example.com", "CDISC", "I1", "1", "Create"])
+      ui_check_table_row("main", 2, [Timestamp.new(@now - 10).to_datetime, "audit_trail_user_1@example.com", "CDISC", "I2", "1", "Create"])
     end
 
     it "allows searching - event" do
@@ -82,7 +86,7 @@ describe "Audit Trail", :type => :feature do
       expect(page).to have_content 'Signed in successfully'
       click_link 'Audit Trail'
       expect(page).to have_content 'Index: Audit Trail'
-      select 'user2@example.com', from: "audit_trail_user"
+      select 'audit_trail_user_2@example.com', from: "audit_trail_user"
       click_button 'Submit'
       expect(page.all('table#main tr').count).to eq(3)
     end
--- spec/features/users_js_spec.rb
+++ spec/features/users_js_spec.rb
@@ -3,27 +3,33 @@ require 'rails_helper'
 describe "Users", :type => :feature do
   
   include PauseHelpers
+  include UserAccountHelpers
   
   before :all do
+    ua_create
     user = User.create :email => "delete@example.com", :password => "12345678" 
     user.add_role :curator
   end
 
-  describe "Reader User", :type => :feature do
+  after :all do
+    ua_destroy
+    # No need to destroy "delete@exampe.com as the test does it"
+  end
+
+  describe "System Admin User", :type => :feature do
 
     it "allows a user to be deleted", js: true do
-      audit_count = AuditTrail.count
       visit '/users/sign_in'
       fill_in 'Email', with: 'sys_admin@example.com'
-      fill_in 'Password', with: 'changeme'
+      fill_in 'Password', with: '12345678'
       click_button 'Log in'
-      #pause
       click_link 'users_button'
       expect(page).to have_content 'Index: User'
+      audit_count = AuditTrail.count
       find(:xpath, "//tr[contains(.,'delete@example.com')]/td/a", :text => 'Delete').click
       page.accept_alert
-      click_link 'Audit Trail'
-      expect(AuditTrail.count).to eq(audit_count + 2)
+      expect(page).to have_content 'User delete@example.com was successfully deleted.'
+      expect(AuditTrail.count).to eq(audit_count + 1)
     end
 
   end
