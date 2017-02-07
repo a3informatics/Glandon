0812aa8c291f38666aed25edc28edeb8f2a15c10
Dave Iberson-Hurst <daveih1664@gmail.com>
2017-02-07 10:55:56 +0000

    First view test. Background was used as a simple test

---
+++ spec/views/backgrounds/index_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'backgrounds/index.html.erb' do
+
+  include UiHelpers
+
+  it 'displays product details correctly' do
+    
+    x = Time.now
+    timestamp = Timestamp.new(x).to_datetime
+    jobs = []
+    job = Background.new
+    job.update(description: "Job 1", status: "Reading file.", started: x, percentage: 50, complete: false)
+    jobs << job
+    job = Background.new
+    job.update(description: "Job 2", status: "Finished", started: x, percentage: 100, complete: true, completed: x)
+    jobs << job
+    assign(:jobs, jobs)
+
+    render
+    expect(rendered).to have_content("Index: Background Jobs")
+    #ui_check_breadcrumb("Background", "", "", "")
+    expect(rendered).to have_selector("table#main tbody tr:nth-of-type(1) td:nth-of-type(1)", text: 'Job 1')
+    expect(rendered).to have_selector("table#main tbody tr:nth-of-type(1) td:nth-of-type(2)", text: 'Reading file')
+		expect(rendered).to have_selector("table#main tbody tr:nth-of-type(1) td:nth-of-type(3)", text: timestamp)
+		expect(rendered).to have_selector("table#main tbody tr:nth-of-type(1) td:nth-of-type(4)", text: '50')
+		expect(rendered).to have_selector("table#main tbody tr:nth-of-type(1) td:nth-of-type(5)", text: 'false')
+    expect(rendered).to have_selector("table#main tbody tr:nth-of-type(2) td:nth-of-type(1)", text: 'Job 2')
+    expect(rendered).to have_selector("table#main tbody tr:nth-of-type(2) td:nth-of-type(2)", text: 'Finished')
+		expect(rendered).to have_selector("table#main tbody tr:nth-of-type(2) td:nth-of-type(3)", text: timestamp)
+		expect(rendered).to have_selector("table#main tbody tr:nth-of-type(2) td:nth-of-type(4)", text: '100')
+		expect(rendered).to have_selector("table#main tbody tr:nth-of-type(2) td:nth-of-type(5)", text: 'true')
+    expect(rendered).to have_selector("table#main tbody tr:nth-of-type(2) td:nth-of-type(6)", text: timestamp)
+
+  end
+
+end
