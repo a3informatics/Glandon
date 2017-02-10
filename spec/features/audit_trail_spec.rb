0251cd00b86793e05d2dd6f808de3e5f46f3f510
Dave Iberson-Hurst <daveih1664@gmail.com>
2017-02-10 22:19:30 +0000

    GLAN-237: Audit Trail
    [1] Fix test that failed randomly

--- spec/features/audit_trail_spec.rb
+++ spec/features/audit_trail_spec.rb
@@ -14,23 +14,23 @@ describe "Audit Trail", :type => :feature do
     user1 = User.create :email => "audit_trail_user_1@example.com", :password => "changeme" 
     user2 = User.create :email => "audit_trail_user_2@example.com", :password => "changeme" 
     AuditTrail.delete_all
-    @now1 = Time.now
-    @now2 = Time.now - 10
+    @now1 = Time.now - 70
+    @now2 = Time.now - 80
     ar = AuditTrail.create(date_time: @now1, user: "audit_trail_user_1@example.com", owner: "CDISC", identifier: "I1", version: "1", event: 1, description: "description")
     ar = AuditTrail.create(date_time: @now2, user: "audit_trail_user_1@example.com", owner: "CDISC", identifier: "I2", version: "1", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 20, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 30, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 40, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 1, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 50, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 2, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 60, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 2, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 70, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 2, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 80, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 3, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 90, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 3, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 100, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 3, description: "description")
-    ar = AuditTrail.create(date_time: Time.now - 110, user: "audit_trail_user_1@example.com", owner: "", identifier: "", version: "", event: 4, description: "Login")
-    ar = AuditTrail.create(date_time: Time.now - 120, user: "audit_trail_user_1@example.com", owner: "", identifier: "", version: "", event: 4, description: "Logout")
-    ar = AuditTrail.create(date_time: Time.now - 130, user: "audit_trail_user_2@example.com", owner: "", identifier: "", version: "", event: 4, description: "Login")
-    ar = AuditTrail.create(date_time: Time.now - 140, user: "audit_trail_user_2@example.com", owner: "", identifier: "", version: "", event: 4, description: "Logout")
+    ar = AuditTrail.create(date_time: Time.now - 90, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 1, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 100, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 1, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 110, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 1, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 120, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 2, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 130, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 2, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 140, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 2, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 150, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T1", version: "1", event: 3, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 160, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T2", version: "2", event: 3, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 170, user: "audit_trail_user_1@example.com", owner: "ACME", identifier: "T3", version: "3", event: 3, description: "description")
+    ar = AuditTrail.create(date_time: Time.now - 180, user: "audit_trail_user_1@example.com", owner: "", identifier: "", version: "", event: 4, description: "Login")
+    ar = AuditTrail.create(date_time: Time.now - 190, user: "audit_trail_user_1@example.com", owner: "", identifier: "", version: "", event: 4, description: "Logout")
+    ar = AuditTrail.create(date_time: Time.now - 200, user: "audit_trail_user_2@example.com", owner: "", identifier: "", version: "", event: 4, description: "Login")
+    ar = AuditTrail.create(date_time: Time.now - 210, user: "audit_trail_user_2@example.com", owner: "", identifier: "", version: "", event: 4, description: "Logout")
   end
 
   after :all do
@@ -62,8 +62,8 @@ describe "Audit Trail", :type => :feature do
       expect(page).to have_content 'Signed in successfully'
       click_link 'Audit Trail'
       expect(page).to have_content 'Index: Audit Trail'
-      ui_check_table_row("main", 1, [Timestamp.new(@now1).to_datetime, "audit_trail_user_1@example.com", "CDISC", "I1", "1", "Create"])
-      ui_check_table_row("main", 2, [Timestamp.new(@now2).to_datetime, "audit_trail_user_1@example.com", "CDISC", "I2", "1", "Create"])
+      ui_check_table_row("main", 2, [Timestamp.new(@now1).to_datetime, "audit_trail_user_1@example.com", "CDISC", "I1", "1", "Create"])
+      ui_check_table_row("main", 3, [Timestamp.new(@now2).to_datetime, "audit_trail_user_1@example.com", "CDISC", "I2", "1", "Create"])
     end
 
     it "allows searching - event" do
