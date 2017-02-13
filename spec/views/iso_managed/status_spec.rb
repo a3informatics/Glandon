97ac59332c5bd9076e796eb30751f7e9d197cde3
Dave Iberson-Hurst <daveih1664@gmail.com>
2017-02-13 15:10:29 +0000

    GLAN-249: Bugs & Test Fixes
    [1] Tweak to BC Add display for SDTM User Domains
    [2] Add Version label to status update display
    [3] Add basic view tests

---
+++ spec/views/iso_managed/status_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'iso_managed/status.html.erb', :type => :view do
+
+  it 'displays the version info' do 
+
+    mi = BiomedicalConcept.new
+    mi.scopedIdentifier.identifier = "X"
+    mi.scopedIdentifier.semantic_version = "1.2.3"
+    mi.scopedIdentifier.version = 112
+    mi.scopedIdentifier.versionLabel = "Draft 1"
+    assign(:managed_item, mi)
+
+    rs = mi.registrationState
+    rs.registrationStatus = "Candidate"
+    assign(:registration_state, rs)
+
+    si = IsoScopedIdentifier.new
+    assign(:scoped_identifier, si)
+
+    render
+    expect(rendered).to have_content("Version:1.2.3")
+    expect(rendered).to have_content("Internal Version:112")
+    expect(rendered).to have_content("Version Label:Draft 1")
+
+  end
+
+end
