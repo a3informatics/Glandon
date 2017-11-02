4117c411a192310775bd5b7c71aa5d7c68fb713c
Dave Iberson-Hurst <daveih1664@gmail.com>
2017-11-02 10:56:40 +0000

    GLAN-363: CrossReference
    [1] Model
    [2] Tests
    [3] Updated schema to include ordinal

--- app/models/cross_reference.rb
+++ app/models/cross_reference.rb
@@ -1,10 +1,9 @@
 class CrossReference < IsoConcept
   
-  attr_accessor :children, :comments
+  attr_accessor :children, :comments, :ordinal
   
   C_CLASS_NAME = self.name
   C_SCHEMA_PREFIX = "bcr"
-  C_CLASS_NAME = "CrossReference"
   C_SCHEMA_NS = UriManagement.getNs(C_SCHEMA_PREFIX)
   C_RDF_TYPE = "CrossReference"
  	C_CID_SUFFIX = "XR"
@@ -17,6 +16,7 @@ class CrossReference < IsoConcept
   def initialize(triples=nil, id=nil)
     self.children = []
     self.comments = ""
+    self.ordinal = 1
     if triples.nil?
       super
       self.rdf_type = "#{UriV2.new({:namespace => C_SCHEMA_NS, :id => C_RDF_TYPE})}"
@@ -30,10 +30,23 @@ class CrossReference < IsoConcept
   # @param [Hash] params
   # @return [CrossReference] The object
   def self.from_hash(params)
-    object = super(params)
-    json[:comments] = params[:comments]
-    json[:children].each { |c| object.children << OperationalReferenceV2(c) } if !json[:children].blank?
+    object = self.from_json(params)
+    object.comments = params[:comments]
+    object.ordinal = params[:ordinal]
+    params[:children].each { |c| object.children << OperationalReferenceV2.from_json(c) } if !params[:children].blank?
     return object
+  end
+
+  # To Hash
+  #
+  # @return [Hash] The hash
+  def to_hash()
+    result = to_json
+    result[:comments] = self.comments
+    result[:ordinal] = self.ordinal
+    result[:children] = []
+    self.children.each { |c| result[:children] << c.to_json }
+    return result
   end
 
   # To SPARQL
@@ -46,9 +59,10 @@ class CrossReference < IsoConcept
     self.id = "#{parent_uri.id}#{Uri::C_UID_SECTION_SEPARATOR}#{C_CID_SUFFIX}#{self.ordinal}"
     uri = super(sparql, C_SCHEMA_PREFIX)
     subject = {:uri => uri}
+    sparql.triple(subject, {:prefix => C_SCHEMA_PREFIX, :id => "ordinal"}, {:literal => "#{self.ordinal}", :primitive_type => "positiveInteger"})
     sparql.triple(subject, {:prefix => C_SCHEMA_PREFIX, :id => "comments"}, {:literal => "#{self.comments}", :primitive_type => "string"})
     self.children.each do |child|
-      ref_uri = child.to_sparql_v2(uri, sparql)
+      ref_uri = child.to_sparql_v2(uri, "hasCrossReference", C_CID_SUFFIX, child.ordinal, sparql)
       sparql.triple(subject, {:prefix => C_SCHEMA_PREFIX, :id => "hasCrossReference"}, {:uri => ref_uri})
     end
     return uri
--- spec/models/cross_reference_spec.rb
+++ spec/models/cross_reference_spec.rb
@@ -4,6 +4,10 @@ describe CrossReference do
 	
   include DataHelpers
 
+  def sub_dir
+    return "models/cross_reference"
+  end
+
   before :each do
     clear_triple_store
     load_schema_file_into_triple_store("ISO11179Types.ttl")
@@ -16,10 +20,56 @@ describe CrossReference do
     load_schema_file_into_triple_store("business_cross_reference.ttl")
   end
 
-  it "will initialize an object"
+  it "will initialize an object" do
+  	result = CrossReference.new
+  	expect(result.comments).to eq("")
+  	expect(result.rdf_type).to eq("http://www.assero.co.uk/BusinessCrossReference#CrossReference")  	
+  	expect(result.children).to match_array([])  	
+  end
 
-  it "will creat ean object from a hash"
-  
-  it "will output as sparql"
+	it "will serialize as a hash" do
+  	result = CrossReference.new
+  	result.id = "id"
+  	result.namespace = "namespace"
+  	result.comments = "Comment Text"
+  	result.label = "whatevs"
+  	expected = {id: "id", namespace: "namespace", type: "http://www.assero.co.uk/BusinessCrossReference#CrossReference", label: "whatevs", 
+  		comments: "Comment Text", children: [], ordinal: 1, extension_properties: []}
+  	expect(result.to_hash).to eq(expected)
+  end
+
+  it "will create an object from a hash" do
+  	xref_1 = OperationalReferenceV2.new
+  	xref_2 = OperationalReferenceV2.new
+  	xref_1.subject_ref = UriV2.new(uri: "http://example.com/A1")
+  	xref_1.ordinal = 1
+  	xref_2.subject_ref = UriV2.new(uri: "http://example.com/A2")
+  	xref_2.ordinal = 2
+  	input = { id: "CR1", namespace: "http://www.example.com/XR", type: "http://www.assero.co.uk/BusinessCrossReference#CrossReference", label: "Cross Reference",
+  		comments: "The comments", children: [], ordinal: 1, extension_properties: []}
+  	input[:children] << xref_1.to_json
+  	input[:children] << xref_2.to_json
+  	result = CrossReference.from_hash(input)
+  	expect(result.to_hash).to eq(input)
+  end
+
+  it "will output as sparql" do
+  	xref_1 = OperationalReferenceV2.new
+  	xref_2 = OperationalReferenceV2.new
+  	xref_1.ordinal = 1
+  	xref_1.subject_ref = UriV2.new(uri: "http://example.com/A1")
+  	xref_2.ordinal = 2
+  	xref_2.subject_ref = UriV2.new(uri: "http://example.com/A2")
+  	result = CrossReference.new
+		result.comments = "This is the comment"
+		result.children << xref_1
+		result.children << xref_2
+		parent_uri = UriV2.new(uri: "http://example.com/A#base")
+		sparql = SparqlUpdateV2.new
+		result.to_sparql_v2(parent_uri, sparql)
+	#write_text_file_2(sparql.to_s, sub_dir, "to_sparql_1.txt")
+    expected = read_text_file_2(sub_dir, "to_sparql_1.txt")
+		expect(sparql.to_s).to eq(expected)
+	end
   
 end
