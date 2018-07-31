52c59ad2b59cc27e0f85eb70a51b75a330f15049
Jakub Lemiszewski <jl@a3informatics.com>
2018-07-31 14:56:34 +0200

    git cache cleared

--- public/assets/spinner-4ae9350493b5296f808c5db60936061be562eb39e03b0fb7c7bcb9b0a5378304.js
+++
@@ -1,8 +0,0 @@
-function spRemoveSpinner(field_id) {
-  $(field_id + " > span").removeClass('glyphicon-spin');
-}
-
-function spAddSpinner(field_id) {
-  $(field_id + " > span").addClass('glyphicon-spin');
-}
-;
