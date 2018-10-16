f996772780c2caf58b5b3a9751fb4b7662d832b0
Dave Iberson-Hurst <daveih1664@gmail.com>
2018-10-16 12:55:08 +0100

    GLAN-466 Controller Helpers and Unit Test

---
+++ spec/controllers/concerns/controller_helpers_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe ControllerHelpers do
+	
+	include PublicFileHelpers
+  include ControllerHelpers
+
+  def sub_dir
+    return "controllers/concerns/controller_helpers"
+  end
+
+  before :each do
+    delete_all_public_files
+  end
+
+	it "lists upload files" do
+    copy_file_to_public_files(sub_dir, "new_1.xml", "test")
+    copy_file_to_public_files(sub_dir, "new_2.xml", "test")
+    copy_file_to_public_files(sub_dir, "new_3.txt", "test")
+    file_1 = public_path("test", "new_1.xml").to_s
+    file_2 = public_path("test", "new_2.xml").to_s
+    file_3 = public_path("test", "new_3.txt").to_s
+    expect(upload_files("*.xml")).to match_array([file_1, file_2])
+    expect(upload_files("*.txt")).to match_array([file_3])       
+    expect(upload_files("*.*")).to match_array([file_1, file_2, file_3])       
+  end
+
+end
