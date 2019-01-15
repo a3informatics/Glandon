3a1c4f8f2b69035eb184c6ee6083a8ebc83a60e8
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-01-15 19:39:53 +0000

    GLAN-492
    [1] CDISC term reader and tests

--- app/models/import/cdisc_term.rb
+++ app/models/import/cdisc_term.rb
@@ -2,32 +2,16 @@ class Import::CdiscTerm < Import::Rectangular
 
   C_CLASS_NAME = self.name
   
-  def description
-    "Import of CDISC Terminology"
-  end
-
-  def owner
-    IsoNamespace.findByShortName("CDISC").shortName
-  end
-
-  def import_type
-    :cdisc_term
-  end
-
-  def reader_klass
-    Excel::CdiscTermReader
-  end
-
-  def parent_klass
-    ::CdiscTerm # Make sure using top level class, hence ::
-  end
-
-  def identifier
-    ::CdiscTerm::C_IDENTIFIER
-  end
-
-  def self.next_version
-    ::CdiscTerm.next_version
+  def self.configuration
+    {
+      description: "Import of CDISC Terminology",
+      parent_klass: ::CdiscTerm,
+      reader_klass: Excel::CdiscTermReader,
+      import_type: :cdisc_term,
+      sheet_name: :main,
+      version_label: :date,
+      label: "CDISC Terminology"
+    }
   end
 
 end
