90b83cee324d239050a098e842ac1618c5b5d847
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-02-16 15:18:34 +0000

    GLAN-505
    [1] Fuseki::Resource testing

--- app/models/concerns/fuseki/resource.rb
+++ app/models/concerns/fuseki/resource.rb
@@ -1,33 +1,53 @@
+# Fuseki Resource. Handles the methods to create properties in a class
+#
+# @author Dave Iberson-Hurst
+# @since 2.21.0
 require "active_support/core_ext/class"
 
 module Fuseki
   
   module Resource
 
+    # Configure
+    #
+    # @param opts [Hash] the option hash
+    # @option opts [Symbol] :rdf_type the RDF type for the class
+    # @return [Void] no return
     def configure(opts = {})
-      name = :rdf_type
-      uri = Uri.new(uri: opts[:rdf_type])
-      setup(name, uri)
+      Errors.application_error(self.name, __method__.to_s, "No RDF type specified when configuring class.") if !opts.key?(:rdf_type)
+      create(:rdf_type, Uri.new(uri: opts[:rdf_type]))
     end
 
+    # Object Property
+    #
+    # @param name [Symbol] the property name
+    # @param opts [Hash] the option hash
+    # @option opts [Symbol] :cardinality the cardinality, either :one or :many
+    # @return [Void] no return
     def object_property(name, opts = {})
+      Errors.application_error(self.name, __method__.to_s, "No cardinality specified for object property.") if !opts.key?(:cardinality)
       initial = opts[:cardinality] == :one ? "" : [] 
-      setup(name, initial)
+      create(name, initial)
     end
 
+    # Object Property
+    #
+    # @param name [Symbol] the property name
+    # @param opts [Hash] the option hash. Currently empty
+    # @return [Void] no return
     def data_property(name, opts = {})
-      setup(name, "")
+      create(name, "")
     end
 
-    def setup(name, initial)
+  private
+  
+    # Create the instance variable. Set the info for the instance variable.
+    def create(name, initial)
       self.send(:attr_accessor, "#{name}")
       @properties ||= {}
       @properties[name] = initial
     end
     
-    def properties
-      @properties ||= {}
-    end
   end
 
 end
---
+++ spec/models/concerns/fuseki/resource_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe Fuseki::Base do
+  
+  include DataHelpers
+  include PublicFileHelpers
+
+  def sub_dir
+    return "models/concerns/fuseki/resource"
+  end
+
+  before :each do
+    clear_triple_store
+  end
+
+  after :each do
+    Test.instance_variable_set(:@properties, {})
+  end
+    
+
+  class Test 
+    extend Fuseki::Resource
+  end
+
+  it "error if RDF type not configured" do
+    expect{Test.configure({})}.to raise_error(Errors::ApplicationLogicError, "No RDF type specified when configuring class.")
+  end
+
+  it "RDF type configured" do
+    Test.configure({rdf_type: "http://www.example.com/A#XXX"})
+    expect(Test.instance_variable_get(:@properties)[:rdf_type].to_s).to eq("http://www.example.com/A#XXX")
+  end
+
+  it "error if cardinality not configured" do
+    expect{Test.object_property({})}.to raise_error(Errors::ApplicationLogicError, "No cardinality specified for object property.")
+  end
+
+  it "Object property configured" do
+    Test.object_property(:fred, {cardinality: :one})
+    expect(Test.instance_variable_get(:@properties)).to eq({fred: ""})
+    Test.object_property(:sid, {cardinality: :many})
+    expect(Test.instance_variable_get(:@properties)).to eq({fred: "", sid: []})
+  end
+
+  it "Data property configured" do
+    Test.data_property(:fred1)
+    expect(Test.instance_variable_get(:@properties)).to eq({fred1: ""})
+  end
+
+end
