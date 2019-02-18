9202cf269749a97cf2bcb82cd68da391765de119
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-02-18 08:05:16 +0000

    GLAN-505
    [1] Initial commit

--- app/models/iso_namespace.rb
+++ app/models/iso_namespace.rb
@@ -1,289 +1,70 @@
-require "nokogiri"
-require "uri"
+class IsoNamespace < Fuseki::Base
 
-class IsoNamespace
-
-  include CRUD
-  include ModelUtility
-  include UriManagement
-  include ActiveModel::Naming
-  include ActiveModel::Conversion
-  include ActiveModel::Validations
-
-  attr_accessor :id, :namespace, :name, :shortName
+  configure rdf_type: "http://www.assero.co.uk/ISO11179Identification#Namespace"
+  data_property :short_name
+  data_property :name
+  data_property :authority
   
-  # Constants
-  C_NS_PREFIX = "mdrItems"
-  C_ORG_CID_PREFIX = "O"
-  C_NS_CID_PREFIX = "NS"
-  C_CLASS_NAME = "Namespace"
+  C_CLASS_NAME = self.name
   
-  # Class variables
-  @@baseNs = UriManagement.getNs(C_NS_PREFIX)
-  @@idMap = Hash.new
-  @@nameMap = Hash.new
-  
-  def persisted?
-    id.present?
-  end
-
-  # Initialize
+  # Find namespace by the short name.
   #
-  # @return null
-  def initialize()
-    @@baseNs ||= UriManagement.getNs(C_NS_PREFIX)
-    self.id = ""
-    self.namespace = ""
-    self.name = ""
-    self.shortName = ""
-  end
-
-  # Does the namespace exist?
-  #
-  # @return [boolean] True if the namespace exists, false otherwise.
-  def exists?
-    # Do we have the result stored.
-    if @@nameMap.has_key?(shortName)
-      result = true
-    else
-      result = false
-      # Create the query, submit
-      query = UriManagement.buildNs(@@baseNs, ["isoI", "isoB"]) +
-        "SELECT ?a WHERE \n" +
-        "{\n" +
-        "  ?a isoI:ofOrganization ?b . \n" +
-        "  ?b isoB:shortName \"#{self.shortName}\"^^xsd:string . \n" +
-        "}"
-      response = CRUD.query(query)
-      # Process the response
-      xmlDoc = Nokogiri::XML(response.body)
-      xmlDoc.remove_namespaces!
-      xmlDoc.xpath("//result").each do |node|
-        uriSet = node.xpath("binding[@name='a']/uri")
-        if uriSet.length == 1
-          result = true
-        end
-      end
-      return result
-    end
+  # @param name [String] The short name of the namespace to be found
+  # @return [IsoNamespace] Iso Namespace object
+  def self.find_by_short_name(name)
+    results = IsoNamespace.where({short_name: name})
+    return nil if results.empty?
+    Errors.application_error(C_CLASS_NAME, __method__.to_s, "Multiple short names found.") if results.count > 1
+    return results.first
   end
 
   # Find namespace by the short name.
   #
-  # @todo: Better return for not found (will just be empty at the moment)
-  #
-  # @param shortName [string] The short name of the namespace to be found
+  # @param name [String] The short name of the namespace to be found
   # @return [object] Iso Namespace object
-  def self.findByShortName(shortName)
-    # Do we have a stored result
-    object = self.new
-    if @@nameMap.has_key?(shortName)
-      object = @@nameMap[shortName]
-    else
-      # Create the query, submit.
-      query = UriManagement.buildNs(@@baseNs, ["isoI", "isoB"]) +
-        "SELECT ?a ?c WHERE \n" +
-        "{\n" +
-        "  ?a isoI:ofOrganization ?b . \n" +
-        "  ?b isoB:shortName \"" + shortName + "\"^^xsd:string . \n" +
-        "  ?b isoB:name ?c . \n" +
-        "}"
-      response = CRUD.query(query)
-      # Process the response.
-      xmlDoc = Nokogiri::XML(response.body)
-      xmlDoc.remove_namespaces!
-      xmlDoc.xpath("//result").each do |node|
-        uriSet = node.xpath("binding[@name='a']/uri")
-        nSet = node.xpath("binding[@name='c']/literal")
-        if nSet.length == 1 and uriSet.length == 1
-          object.id = ModelUtility::extractCid(uriSet[0].text)
-          object.namespace = @@baseNs 
-          object.name = nSet[0].text
-          object.shortName = shortName
-          @@nameMap[object.shortName] = object
-          @@idMap[object.id] = object
-        end
-      end
-    end
-    return object
+  def self.exists?(name)
+    !find_by_short_name(name).nil?
+  end
+
+  # Create
+  #
+  # @return [IsoNamespace] the object or errors
+  # @raise [Errors::CreateError] if object not created
+  def create
+    super if self.valid? && !self.class.exists?(self.short_name)
+  end
+
+  # To Hash
+  #
+  # @return [Hash] the object hash 
+  def to_hash
+    {uri: self.uri.to_s, name: self.name, short_name: self.short_name, authority: self.authority}
   end
   
-  # Find based on identifier
-  #
-  # @param id [string] The id of the object
-  # @returns [object] Namespace object
-  def self.find(id)
-    # Do we have a stored result?
-    object = self.new 
-    if @@idMap.has_key?(id)
-      object = @@idMap[id]
-    else
-      # Build query and submit.
-      query = UriManagement.buildNs(@@baseNs, ["isoI", "isoB"]) +
-        "SELECT ?b ?c WHERE \n" +
-        "{\n" +
-        "  :" + id.to_s + " isoI:ofOrganization ?a . \n" +
-        "  ?a isoB:shortName ?b . \n" +
-        "  ?a isoB:name ?c . \n" +
-        "}"
-      response = CRUD.query(query)
-      # Process the reposne.
-      xmlDoc = Nokogiri::XML(response.body)
-      xmlDoc.remove_namespaces!
-      xmlDoc.xpath("//result").each do |node|
-        snSet = node.xpath("binding[@name='b']/literal")
-        nSet = node.xpath("binding[@name='c']/literal")
-        if nSet.length == 1 and snSet.length == 1
-          object.id = id
-          object.namespace = @@baseNs 
-          object.name = nSet[0].text
-          object.shortName = snSet[0].text
-          @@nameMap[object.shortName] = object
-          @@idMap[object.id] = object
-        end
-      end
-    end    
-    return object
-  end
+  alias :to_json :to_hash
 
-  # Find all namespace objects
+  # From hash
   #
-  # @return [hash] Hash of namespace objects
-  def self.all
-    # Build query and submit.
-    results = Array.new
-    query = UriManagement.buildNs(@@baseNs, ["isoI", "isoB"]) +
-      "SELECT ?a ?c ?d WHERE \n" +
-      "{\n" +
-      "  ?a rdf:type isoI:Namespace . \n" +
-      "  ?a isoI:ofOrganization ?b . \n" +
-      "  ?b isoB:shortName ?c . \n" +
-      "  ?b isoB:name ?d . \n" +
-      "}"
-    response = CRUD.query(query)
-    # Process the response
-    xmlDoc = Nokogiri::XML(response.body)
-    xmlDoc.remove_namespaces!
-    xmlDoc.xpath("//result").each do |node|
-      snSet = node.xpath("binding[@name='c']/literal")
-      nSet = node.xpath("binding[@name='d']/literal")
-      uriSet = node.xpath("binding[@name='a']/uri")
-      if uriSet.length == 1 and snSet.length == 1 and nSet.length == 1
-        object = self.new 
-        object.id = ModelUtility.extractCid(uriSet[0].text)
-        object.namespace = @@baseNs 
-        object.name = nSet[0].text
-        object.shortName = snSet[0].text
-        @@nameMap[object.shortName] = object
-        @@idMap[object.id] = object
-        results << object
-      end
-    end
-    return results
-  end
-
-  # Create a namespace object
-  #
-  # @param params [hash] {name, shortName}
-  # @return [object] Namespace object. Errors set if duplicate
-  # @raise [ExceptionClass] CreateError if object not created
-  def self.create(params)
+  # @param params [Hash] the hash of values for the object 
+  # @return [IsoNamespace] the object
+  def self.from_hash(params)
     object = self.new
+    object.uri = Uri.new(uri: params[:uri])
     object.name = params[:name]
-    object.shortName = params[:shortName]
-    if object.valid?
-      if !object.exists?
-        uri = UriV2.new({:namespace => @@baseNs, :prefix => C_NS_CID_PREFIX, :org_name => object.shortName, :identifier => C_NS_CID_PREFIX})  
-        org_uri = UriV2.new({:namespace => @@baseNs, :prefix => C_ORG_CID_PREFIX, :org_name => object.shortName, :identifier => C_NS_CID_PREFIX})  
-        update = UriManagement.buildNs(@@baseNs, ["isoI", "isoB"]) +
-          "INSERT DATA \n" +
-          "{\n" +
-          "  :" + org_uri.id + " rdf:type isoB:Organization . \n" +
-          "  :" + org_uri.id + " isoB:name \"#{object.name}\"^^xsd:string . \n" +
-          "  :" + org_uri.id + " isoB:shortName \"#{object.shortName}\"^^xsd:string . \n" +
-          "  :" + uri.id + " rdf:type isoI:Namespace . \n" +
-          "  :" + uri.id + " isoI:ofOrganization :#{org_uri.id} . \n" +
-          "}"
-        response = CRUD.update(update)
-        if response.success?
-          object.id = uri.id
-          object.namespace = @@baseNs 
-        else
-          ConsoleLogger.info(C_CLASS_NAME,"create", "Failed to create object.")
-          raise Exceptions::CreateError.new(message: "Failed to create " + C_CLASS_NAME + " object.")
-        end
-      else
-        object.errors.add(:base, "The short name entered is already in use.")
-      end
-    end
-    return object
+    object.short_name = params[:short_name]
+    object.authority = params[:authority]
+    object
   end
 
-  # Destroy a namespace object
-  #
-  # @return null
-  # @raise [ExceptionClass] DestroyError if object not destroyed
-  def destroy
-    # Destroy the exisitng maps
-    @@idMap = Hash.new
-    @@nameMap = Hash.new
-    # Create the query and submit.
-    # orgId = ModelUtility.cidSwapPrefix(self.id, C_ORG_CID_PREFIX)
-    org_uri = IsoUtility.uri(self.namespace, self.id)
-    org_uri.update_prefix(C_ORG_CID_PREFIX)
-    update = UriManagement.buildNs(self.namespace, ["isoI", "isoB"]) +
-      "DELETE DATA \n" +
-      "{ \n" +
-      "  :" + org_uri.id + " rdf:type isoB:Organization . \n" +
-      "  :" + org_uri.id + " isoB:name \"" + self.name + "\"^^xsd:string . \n" +
-      "  :" + org_uri.id + " isoB:shortName \"" + self.shortName + "\"^^xsd:string . \n" +
-      "  :" + self.id + " rdf:type isoI:Namespace . \n" +
-      "  :" + self.id + " isoI:ofOrganization :" + org_uri.id + " . \n" +
-      "}"
-    response = CRUD.update(update)
-    # Process the response
-    if !response.success?
-      ConsoleLogger.info(C_CLASS_NAME,"destroy", "Failed to destroy object.")
-      raise Exceptions::DestroyError.new(message: "Failed to destroy " + C_CLASS_NAME + " object.")
-    # Leave the following block in here. Used to test Console Logger & Exception raised were working. Just useful.
-    #else
-    #  ConsoleLogger::log(C_CLASS_NAME,"destroy", "Object destroyed.")
-    #  raise Exceptions::DestroyError.new(message: "Failed to destroy " + C_CLASS_NAME + " object.")
-    end
-  end
-
-  # To JSON
-  #
-  # @return [hash] The object hash 
-  def to_json
-    json = 
-    { 
-      :namespace => self.namespace, 
-      :id => self.id, 
-      :name => self.name,
-      :shortName => self.shortName
-    }
-    return json
-  end
-  
-  # From JSON
-  #
-  # @param json [hash] The hash of values for the object 
-  # @return [object] The object
-  def self.from_json(json)
-    object = self.new
-    object.id = json[:id]
-    object.name = json[:name]
-    object.shortName = json[:shortName]
-    object.namespace = json[:namespace]
-    return object
-  end
+  class << self  
+     alias :from_json :from_hash
+   end  
 
   # Object Valid
   #
-  # @return [boolean] True if valid, false otherwise.
+  # @return [Boolean] true if valid, false otherwise.
   def valid?
-    return FieldValidation.valid_short_name?(:short_name, self.shortName, self) && FieldValidation.valid_long_name?(:name, self.name, self)
+    return FieldValidation.valid_short_name?(:short_name, self.short_name, self) && FieldValidation.valid_long_name?(:name, self.name, self)
   end
 
 end
--- spec/models/iso_namespace_spec.rb
+++ spec/models/iso_namespace_spec.rb
@@ -4,53 +4,50 @@ describe IsoNamespace do
 	
   include DataHelpers
 
-  it "clears triple store and loads test data" do
+  before :each do
     clear_triple_store
+    load_schema_file_into_triple_store("ISO11179IdentificationSimplified.ttl")
     load_test_file_into_triple_store("iso_namespace_fake.ttl")
   end
 
 	it "can be filled from JSON" do
     result = IsoNamespace.new
-    result.id = "NS-XXX"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
+    result.uri = "http://www.assero.co.uk/MDRItems#NS-XXX"
     result.name = "XXX Long"
-    result.shortName = "XXX"
-    expect(IsoNamespace.from_json({id: "NS-XXX", namespace: "http://www.assero.co.uk/MDRItems", name: "XXX Long", shortName: "XXX"}).to_json).to eq(result.to_json)
+    result.short_name = "XXX"
+    result.authority = "www.a3.com"
+    expect(IsoNamespace.from_json({uri: "http://www.assero.co.uk/MDRItems#NS-XXX", name: "XXX Long", short_name: "XXX", authority: "www.a3.com"}).to_json).to eq(result.to_json)
   end
 
 	it "can be returned as JSON" do
     result = IsoNamespace.new
-    result.id = "NS-XXX"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
+    result.uri = "http://www.assero.co.uk/MDRItems#NS-XXX"
     result.name = "XXX Long"
-    result.shortName = "XXX"
-    expect(result.to_json).to eq({id: "NS-XXX", namespace: "http://www.assero.co.uk/MDRItems", name: "XXX Long", shortName: "XXX"})
+    result.short_name = "XXX"
+    result.authority = "www.a3.com"
+    expect(result.to_json).to eq({uri: "http://www.assero.co.uk/MDRItems#NS-XXX", name: "XXX Long", short_name: "XXX", authority: "www.a3.com"})
   end
 
   it "determines namespace exists" do
-    namespace = IsoNamespace.new
-    namespace.shortName = "AAA"
-		expect(namespace.exists?).to eq(true)   
+		expect(IsoNamespace.exists?("AAA")).to eq(true)   
 	end
 
 	it "determines namespace does not exists" do
-    namespace = IsoNamespace.new
-    namespace.shortName = "AAA1"
-    expect(namespace.exists?).to eq(false)   
+    expect(IsoNamespace.exists?("AAA1")).to eq(false)   
   end
 
   it "finds namespace by short name" do
     result = IsoNamespace.new
-    result.id = "NS-AAA"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
+    result.uri = "http://www.assero.co.uk/NS#AAA"
     result.name = "AAA Long"
-    result.shortName = "AAA"
-    expect(IsoNamespace.findByShortName("AAA").to_json).to eq(result.to_json)   
+    result.short_name = "AAA"
+    result.authority = "www.aaa.com"
+    expect(IsoNamespace.find_by_short_name("AAA").to_json).to eq(result.to_json)   
   end
 
 	it "determines namespace exists without query" do
     namespace = IsoNamespace.new
-    namespace.shortName = "AAA"
+    namespace.short_name = "AAA"
     expect(namespace.exists?).to eq(true) 
   end
 
@@ -59,17 +56,14 @@ describe IsoNamespace do
     result.id = "NS-AAA"
     result.namespace = "http://www.assero.co.uk/MDRItems"
     result.name = "AAA Long"
-    result.shortName = "AAA"
+    result.short_name = "AAA"
     expect(IsoNamespace.findByShortName("AAA").to_json).to eq(result.to_json)  
   end
 
 	it "finds namespace" do
-    result = IsoNamespace.new
-    result.id = "NS-BBB"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
-    result.name = "BBB Pharma"
-    result.shortName = "BBB"
-    expect(IsoNamespace.find("NS-BBB").to_json).to eq(result.to_json)   
+    result = IsoNamespace.find(Uri.new(uri: "http://www.assero.co.uk/NS#AAA"))
+    expected = {uri: "http://www.assero.co.uk/NS#AAA", name: "AAA Long", short_name: "AAA", authority: "www.aaa.com"}
+    expect(result.to_hash).to eq(expected)   
   end
 
 	it "finds namespace without query" do
@@ -77,219 +71,77 @@ describe IsoNamespace do
     result.id = "NS-AAA"
     result.namespace = "http://www.assero.co.uk/MDRItems"
     result.name = "AAA Long"
-    result.shortName = "AAA"
+    result.short_name = "AAA"
     expect(IsoNamespace.find("NS-AAA").to_json).to eq(result.to_json)  
   end
 
-	it "all namespace" do
-    results = Array.new
-    result = IsoNamespace.new
-    result.id = "NS-BBB"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
-    result.name = "BBB Pharma"
-    result.shortName = "BBB"
-    results << result
-    result = IsoNamespace.new
-    result.id = "NS-AAA"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
-    result.name = "AAA Long"
-    result.shortName = "AAA"
-    results << result
-    expect(IsoNamespace.all.to_json).to eq(results.to_json)   
+	it "all namespaces" do
+    expected = [
+      {uri: "http://www.assero.co.uk/NS#AAA", name: "AAA Long", short_name: "AAA", authority: "www.aaa.com"},
+      {uri: "http://www.assero.co.uk/NS#BBB", name: "BBB Long", short_name: "BBB", authority: "www.bbb.com"},
+    ]
+    result = IsoNamespace.all
+    expect(result).to eq(expected)   
   end
 
 	it "create a namespace" do
+    expected = {uri: "http://www.assero.co.uk/NS#AAA", name: "AAA Long", short_name: "AAA", authority: "www.aaa.com"},
     result = IsoNamespace.new
-    result.id = "NS-CCC_NS"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
     result.name = "CCC Long"
-    result.shortName = "CCC"
-    ns = IsoNamespace.create({shortName: "CCC", name: "CCC Long"})
-    expect(ns.to_json).to eq(result.to_json)  
+    result.short_name = "CCC"
+    result.authority = "www.ccc.com"
+    ns = result.create
+    expect(ns.to_json).to eq(expected)  
 	end
 
   it "does not create a namespace with an invalid shortname" do
-    predicted_result = IsoNamespace.new
-    predicted_result.name = "CCC Long"
-    predicted_result.shortName = "CCC%$£@"
-    actual_result = IsoNamespace.create({shortName: "CCC%$£@", name: "CCC Long"})
-    expect(actual_result.to_json).to eq(predicted_result.to_json)
-    expect(actual_result.errors.messages[:short_name]).to include("contains invalid characters") 
+    result = IsoNamespace.new
+    result.name = "CCC Long"
+    result.short_name = "CCC%$£@"
+    result.authority = "www.ccc.com"
+    result.create
+    expect(result.errors.messages[:short_name]).to include("contains invalid characters") 
   end
 
   it "does not create a namespace with an invalid name" do
-    predicted_result = IsoNamespace.new
-    predicted_result.name = "CCC%$£@ Long"
-    predicted_result.shortName = "CCC"
-    actual_result = IsoNamespace.create({shortName: "CCC", name: "CCC%$£@ Long"})
-    expect(actual_result.to_json).to eq(predicted_result.to_json)
-    expect(actual_result.errors.messages[:name]).to include("contains invalid characters or is empty") 
+    result = IsoNamespace.new
+    result.name = "CCC%$£@ Long"
+    result.short_name = "CCC"
+    result.authority = "www.ccc.com"
+    result.create
+    expect(result.errors.messages[:name]).to include("contains invalid characters or is empty") 
   end
 
   it "does not create a namespace that already exists" do
-    predicted_result = IsoNamespace.new
-    predicted_result.name = "CCC Long"
-    predicted_result.shortName = "CCC"
-    actual_result = IsoNamespace.create({shortName: "CCC", name: "CCC Long"})
-    expect(actual_result.to_json).to eq(predicted_result.to_json)
-    expect(actual_result.errors.messages[:base]).to include("The short name entered is already in use.")
+    result = IsoNamespace.new
+    result.name = "AAA XXX"
+    result.short_name = "AAA"
+    result.authority = "www.aaa.com"
+    result.create
+    expect(result.errors.messages[:name]).to include("contains invalid characters or is empty") 
   end
     
   it "destroy a namespace" do
-    object = IsoNamespace.new
-    object.id = "NS-CCC"
-    object.namespace = "http://www.assero.co.uk/MDRItems"
-    object.name = "CCC Long"
-    object.shortName = "CCC"
-    object.destroy
+    object = IsoNamespace.find(Uri.new(uri: "http://www.assero.co.uk/NS#AAA"))
+    object.delete
+    expect{IsoNamespace.find(Uri.new(uri: "http://www.assero.co.uk/NS#AAA"))}.to raise_error(Exceptions::NotFoundError, "Failed to find http://www.assero.co.uk/NS#AAA in Class object.")
   end
 
   it "determines if the namespace is valid" do
-    result = IsoNamespace.new
-    result.id = "NS-AAA"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
-    result.name = "AAA Long"
-    result.shortName = "AAA"
+    result = IsoNamespace.find(Uri.new(uri: "http://www.assero.co.uk/NS#AAA"))
     expect(result.valid?).to eq(true)   
   end
 
   it "determines the namespace is invalid with a invalid short name" do
-    result = IsoNamespace.new
-    result.id = "NS-AAA"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
-    result.name = "AAA Long"
-    result.shortName = "AAAaaa123^"
+    result = IsoNamespace.find(Uri.new(uri: "http://www.assero.co.uk/NS#AAA"))
+    result.short_name = "AAAaaa123^"
     expect(result.valid?).to eq(false)   
   end
 
   it "determines the namespace is invalid with a invalid name" do
-    result = IsoNamespace.new
-    result.id = "NS-AAA"
-    result.namespace = "http://www.assero.co.uk/MDRItems"
+    result = IsoNamespace.find(Uri.new(uri: "http://www.assero.co.uk/NS#AAA"))
     result.name = "AAA Long£"
-    result.shortName = "AAAaaa123XX"
     expect(result.valid?).to eq(false)   
-  end
-
-  it "clears triple store" do
-    clear_triple_store
-  end
-
-  it "exists handles empty response" do
-    namespace = IsoNamespace.new
-    namespace.shortName = "AAA"
-    expect(namespace.exists?).to eq(false)   
-  end
-
-  it "findByShortName handles empty response" do
-    result = IsoNamespace.new
-    expect(IsoNamespace.findByShortName("XXX").to_json).to eq(result.to_json)
-  end
-
-  it "find handles empty response" do
-    result = IsoNamespace.new
-    expect(IsoNamespace.find("NS-XXX").to_json).to eq(result.to_json)  
-  end
-
-  it "create a namespace, error response" do
-    sparql_query1 = "query=PREFIX : <http://www.assero.co.uk/MDRItems#>\n" +
-      "PREFIX isoI: <http://www.assero.co.uk/ISO11179Identification#>\n" +
-      "PREFIX isoB: <http://www.assero.co.uk/ISO11179Basic#>\n" +
-      "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-      "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-      "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-      "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
-      "SELECT ?a WHERE \n" +
-      "{\n" +
-      "  ?a isoI:ofOrganization ?b . \n" +
-      "  ?b isoB:shortName \"CCC\"^^xsd:string . \n" +
-      "}"
-    sparql_result1 = '<?xml version="1.0"?>
-      <sparql xmlns="http://www.w3.org/2005/sparql-results#">
-        <head>
-          <variable name="a"/>
-        </head>
-        <results>
-        </results>
-      </sparql>'
-    response1 = Typhoeus::Response.new(code: 200, body: sparql_result1)
-    expect(Rest).to receive(:sendRequest).with('http://localhost:3030/test/query', 
-      :post, 
-      '', 
-      '', 
-      sparql_query1, 
-      {"Accept" => "application/sparql-results+xml", "Content-type"=> "application/x-www-form-urlencoded"}).and_return(response1)
-    sparql_query2 = "update=PREFIX : <http://www.assero.co.uk/MDRItems#>\n" +
-      "PREFIX isoI: <http://www.assero.co.uk/ISO11179Identification#>\n" +
-      "PREFIX isoB: <http://www.assero.co.uk/ISO11179Basic#>\n" +
-      "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-      "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-      "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-      "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
-      "INSERT DATA \n" +
-      "{\n" +
-      "  :O-CCC_NS rdf:type isoB:Organization . \n" +
-      "  :O-CCC_NS isoB:name \"CCC Long\"^^xsd:string . \n" +
-      "  :O-CCC_NS isoB:shortName \"CCC\"^^xsd:string . \n" +
-      "  :NS-CCC_NS rdf:type isoI:Namespace . \n" +
-      "  :NS-CCC_NS isoI:ofOrganization :O-CCC_NS . \n" +
-      "}"
-    sparql_result2 = ""
-    response2 = Typhoeus::Response.new(code: 200, body: sparql_result2)
-    expect(Rest).to receive(:sendRequest).with('http://localhost:3030/test/update', 
-      :post, 
-      '', 
-      '', 
-      sparql_query2, 
-      {"Content-type"=> "application/x-www-form-urlencoded"}).and_return(response2)
-    expect(response2).to receive(:success?).and_return(false)
-    expect {
-      IsoNamespace.create({shortName: "CCC", name: "CCC Long"})
-    }.to raise_error(Exceptions::CreateError)
-  end
-
-  it "all handles empty response" do
-    results = Array.new
-    expect(IsoNamespace.all.to_json).to eq(results.to_json)  
-  end
-
-  it "destroy a namespace, error response" do
-    sparql_query = "update=PREFIX : <http://www.assero.co.uk/MDRItems#>\n" +
-      "PREFIX isoI: <http://www.assero.co.uk/ISO11179Identification#>\n" +
-      "PREFIX isoB: <http://www.assero.co.uk/ISO11179Basic#>\n" +
-      "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-      "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
-      "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
-      "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" +
-      "DELETE DATA \n" +
-      "{ \n" +
-      "  :O-CCC rdf:type isoB:Organization . \n" +
-      "  :O-CCC isoB:name \"CCC Long\"^^xsd:string . \n" +
-      "  :O-CCC isoB:shortName \"CCC\"^^xsd:string . \n" +
-      "  :NS-CCC rdf:type isoI:Namespace . \n" +
-      "  :NS-CCC isoI:ofOrganization :O-CCC . \n" +
-      "}"
-    sparql_result = ""
-    response = Typhoeus::Response.new(code: 200, body: "")
-    expect(Rest).to receive(:sendRequest).with('http://localhost:3030/test/update', 
-      :post, 
-      '', 
-      '', 
-      sparql_query, 
-      {"Content-type"=> "application/x-www-form-urlencoded"}).and_return(response)
-    expect(response).to receive(:success?).and_return(false)
-    object = IsoNamespace.new
-    object.id = "NS-CCC"
-    object.namespace = "http://www.assero.co.uk/MDRItems"
-    object.name = "CCC Long"
-    object.shortName = "CCC"
-    expect {
-      object.destroy
-    }.to raise_error(Exceptions::DestroyError)
-  end
-
-  it "clears triple store" do
-    clear_triple_store
   end
 
 end
