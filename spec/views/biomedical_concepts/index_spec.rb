cf2bf6c8d7ebe09e4e87e38c503780973878aada
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-03-20 13:08:22 +0000

    GLAN-510
    [1] BC index view spec

--- spec/views/biomedical_concepts/index_spec.rb
+++ spec/views/biomedical_concepts/index_spec.rb
@@ -14,12 +14,15 @@ describe 'biomedical_concepts/index.html.erb', :type => :view do
     allow(view).to receive(:policy).and_return double(edit?: true, new?: true)
 
     bcs = []
-    bcs << { :owner => "ABC", :identifier => "ID1", :label => "Number 1", :owner_id => "ABC_ID" }
-    bcs << { :owner => "ABC", :identifier => "ID2", :label => "Number 2", :owner_id => "ABC_ID" } 
-    bcs << { :owner => "XYZ", :identifier => "ID3", :label => "Number 3", :owner_id => "XYZ_ID" } 
+    bcs << { :owner => "ABC", :identifier => "ID1", :label => "Number 1", :scope_id => "ABC_ID" }
+    bcs << { :owner => "ABC", :identifier => "ID2", :label => "Number 2", :scope_id => "ABC_ID" } 
+    bcs << { :owner => "XYZ", :identifier => "ID3", :label => "Number 3", :scope_id => "XYZ_ID" } 
     assign(:bcs, bcs)
 
     render
+
+  #puts response.body
+
     expect(rendered).to have_content("Index: Biomedical Concepts")
     expect(rendered).to have_content("New Biomedical Concept")
     expect(rendered).to have_content("Edit Multiple Biomedical Concepts")
@@ -33,7 +36,7 @@ describe 'biomedical_concepts/index.html.erb', :type => :view do
     expect(rendered).to have_selector("table#main tbody tr:nth-of-type(3) td:nth-of-type(1)", text: 'XYZ')
     expect(rendered).to have_selector("table#main tbody tr:nth-of-type(3) td:nth-of-type(2)", text: 'ID3')
     expect(rendered).to have_selector("table#main tbody tr:nth-of-type(3) td:nth-of-type(3)", text: 'Number 3')
-    
+  
   end
 
   it 'displays product details correctly, edit not enabled, new enabled' do
@@ -68,7 +71,7 @@ describe 'biomedical_concepts/index.html.erb', :type => :view do
     allow(view).to receive(:policy).and_return double(edit?: true, new?: false)
 
     bcs = []
-    bcs << { :owner => "ABC", :identifier => "ID1", :label => "Number 1", :owner_id => "ABC_ID" }
+    bcs << { :owner => "ABC", :identifier => "ID1", :label => "Number 1", :scope_id => "ABC_ID" }
     assign(:bcs, bcs)
 
     render
