f634c2bc1e2fa97177bda00c408d24014f66b7c9
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-03-22 12:44:29 +0000

    GLAN-510
    [1] SdtmIg tests

--- spec/models/sdtm_ig_spec.rb
+++ spec/models/sdtm_ig_spec.rb
@@ -56,10 +56,12 @@ describe SdtmIg do
   end
 
 	it "allows the model to be created from JSON" do 
-		expected = read_yaml_file(sub_dir, "from_json_input.yaml")
-    item = SdtmIg.from_json(expected)
-    expect(item.to_json).to hash_equal(expected)
-	end
+		input = read_yaml_file(sub_dir, "from_json_input.yaml")
+    item = SdtmIg.from_json(input)
+  #Xwrite_yaml_file(item.to_json, sub_dir, "from_json_expected.yaml")
+    expected = read_yaml_file(sub_dir, "from_json_expected.yaml")
+    expect(item.to_json).to eq(expected)
+  end
 
 	it "allows the object to be output as sparql" do
   	sparql = SparqlUpdateV2.new
