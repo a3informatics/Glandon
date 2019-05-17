d5099d6ab3bc005b3de7ce5f187bc5ffc70bb585
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-05-17 16:17:13 +0100

    GLAN-310 & GLAN-435
    [1] Move JS file to better file location
    [2] Update tests
    [3] Fix bug where "All" not being stripped

--- spec/javascripts/shared/thesauri/search_spec.js
+++ spec/javascripts/shared/thesauri/search_spec.js
@@ -1,4 +1,4 @@
-//= require thesauri/search
+//= require shared/thesauri/search
 //= require rspec_helper
 //= require sinon
 //= require application
@@ -51,41 +51,29 @@ describe("Thesauri Search", function() {
     server.restore();
   })
 
-
-
-
   it("Initialises the object", function() {
+    var size = ["10", "10", "10", "10", "10", "20"]
+    var pages = ["5","10","15","25","50","100"]
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
+    expect($('#searchTable').DataTable().data().length == 0).to.eq(true); // Check we dont load anything
+    $('#searchTable thead tr:eq(0) th').each( function (index) {
+      var thead = $('#searchTable thead tr:eq(1) th:eq(' + index + ')');
+      expect(thead[0].innerHTML).to.eq('<input id="searchTable_csearch_' + this.innerHTML.toLowerCase().replace(/ /g,"_") + '" type="text" class="form-control" size="' + size[index] + '" placeholder="Search ..." style="width: 100%;">');
+    });
+    $('#searchTable_length option').each( function (index) {
+      expect(this.innerHTML).to.eq(pages[index]);
+    });
   })
-
-  it("Setup text input to each header cell", function(){
-    var id = $('#thesaurus_id').val();
-    var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
-    var tsp = new ThesauriSearchPanel(id, namespace);
-    if($('#searchTable thead tr:eq(1) th').text() == "Extensible") {
-    } else if($('#searchTable thead tr:eq(1) th').text() == "Definition") {
-      expect($('#searchTable thead tr:eq(1) th').html()).to.eq('<input id="searchTable_csearch_definition" type="text" class="form-control" size="20" placeholder="Search ...">');
-    } else if($('#searchTable thead tr:eq(1) th').text() == "Code List") {
-      expect($('#searchTable thead tr:eq(1) th').html()).to.eq('<input id="searchTable_csearch_cl" type="text" class="form-control" size="10" placeholder="Search ...">');
-    }
-  });
 
   it("Search from input header cell", function() {
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
     data = {"draw":"2","recordsTotal":"10","recordsFiltered":"1","data":[{"identifier":"C49484","notation":"NOT DONE","synonym":"","definition":"Indicates a task, process or examination that has either not been initiated or completed. (NCI)","preferredTerm":"Not Done","topLevel":false,"parentIdentifier":"C66789","children":[],"rdf_type":"http://www.assero.co.uk/ISO25964#ThesaurusConcept","id":"CLI-C66789_C49484","namespace":"http://www.assero.co.uk/MDRThesaurus/CDISC/V49","label":"","properties":[],"links":[],"extension_properties":[],"triples":{}}]}
     $('#searchTable thead tr:eq(1) th:eq(3) input').val("NOT DONE");
     keyUpReturn('#searchTable thead tr:eq(1) th:eq(3) input');
-    // server.respondWith("GET", "/^\/thesauri\/search_results?draw.*$/", [200, {"Content-Type":"application/json"}, JSON.stringify(data) ]);
     server.respond([200, {"Content-Type":"application/json"}, JSON.stringify(data) ]);
     expect($("#searchTable tbody tr:nth-child(1) td:nth-child(1)").text()).to.eq("C66789");
   })
@@ -93,8 +81,6 @@ describe("Thesauri Search", function() {
   it("Overall search", function(){
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
     data = {"draw":"2","recordsTotal":"10","recordsFiltered":"1","data":[{"identifier":"C66789","notation":"ND","synonym":"Not Done","definition":"Indicates a task, process or examination that has either not been initiated or completed. (NCI)","preferredTerm":"CDISC SDTM Not Done Terminology","topLevel":true,"parentIdentifier":"C66789","children":[],"rdf_type":"http://www.assero.co.uk/ISO25964#ThesaurusConcept","id":"CL-C66789","namespace":"http://www.assero.co.uk/MDRThesaurus/CDISC/V49","label":"","properties":[],"links":[],"extension_properties":[],"triples":{}}]}
     $('#searchTable_filter input').val("C66789");
@@ -107,8 +93,6 @@ describe("Thesauri Search", function() {
   it("Click on table row", function(){
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
     data = {"draw":"2","recordsTotal":"10","recordsFiltered":"1","data":[{"identifier":"C49484","notation":"NOT DONE","synonym":"","definition":"Indicates a task, process or examination that has either not been initiated or completed. (NCI)","preferredTerm":"Not Done","topLevel":false,"parentIdentifier":"C66789","children":[],"rdf_type":"http://www.assero.co.uk/ISO25964#ThesaurusConcept","id":"CLI-C66789_C49484","namespace":"http://www.assero.co.uk/MDRThesaurus/CDISC/V49","label":"","properties":[],"links":[],"extension_properties":[],"triples":{}}]}
     $('#searchTable thead tr:eq(1) th:eq(3) input').val("NOT DONE");
@@ -123,8 +107,6 @@ describe("Thesauri Search", function() {
   it("Doubleclick on table row", function(){
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
     data = {"draw":"2","recordsTotal":"10","recordsFiltered":"1","data":[{"identifier":"C49484","notation":"NOT DONE","synonym":"","definition":"Indicates a task, process or examination that has either not been initiated or completed. (NCI)","preferredTerm":"Not Done","topLevel":false,"parentIdentifier":"C66789","children":[],"rdf_type":"http://www.assero.co.uk/ISO25964#ThesaurusConcept","id":"CLI-C66789_C49484","namespace":"http://www.assero.co.uk/MDRThesaurus/CDISC/V49","label":"","properties":[],"links":[],"extension_properties":[],"triples":{}}]}
     $('#searchTable thead tr:eq(1) th:eq(3) input').val("NOT DONE");
@@ -144,21 +126,9 @@ describe("Thesauri Search", function() {
     expect($("#searchTable tbody tr:nth-child(2) td:nth-child(2)").text()).to.eq("C49484");
   })
 
-  it("Empty table on load using deferLoading", function(){
-    var id = $('#thesaurus_id').val();
-    var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
-    var tsp = new ThesauriSearchPanel(id, namespace);
-    expect($('#searchTable').DataTable().data().length == 0).to.eq(true);
-  })
-
-
   it("single I, page length changed", function() {
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
   	pageSettings = [[5, 10, 15, 20, 50, 100, -1], ["5", "10", "15", "20", "50", "100", "All"]]
   	pageLength = -1;
@@ -171,8 +141,6 @@ describe("Thesauri Search", function() {
   it("single II, page length not changed", function() {
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
   	pageSettings = [[5, 10, 15, 20, -1, 50, 100], ["5", "10", "15", "20", "All", "50", "100"]]
   	pageLength = 5;
@@ -185,8 +153,6 @@ describe("Thesauri Search", function() {
   it("multiple I, page length changed", function() {
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
   	pageSettings = [[5, 10, 15, 20, -1, 50, 100, -1], ["5", "10", "15", "20", "All", "50", "100", "All"]]
   	pageLength = -1;
@@ -199,8 +165,6 @@ describe("Thesauri Search", function() {
   it("multiple II, page length not changed", function() {
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
   	pageSettings = [[5, 10, 15, 20, -1, 50, 100, -1], ["5", "10", "15", "20", "All", "50", "100", "All"]]
   	pageLength = 10;
@@ -213,8 +177,6 @@ describe("Thesauri Search", function() {
   it("none present", function() {
     var id = $('#thesaurus_id').val();
     var namespace = $('#thesaurus_namespace').val();
-    expect(id).to.eq("aaa");
-    expect(namespace).to.eq("bbb");
     var tsp = new ThesauriSearchPanel(id, namespace);
   	pageSettings = [[5, 10, 15, 20, 50, 100], ["5", "10", "15", "20", "50", "100"]]
   	pageLength = 100;
