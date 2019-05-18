778d526def24bcb334c1b76f8ea00f277f555488
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-05-18 05:57:26 +0100

    GLAN-310 & GLAN-435
    [1] Test tweak

--- spec/features/sdtm_user_domain_editor_js_spec.rb
+++ spec/features/sdtm_user_domain_editor_js_spec.rb
@@ -162,11 +162,13 @@ describe "SDTM User Domain Editor", :type => :feature do
     it "allows non-standard variable to be moved up and down and not pass a standard variable", js: true do
       load_domain("DM Domain")
       click_button "V+"
+      sleep 0.25
       key_1 = ui_get_key_by_path('["Demographics", "DM000029"]')
       expect(key_1).not_to eq(-1)
       ui_click_node_name("Demographics")
       #wait_for_ajax
       click_button "V+"
+      sleep 0.25
       key_2 = ui_get_key_by_path('["Demographics", "DM000030"]')
       expect(key_2).not_to eq(-1)
       ui_click_node_key(key_2)
@@ -240,7 +242,7 @@ describe "SDTM User Domain Editor", :type => :feature do
     it "allows the edit session to be saved", js: true do
       load_domain("DM Domain")
       click_button "V+"
-      wait_for_ajax
+      sleep 0.25
       key_1 = ui_get_key_by_path('["Demographics", "DM000029"]')
       expect(key_1).not_to eq(-1)
       ui_click_save
