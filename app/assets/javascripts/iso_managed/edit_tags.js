09f105573ee4273f2a7d7b7a84e0e91dd7804bd9
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-06-20 11:42:29 +0200

    GLAN-548
    [1] Further refactoring and removal of bugs

--- app/assets/javascripts/iso_managed/edit_tags.js
+++ app/assets/javascripts/iso_managed/edit_tags.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   
   var csvp = new ConceptSystemViewPanel(conceptSystemId, conceptSystemNamespace, 100, nodeSelected);
-  // Manage Tags
-  var imtlp = new IsoManagedTagListPanel(csvp.display());
-  // Item List
-  var imlop = new IsoManagedListOldPanel(csvp.display());
+  var imtlp = new IsoManagedTagListPanel(isoManagedId, isoManagedNamespace);
+  var imlop = new IsoManagedListOldPanel();
   
   function nodeSelected(data) {
     imtlp.selected(data);
-    imlop.imlRefresh(data.id, data.namespace);
+    imlop.refresh(data.id, data.namespace);
   } 
 
   // Set window resize.
