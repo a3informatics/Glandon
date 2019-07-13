9b6a99a8fca915731698f863679d488646076d70
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-07-13 20:19:23 +0100

    GLAN-581-589
    [1] Remove old files
    [2] remove debug statement

--- app/models/concerns/excel/engine.rb
+++ app/models/concerns/excel/engine.rb
@@ -56,7 +56,6 @@ class Excel::Engine
               self.send(action[:method], params)
             end
           rescue => e
-byebug
             @errors.add(:base, "Process error #{e} when processing action '#{action}' on row #{row} column #{col}.")
           end
         end
