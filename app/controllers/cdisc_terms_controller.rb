182e9d5f87a3d50bea27f906c36829b880956911
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-09-10 11:45:39 +0200

    GLAN-768
    [1] Update CDISC Terminology controller to set correct versions
    [2] Update tests

--- app/controllers/cdisc_terms_controller.rb
+++ app/controllers/cdisc_terms_controller.rb
@@ -30,12 +30,11 @@ class CdiscTermsController < ApplicationController
     @versions = CdiscTerm.version_dates
     @versions_normalized = normalize_versions(@versions)
     @versions_yr_span = [ @versions[0][:date].split('-')[0], @versions[-1][:date].split('-')[0] ]
-
     width = current_user.max_term_display.to_i
-    current_index = @versions.length > width ? width - 1 : 0
+    current_index = @versions.length > width ? (@versions.length - width) : 0
     @current_id = @versions[current_index][:id]
-    @latest_id = @versions.first[:id]
-    render :index #, layout: "full_width"
+    @latest_id = @versions.last[:id]
+    render :index
   end
 
   def history
@@ -79,195 +78,6 @@ class CdiscTermsController < ApplicationController
     render json: {data: results}
   end
 
-  # def import
-  #   authorize CdiscTerm
-  #   @files = Dir.glob(Rails.root.join("public", "upload") + "*.owl")
-  #   @cdiscTerm = CdiscTerm.new
-  #   all = CdiscTerm.all
-  #   @next_version = all.last.next_version
-  # end
-
-  # def import_cross_reference
-  #   authorize CdiscTerm, :import?
-  #   @files = Dir.glob(Rails.root.join("public", "upload") + "*.xlsx")
-  #   @cdisc_term = CdiscTerm.find(params[:id], the_params[:namespace], false)
-  # end
-
-  # def create
-  #   authorize CdiscTerm, :import?
-  #   hash = CdiscTerm.create(the_params)
-  #   @cdiscTerm = hash[:object]
-  #   @job = hash[:job]
-  #   if @cdiscTerm.errors.empty?
-  #     redirect_to backgrounds_path
-  #   else
-  #     flash[:error] = @cdiscTerm.errors.full_messages.to_sentence
-  #     redirect_to import_cdisc_terms_path
-  #   end
-  # end
-
-  # def create_cross_reference
-  #   authorize CdiscTerm, :import?
-  #   cdisc_term = CdiscTerm.find(params[:id], the_params[:namespace], false)
-  #   hash = cdisc_term.create_cross_reference(the_params)
-  #   if hash[:object].errors.empty?
-  #     redirect_to backgrounds_path
-  #   else
-  #     flash[:error] = hash[:object].errors.full_messages.to_sentence
-  #     redirect_to import_cdisc_terms_path
-  #   end
-  # end
-
-  # def search
-  #   authorize CdiscTerm, :view?
-  #   @cdiscTerm = CdiscTerm.find(params[:id], params[:namespace], false)
-  #   # @items = Notepad.where(user_id: current_user).find_each
-  #   @close_path = history_thesauri_index_path(identifier: @cdiscTerm.identifier, scope_id: @cdiscTerm.scope.id)
-  # end
-
-  # def search_results
-  #   authorize CdiscTerm, :view?
-  #   results = Thesaurus.search(params)
-  #   render json: { :draw => params[:draw], :recordsTotal => params[:length], :recordsFiltered => results[:count].to_s,
-  #   	:data => results[:items] }
-  # end
-
-  # def compare_calc
-  #   authorize CdiscTerm, :view?
-  #   old_cdisc_term = CdiscTerm.find(params[:oldId], params[:oldNamespace], false)
-  #   new_cdisc_term = CdiscTerm.find(params[:newId], params[:newNamespace], false)
-  #   # If results already prepared redirect, else calculate.
-  #   version_hash = {:new_version => new_cdisc_term.version.to_s, :old_version => old_cdisc_term.version.to_s}
-  #   if CdiscCtChanges.exists?(CdiscCtChanges::C_TWO_CT, version_hash)
-  #     redirect_to compare_cdisc_terms_path(params.symbolize_keys)
-  #   else
-  #     hash = CdiscTerm.compare(old_cdisc_term, new_cdisc_term)
-  #     @cdiscTerm = hash[:object]
-  #     @job = hash[:job]
-  #     if @cdiscTerm.errors.empty?
-  #       redirect_to backgrounds_path
-  #     else
-  #       flash[:error] = @cdiscTerm.errors.full_messages.to_sentence
-  #       redirect_to history_cdisc_terms_path
-  #     end
-  #   end
-  # end
-
-  # def compare
-  #   authorize CdiscTerm, :view?
-  #   old_cdisc_term = CdiscTerm.find(params[:oldId], params[:oldNamespace], false)
-  #   new_cdisc_term = CdiscTerm.find(params[:newId], params[:newNamespace], false)
-  #   version_hash = {:new_version => new_cdisc_term.version.to_s, :old_version => old_cdisc_term.version.to_s}
-  #   @identifier = old_cdisc_term.identifier
-  #   @trimmed_results = CdiscCtChanges.read(CdiscCtChanges::C_TWO_CT, version_hash)
-  #   @cls = CdiscTerm::Utility.transpose_results(@trimmed_results)
-  #   render "changes"
-  # end
-
-  # def changes_calc
-  #   authorize CdiscTerm, :view?
-  #   if CdiscCtChanges.exists?(CdiscCtChanges::C_ALL_CT)
-  #       redirect_to changes_cdisc_terms_path
-  #   else
-  #     hash = CdiscTerm.changes()
-  #     @cdiscTerm = hash[:object]
-  #     @job = hash[:job]
-  #     if @cdiscTerm.errors.empty?
-  #       redirect_to backgrounds_path
-  #     else
-  #       flash[:error] = @cdiscTerm.errors.full_messages.to_sentence
-  #       redirect_to history_cdisc_terms_path
-  #     end
-  #   end
-  # end
-
-  # def changes
-  #   authorize CdiscTerm, :view?
-  #   @version_count = current_user.max_term_display.to_i
-  #   @ct = CdiscTerm.find(params[:id], false)
-  #   link_objects = @ct.forward_backward(1, current_user.max_term_display.to_i)
-  #   @links = {}
-  #   link_objects.each {|k,v| @links[k] = v.nil? ? "" : changes_thesauri_managed_concept_path(v)}
-  # end
-
-  # def changes_results
-  #   authorize CdiscTerm, :view?
-  #   ct = CdiscTerm.find(params[:id], false)
-  #   cls = ct.changes(current_user.max_term_display.to_i)
-  #   cls[:items].each do |k,v|
-  #     v[:changes_path] = changes_thesauri_managed_concept_path(v[:id])
-  #   end
-  #   render json: {data: cls}
-  # end
-
-  # def changes_report
-  #   authorize CdiscTerm, :view?
-  #   ct = CdiscTerm.find(params[:id], false)
-  #   cls = ct.changes(current_user.max_term_display.to_i)
-  #   respond_to do |format|
-  #     format.pdf do
-  #       @html = Reports::CdiscChangesReport.new.create(cls, current_user)
-  #       render pdf: "cdisc_changes.pdf", page_size: current_user.paper_size, orientation: 'Landscape', lowquality: true
-  #     end
-  #   end
-  # end
-
-  # def submission
-  #   authorize CdiscTerm, :view?
-  #   @version_count = current_user.max_term_display.to_i
-  #   @ct = CdiscTerm.find(params[:id], false)
-  #   link_objects = @ct.forward_backward(1, current_user.max_term_display.to_i)
-  #   @links = {}
-  #   link_objects.each {|k,v| @links[k] = v.nil? ? "" : submission_cdisc_term_path(v)}
-  # end
-
-  # def submission_results
-  #   authorize CdiscTerm, :view?
-  #   ct = CdiscTerm.find(params[:id], false)
-  #   cls = ct.submission(current_user.max_term_display.to_i)
-  #   render json: {data: cls}
-  # end
-
-  # def submission_report
-  #   authorize CdiscTerm, :view?
-  #   ct = CdiscTerm.find(params[:id], false)
-  #   cls = ct.submission(current_user.max_term_display.to_i)
-  #   respond_to do |format|
-  #     format.pdf do
-  #       @html = Reports::CdiscSubmissionReport.new.create(cls, current_user)
-  #       @render_args = {pdf: 'cdisc_submission', page_size: current_user.paper_size, orientation: 'Landscape', lowquality: true}
-  #       render @render_args
-  #     end
-  #   end
-  # end
-
-  # def file
-  #   authorize CdiscTerm, :import?
-  #   @files = Dir.glob(CdiscCtChanges.dir_path + "*")
-  # end
-
-  # def file_delete
-  #   authorize CdiscTerm, :import?
-  #   files = the_params[:files]
-  #   files.each do |file|
-  #     File.delete(file) if File.exist?(file)
-  #   end
-  #   redirect_to file_cdisc_terms_path
-  # end
-
-  # def cross_reference
-  # 	authorize CdiscTerm, :show?
-  # 	@direction = the_params[:direction]
-  # 	@cdisc_term = CdiscTerm.find(params[:id], the_params[:namespace], false)
-  # end
-
-  # def export_csv
-  #   authorize CdiscTerm, :show?
-  #   uri = UriV3.new(id: params[:id]) # Using new mechanism
-  #   ct = CdiscTerm.find(uri.fragment, uri.namespace)
-  #   send_data ct.to_csv, filename: "CDISC_Term_#{ct.version_label}.csv", :type => 'text/csv; charset=utf-8; header=present', disposition: "attachment"
-  # end
-
 private
 
   def the_params
@@ -299,10 +109,5 @@ private
   def strdate_to_f(d)
     return Date.parse(d.to_s).strftime('%Q').to_f
   end
-
-  # def get_version
-  # 	return nil if params[:cdisc_term].blank?
-  # 	return the_params[:version].to_i
-  # end
 
 end
--- spec/controllers/cdisc_terms_controller_spec.rb
+++ spec/controllers/cdisc_terms_controller_spec.rb
@@ -54,12 +54,13 @@ describe CdiscTermsController do
 
     it "index" do
       return_values = []
-      (1..20).each_with_index {|x, index| return_values << {id: Uri.new(uri: "http://www.example.com/a##{index+1}").to_id, date: "2019-01-01"}}
+      (1..20).each_with_index {|x, index| return_values << {id: Uri.new(uri: "http://www.example.com/a##{index+1}").to_id, date: "#{1998+index}-01-01"}}
       expect(CdiscTerm).to receive(:version_dates).and_return(return_values)
       get :index
       expect(assigns(:current_id)).to eq(Uri.new(uri: "http://www.example.com/a#17").to_id) # 4th to end
       expect(assigns(:latest_id)).to eq(Uri.new(uri: "http://www.example.com/a#20").to_id) # Latest
-      expect(assigns(:versions)).to eq(return_values.reverse)
+      expect(assigns(:versions)).to eq(return_values)
+      expect(assigns(:versions_yr_span)).to eq(["1998", "2017"])
       expect(response).to render_template("index")
     end
 
@@ -85,135 +86,6 @@ describe CdiscTermsController do
       check_file_actual_expected(actual, sub_dir, "history_expected_1.yaml", equate_method: :hash_equal)
     end
 
-=begin
-    it "show the terminology" do
-      params = { :id => "TH-CDISC_CDISCTerminology", :namespace => "http://www.assero.co.uk/MDRThesaurus/CDISC/V39" }
-      get :show, params
-      expect(response).to render_template("show")
-    end
-
-    it "initiates a search" do
-      params = { :id => "TH-CDISC_CDISCTerminology", :namespace => "http://www.assero.co.uk/MDRThesaurus/CDISC/V39" }
-      get :search, params
-      expect(response).to render_template("search")
-    end
-
-    it "obtains the search results" do
-      request.env['HTTP_ACCEPT'] = "application/json"
-      params = standard_params
-      params[:columns]["5"][:search][:value] = "cerebral"
-      params[:search][:value] = "Temporal"
-      results = CdiscTerm.search(params)
-      get :search_results, params
-      expect(response.content_type).to eq("application/json")
-      expect(response.code).to eq("200")
-    end
-
-    it "calculates the compare results, no file" do
-      file = CdiscCtChanges.dir_path + "CDISC_CT_40_39_Changes.yaml"
-      File.delete(file) if File.exist?(file)
-      params = { 
-        :oldId => "TH-CDISC_CDISCTerminology", 
-        :oldNamespace => "http://www.assero.co.uk/MDRThesaurus/CDISC/V39", 
-        :newId => "TH-CDISC_CDISCTerminology", 
-        :newNamespace => "http://www.assero.co.uk/MDRThesaurus/CDISC/V40" }
-      get :compare_calc, params
-      expect(response).to redirect_to("/backgrounds")
-    end
-    
-    it "calculates the compare results, file" do
-      params = { 
-        :oldId => "TH-CDISC_CDISCTerminology", 
-        :oldNamespace => "http://www.assero.co.uk/MDRThesaurus/CDISC/V39", 
-        :newId => "TH-CDISC_CDISCTerminology", 
-        :newNamespace => "http://www.assero.co.uk/MDRThesaurus/CDISC/V40" }
-      get :compare_calc, params
-      expect(response).to redirect_to("http://test.host/cdisc_terms/compare?newId=TH-CDISC_CDISCTerminology&newNamespace=http%3A%2F%2Fwww.assero.co.uk%2FMDRThesaurus%2FCDISC%2FV40&oldId=TH-CDISC_CDISCTerminology&oldNamespace=http%3A%2F%2Fwww.assero.co.uk%2FMDRThesaurus%2FCDISC%2FV39")
-    end
-    
-    it "obtains the compare results" do
-      params = { 
-        :oldId => "TH-CDISC_CDISCTerminology", 
-        :oldNamespace => "http://www.assero.co.uk/MDRThesaurus/CDISC/V39", 
-        :newId => "TH-CDISC_CDISCTerminology", 
-        :newNamespace => "http://www.assero.co.uk/MDRThesaurus/CDISC/V40" }
-      get :compare, params
-      expect(assigns(:identifier)).not_to eq(nil)
-      expect(assigns(:trimmed_results)).not_to eq(nil)
-      expect(assigns(:cls)).not_to eq(nil)
-      expect(response).to render_template("changes")
-    end
-
-    it "calculates the changes results, no file" do
-      file = CdiscCtChanges.dir_path + "CDISC_CT_Changes.yaml"
-      File.delete(file) if File.exist?(file)
-      get :changes_calc
-      expect(response).to redirect_to("/backgrounds")
-    end
-
-    it "calculates the changes results, file" do
-      get :changes_calc
-      expect(response).to redirect_to("/cdisc_terms/changes")
-    end
-=end
-    # it "changes" do
-    #   @user.write_setting("max_term_display", 2)
-    #   expect(CdiscTerm).to receive(:find).and_return(CdiscTerm.new)
-    #   expect_any_instance_of(CdiscTerm).to receive(:forward_backward).and_return({start: nil, end: "aaa1"})
-    #   get :changes, id: "aaa"
-    #   expect(assigns(:links)).to eq({start: "", end: "/cdisc_terms/aaa1/changes"})
-    #   expect(response).to render_template("changes")
-    # end
-
-    # it "obtains the change results" do
-    #   @user.write_setting("max_term_display", 2)
-    #   expect(CdiscTerm).to receive(:find).and_return(CdiscTerm.new)
-    #   expect_any_instance_of(CdiscTerm).to receive(:changes).with(2).and_return({versions: ["2019-01-01"], items: {}})
-    #   request.env['HTTP_ACCEPT'] = "application/json"
-    #   get :changes_results, id: "aaa"
-    #   expect(response.content_type).to eq("application/json")
-    #   expect(response.code).to eq("200") 
-    #   expect(JSON.parse(response.body).deep_symbolize_keys[:data]).to eq({items: {}, versions: ["2019-01-01"]})
-    # end
-
-    # it "changes_report" do
-    #   @user.write_setting("max_term_display", 2)
-    #   request.env['HTTP_ACCEPT'] = "application/pdf"
-    #   expect(CdiscTerm).to receive(:find).and_return(CdiscTerm.new)
-    #   expect_any_instance_of(CdiscTerm).to receive(:changes).with(2).and_return({versions: ["2019-01-01"], items: {}})
-    #   get :changes_report, id: "aaa"
-    #   expect(response.content_type).to eq("application/pdf")
-    # end
-
-    # it "submission" do
-    #   @user.write_setting("max_term_display", 2)
-    #   expect(CdiscTerm).to receive(:find).and_return(CdiscTerm.new)
-    #   expect_any_instance_of(CdiscTerm).to receive(:forward_backward).and_return({start: nil, end: "aaa1"})
-    #   get :submission, id: "aaa"
-    #   expect(assigns(:links)).to eq({start: "", end: "/cdisc_terms/aaa1/submission"})
-    #   expect(response).to render_template("submission")
-    # end
-
-    # it "obtains the submission results" do
-    #   @user.write_setting("max_term_display", 2)
-    #   expect(CdiscTerm).to receive(:find).and_return(CdiscTerm.new)
-    #   expect_any_instance_of(CdiscTerm).to receive(:submission).with(2).and_return({versions: ["2019-01-01"], items: {}})
-    #   request.env['HTTP_ACCEPT'] = "application/json"
-    #   get :submission_results, id: "aaa"
-    #   expect(response.content_type).to eq("application/json")
-    #   expect(response.code).to eq("200") 
-    #   expect(JSON.parse(response.body).deep_symbolize_keys[:data]).to eq({items: {}, versions: ["2019-01-01"]})
-    # end
-
-    # it "submission_report" do
-    #   @user.write_setting("max_term_display", 2)
-    #   request.env['HTTP_ACCEPT'] = "application/pdf"
-    #   expect(CdiscTerm).to receive(:find).and_return(CdiscTerm.new)
-    #   expect_any_instance_of(CdiscTerm).to receive(:submission).with(2).and_return({versions: ["2019-01-01"], items: {}})
-    #   get :submission_report, id: "aaa"
-    #   expect(response.content_type).to eq("application/pdf")
-    #end
-
   end
 
   describe "Content Admin User" do
@@ -237,113 +109,6 @@ describe CdiscTermsController do
       get :history
       expect(response).to render_template("history")
     end
-
-    # it "prevents access to the import view"  do
-    #   get :import
-    #   expect(response).to redirect_to("/")
-    # end
-
-    # it "prevents access to the import cross reference view"  do
-    # 	params = { id: "TH-CDISC_CDISCTerminology", cdisc_term: { namespace: "http://www.assero.co.uk/MDRThesaurus/CDISC/V39" }}
-    #   get :import_cross_reference, params
-    #   expect(response).to redirect_to("/")
-    # end
-
-    # it "prevents access to creation of a CDISC Terminology" do
-    #   params = { cdisc_term: { version: "12", date: "2016-12-13", files: ["xxx.txt"] }}
-    #   post :create, params
-    #   expect(response).to redirect_to("/")
-    # end
-    
-    # it "prevents access to creation of a cross reference" do
-    #   params = { id: "TH-CDISC_CDISCTerminology", cdisc_term: { namespace: "http://www.assero.co.uk/MDRThesaurus/CDISC/V39", uri: "", 
-    #   	version: "39", files: ["xxx.txt"] }}
-    #   post :create_cross_reference, params
-    #   expect(response).to redirect_to("/")
-    # end
-    
-    # it "prevents access to the list of the CDISC files" do
-    #   expected = 
-    #   [
-    #     "public/test/CDISC_CT_40_39_Changes.yaml", 
-    #     "public/test/CDISC_CT_Changes.yaml", 
-    #     "public/test/CDISC_CT_Submission_Changes.yaml"
-    #   ]
-    #   get :file
-    #   expect(response).to redirect_to("/")
-    # end
-
-    # it "prevents access to file deletion" do
-    #   expected = 
-    #   [
-    #     "public/test/CDISC_CT_Changes.yaml", 
-    #     "public/test/CDISC_CT_Submission_Changes.yaml"
-    #   ]
-    #   params = {:cdisc_term => { :files => ["public/test/CDISC_CT_40_39_Changes.yaml"] }}
-    #   delete :file_delete, params
-    #   expect(response).to redirect_to("/")
-    # end
-
-    # it "displays the cross reference" do
-    # 	params = { :id => "TH-CDISC_CDISCTerminology", cdisc_term: { direction: :to, namespace: "http://www.assero.co.uk/MDRThesaurus/CDISC/V39" }}
-    #   get :cross_reference, params
-    #   expect(response).to render_template("cross_reference")
-    #   expect(assigns(:cdisc_term).version).to eq(39)
-    #   expect(assigns(:direction)).to eq(:to.to_s)
-    # end
-
-  end
-
-  describe "Curator User" do
-    
-    login_curator
-
-    it "shows the history, initial view" do
-      expect(Thesaurus).to receive(:history_uris).with({identifier: CdiscTerm::C_IDENTIFIER, scope: instance_of(IsoNamespace)}).and_return([Uri.new(uri: "http://www.example.com/a#1")])
-      get :history
-      expect(response).to render_template("history")
-    end
-
-    # it "prevents access to the import view"  do
-    #   get :import
-    #   expect(response).to redirect_to("/")
-    # end
-
-    # it "prevents access to creation of a CDISC Terminology" do
-    #   params = 
-    #   {
-    #     :cdisc_term => 
-    #     { 
-    #       :version => "12", 
-    #       :date => "2016-12-13", 
-    #       :files => ["xxx.txt"]
-    #     }
-    #   }
-    #   post :create, params
-    #   expect(response).to redirect_to("/")
-    # end
-    
-    # it "prevents access to the list of the CDISC files" do
-    #   expected = 
-    #   [
-    #     "public/test/CDISC_CT_40_39_Changes.yaml", 
-    #     "public/test/CDISC_CT_Changes.yaml", 
-    #     "public/test/CDISC_CT_Submission_Changes.yaml"
-    #   ]
-    #   get :file
-    #   expect(response).to redirect_to("/")
-    # end
-
-    # it "prevents access to file deletion" do
-    #   expected = 
-    #   [
-    #     "public/test/CDISC_CT_Changes.yaml", 
-    #     "public/test/CDISC_CT_Submission_Changes.yaml"
-    #   ]
-    #   params = {:cdisc_term => { :files => ["public/test/CDISC_CT_40_39_Changes.yaml"] }}
-    #   delete :file_delete, params
-    #   expect(response).to redirect_to("/")
-    # end
 
   end
   
