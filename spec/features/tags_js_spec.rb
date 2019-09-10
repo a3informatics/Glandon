fe6d435722e81438dd833b362787eb5daf0f7a9e
Dorte <ds@s-cubed.dk>
2019-09-10 12:48:52 +0200

    GLAN-760
    Updates to tag test affected by the left navigation bar

--- spec/features/tags_js_spec.rb
+++ spec/features/tags_js_spec.rb
@@ -68,7 +68,7 @@ describe "Tags", :type => :feature do
 
     it "create child tags organized in a hierarchical structure (REQ-MDR-TAG-020, REQ-MDR-TAG-040)", js: true do 
       create_tag_first_level("Tag1", "Tag 1 level 1")
-      click_navbar_tags
+      #click_navbar_tags
       expect(page).to have_content 'Manage Tags'
       ui_click_node_name ("Tag1")
       fill_in 'add_label', with: 'Tag1_1'
@@ -106,6 +106,7 @@ describe "Tags", :type => :feature do
 
     it "view all managed items instances for each tag when managing tags (REQ-MDR-TAG-030)", js: true do
       load_test_file_into_triple_store("tag_test_data.ttl")
+      #currently not working
       create_tag_form("TAGFORM", "Tag test form" )
       add_tags("Forms", "TAGFORM", "TAG1-1-3")
       create_tag_bc("TAGBC", "Tag test BC", "Obs PQR")
@@ -158,6 +159,7 @@ describe "Tags", :type => :feature do
     #not implemented
     it "still (not) delete tags used by managed items (REQ-MDR-TAG-060)", js: true do
       load_test_file_into_triple_store("tag_test_data.ttl")
+      #currently not working
       create_tag_form("TAGFORM", "Tag test form" )
       add_tags("Forms", "TAGFORM", "TAG4-1-1")
       click_navbar_tags
@@ -188,6 +190,7 @@ describe "Tags", :type => :feature do
      ### Add Tags to Content (MDR-TAG-15, MDR-TAG-50, MDR-TAG-70, MDR-TAG-100)
     
     it "add tags to forms auto add child tags when created (REQ-MDR-15, REQ-MDR-TAG-050)", js: true do
+      #currently not working
       create_tag_first_level("Tag1", "Tag 1 level 1")
       create_tag_form("TAGFORM", "Form for Tag Testing" )
       add_tags("Forms", "TAGFORM", "Tag1")
@@ -201,7 +204,7 @@ describe "Tags", :type => :feature do
       ui_click_node_name ("Tag1_1")
       #not implemented ui_check_table_cell('iso_managed_table',1,1,'TAGFORM')
       #2. check child tag is added to TAGFORM
-      click_link 'Forms'
+      click_navbar_forms
       expect(page).to have_content 'Index: Forms' 
       find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'History').click
       find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'Update Tags').click 
@@ -210,7 +213,9 @@ describe "Tags", :type => :feature do
 
      it "add tags to BCs auto add child tags when created (REQ-MDR-15, REQ-MDR-TAG-050)", js: true do
       create_tag_first_level("Tag1", "Tag 1 level 1")
+      #currently not working
       create_tag_bc("TAGBC", "BC for Tag Testing", "Obs PQR")
+      pause
       add_tags("Biomedical Concepts", "TAGBC", "Tag1")
       wait_for_ajax
       find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1")
@@ -222,7 +227,7 @@ describe "Tags", :type => :feature do
       ui_click_node_name ("Tag1_1")
       #not implemented ui_check_table_cell('iso_managed_table',1,1,'TAGBC')
       #2. check child tag is added to TAGFORM
-      click_link 'Biomedical Concepts'
+      click_navbar_bc
       expect(page).to have_content 'Index: Biomedical Concepts' 
       find(:xpath, "//tr[contains(.,'TAGBC')]/td/a", :text => 'History').click
       find(:xpath, "//tr[contains(.,'TAGBC')]/td/a", :text => 'Update Tags').click 
@@ -232,21 +237,26 @@ describe "Tags", :type => :feature do
      it "add tags to terminology auto add child tags when created (REQ-MDR-15, REQ-MDR-TAG-050)", js: true do
       create_tag_first_level("Tag1", "Tag 1 level 1")
       create_tag_term("TAGTERM", "Terminology for Tag Testing")
-      add_tags("Terminology", "TAGTERM", "Tag1")
+      pause
+      add_tags_term("TAGTERM", "Tag1")
       wait_for_ajax
       find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1")
       #create child tag
       create_tag_child("Tag1", "Tag1_1", "Tag 1.1 level 2")
       ui_click_node_name ("Tag1")
       ui_check_table_cell('iso_managed_table',1,1,'TAGTERM')
+      pause
       #1. check child tag is added to TAGTERM
       ui_click_node_name ("Tag1_1")
       #not implemented ui_check_table_cell('iso_managed_table',1,1,'TAGTERM')
       #2. check child tag is added to TAGTERM
-      click_link 'Terminology'
+      click_navbar_dashboard
+      click_navbar_terminology
       expect(page).to have_content 'Index: Terminology' 
       find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'History').click
-      find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'Update Tags').click 
+      pause
+      #find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'Update Tags').click 
+      pause
       #not implemented find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1_1")
     end
 
--- spec/support/tag_helpers.rb
+++ spec/support/tag_helpers.rb
@@ -1,6 +1,7 @@
 module TagHelper
 
   def create_classification
+      click_navbar_dashboard
       expect(page).to have_content 'Registration Status Counts'   
       visit 'iso_concept_systems'
       expect(page).to have_content 'Classifications'  
@@ -12,8 +13,9 @@ module TagHelper
     end
 
   def create_tag_first_level(label, description)
+      click_navbar_dashboard
       expect(page).to have_content 'Registration Status Counts'   
-      visit 'iso_concept_systems'
+      click_navbar_tags
       expect(page).to have_content 'Tag Viewer'  
       ui_check_input("edit_label", 'Tags')
       fill_in 'add_label', with: "#{label}"
@@ -24,9 +26,9 @@ module TagHelper
   end
 
   def create_tag_child(parent, identifier, label)
-      
+      click_navbar_dashboard
       expect(page).to have_content 'Registration Status Counts'   
-      visit 'iso_concept_systems'
+      click_navbar_tags
       expect(page).to have_content 'Tag Viewer'  
       ui_click_node_name ("#{parent}")
       ui_check_input("edit_label", "#{parent}")
@@ -37,7 +39,8 @@ module TagHelper
   end
 
   def create_tag_form(identifier, label)
-    visit 'forms'
+    click_navbar_dashboard
+    click_navbar_forms
     expect(page).to have_content 'Index: Forms'  
     click_link 'New'
     fill_in 'form_identifier', with: "#{identifier}"
@@ -47,8 +50,8 @@ module TagHelper
   end
 
   def create_tag_bc(identifier, label, template)
-    #ui_scroll_to_id("biomedical_concept_identifier")
-    visit 'biomedical_concepts'
+    click_navbar_dashboard
+    click_navbar_bc
     wait_for_ajax
     expect(page).to have_content 'Index: Biomedical Concepts'  
     click_link 'New'
@@ -58,35 +61,28 @@ module TagHelper
     ui_click_by_id("ims_add_button")
     click_button 'Create'
     expect(page).to have_content 'Biomedical Concept was successfully created.'
-    #expect(page).to have_content("The Biomedical Concept was succesfully created.") 
   end
 
   def create_tag_term(identifier, label)
-      visit 'thesauri'
+      click_navbar_dashboard
+      click_navbar_terminology
       expect(page).to have_content 'Index: Terminology'
-      click_link 'New'
-      expect(page).to have_content 'New Terminology:'
       fill_in 'thesauri_identifier', with: "#{identifier}"
       fill_in 'thesauri_label', with: "#{label}"
       click_button 'Create'
       expect(page).to have_content 'Terminology was successfully created.'
     end
  
-  def add_tags(link, identifier, tag)
-    click_link "#{link}"
-    expect(page).to have_content 'Index:' 
+  def add_tags_term(identifier, tag)
+    click_navbar_dashboard
+    click_navbar_terminology
+    expect(page).to have_content 'Index: Terminology' 
     find(:xpath, "//tr[contains(.,'#{identifier}')]/td/a", :text => 'History').click
     expect(page).to have_content 'History:'  
-    # find(:xpath, "//tr[contains(.,'#{identifier}')]/td/a", :text => 'Edit').click
-    # expect(page).to have_content 'Edit:'  
     find(:xpath, "//tr[contains(.,'#{identifier}')]/td/a", :text => 'Update Tags').click
     expect(page).to have_content 'Edit Tags:'
     ui_click_node_name("#{tag}") #{tag}"
-    #expect(display_label).to have_content "#{tag}"
     ui_click_tag_add
-    #ui_check_table_cell("iso_managed_tag_table", 1, 1, "#{tag}")
-    #ui_click_save
-    #ui_click_close
   end
 
 end
