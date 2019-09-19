5c14376944b05f1c31026844351b2b34f50ac563
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-09-19 19:38:59 +0200

    GLAN-825
    [1] Timestamp tweaks and tests

--- spec/models/concerns/timestamp_spec.rb
+++ spec/models/concerns/timestamp_spec.rb
@@ -9,13 +9,13 @@ describe Timestamp do
 
   it "allows for the class to be created with an initial value" do
     timestamp = Timestamp.new("2016-11-04")
-    expect(timestamp.to_json).to eq("{\"time\":\"2016-11-04T00:00:00.000+00:00\"}")
+    expect(timestamp.time.strftime("%Y-%b-%d").to_s).to eq("2016-Nov-04")
   end
 
   it "allows the time to be set" do
     timestamp = Timestamp.new()
     timestamp.from_timestamp("2016-11-04 16:00:01")
-    expect(timestamp.to_json).to eq("{\"time\":\"2016-11-04T16:00:01.000+00:00\"}")
+    expect(timestamp.time.strftime("%Y-%b-%d, %H:%M")).to eq("2016-Nov-04, 16:00")
   end
 
   it "supports multiple output formats" do
@@ -23,7 +23,7 @@ describe Timestamp do
     timestamp.from_timestamp("2016-11-04 16:00:01")
     expect(timestamp.to_datetime).to eq("2016-Nov-04, 16:00")
     expect(timestamp.to_date).to eq("2016-Nov-04")
-    expect(timestamp.to_8601).to eq("2016-11-04T16:00:01+00:00")
+    expect(timestamp.to_8601).to eq("2016-11-04T16:00:01Z")
   end
 
 end
