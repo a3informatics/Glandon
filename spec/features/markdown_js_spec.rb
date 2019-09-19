a063f448135af5ba96e94158f06b2adb384843f3
Samuel Banas <sab@s-cubed.dk>
2019-09-19 12:53:37 +0200

    GLAN-663
    [1] Fixed Features tests for Markdown (markdown_js_spec.rb)

--- spec/features/markdown_js_spec.rb
+++ spec/features/markdown_js_spec.rb
@@ -1,28 +1,31 @@
 require 'rails_helper'
 
 describe "Markdown", :type => :feature do
-  
+
   include PauseHelpers
+  include UiHelpers
+  include UserAccountHelpers
 
   before :all do
-    user = User.create :email => "curator@example.com", :password => "12345678" 
-    user.add_role :curator
+    ua_create
+  end
+
+  before :each do
+    ua_curator_login
+  end
+
+  after :each do
+    ua_logoff
   end
 
   after :all do
-    user = User.where(:email => "curator@example.com").first
-    user.destroy
+    ua_destroy
   end
 
   describe "valid user", :type => :feature, js: true do
 
     it "allows markdown" do
-      visit '/users/sign_in'
-      fill_in 'Email', with: 'curator@example.com'
-      fill_in 'Password', with: '12345678'
-      click_button 'Log in'
-      expect(page).to have_content 'Signed in successfully'
-      click_link 'Markdown'
+      click_navbar_ma
       expect(page).to have_content 'Raw Markdown:'
       expect(page).to have_content 'Processed Markdown:'
       fill_in "raw_markdown", with: "Hello world I am *here*"
