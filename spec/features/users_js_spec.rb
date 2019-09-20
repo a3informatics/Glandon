adc0e16688aa6c844b2db75f53340cea2f43e83c
Samuel Banas <sab@s-cubed.dk>
2019-09-20 14:52:46 +0200

    GLAN-839
    [1] Redesign of the Index: Users page
    [2] Redesign of the Edit user page
    [3] Introduction of a generalized simple header partial and a ico_btn_secondary_v2 partial
    [4] Feature tests fixes (user_settings_js_spec, users_js_spec, users_spec)

--- spec/features/users_js_spec.rb
+++ spec/features/users_js_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe "Users", :type => :feature do
-  
+
   include PauseHelpers
   include UserAccountHelpers
-  
+
   before :all do
     ua_create
-    user = User.create :email => "delete@example.com", :password => "12345678" 
+    user = User.create :email => "delete@example.com", :password => "12345678"
     user.add_role :curator
   end
 
@@ -24,7 +24,7 @@ describe "Users", :type => :feature do
       fill_in 'Password', with: '12345678'
       click_button 'Log in'
       click_link 'users_button'
-      expect(page).to have_content 'Index: User'
+      expect(page).to have_content 'All user accounts'
       audit_count = AuditTrail.count
       find(:xpath, "//tr[contains(.,'delete@example.com')]/td/a", :text => 'Delete').click
       page.accept_alert
