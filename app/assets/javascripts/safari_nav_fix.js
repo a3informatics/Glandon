932148ca34012f2054b4df57ceae0319252635d5
Samuel Banas <sab@s-cubed.dk>
2019-09-30 15:47:06 +0200

    GLAN-878
    [1] Modified safari loading fix; instead of reloading the page, just remove the loading and clear timeout

--- app/assets/javascripts/safari_nav_fix.js
+++ app/assets/javascripts/safari_nav_fix.js
@@ -1,5 +1,6 @@
 window.onpageshow = function(event) {
-    if (event.persisted && isSafari()) {
-        window.location.reload()
+    if (event.persisted && isSafari() && $(".spinner-wrap").length) {
+      $(".spinner-wrap").remove();
+      clearTimeout(pdfLoadingTimeout);
     }
 };
