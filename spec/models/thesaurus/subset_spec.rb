05f4be948ed6b6874b7bbe95e4357af654f5aa60
Clarisa <car@s-cubed.dk>
2019-10-24 22:54:05 +0200

    GLAN-940

--- spec/models/thesaurus/subset_spec.rb
+++ spec/models/thesaurus/subset_spec.rb
@@ -91,11 +91,27 @@ describe "Thesaurus::Subset" do
   it "allow remove an item from the list" do
     subset_uri_1 = Uri.new(uri: "http://www.assero.co.uk/TS#54176c59-b800-43f5-99c3-d129cb563b79")
     subset = Thesaurus::Subset.find(subset_uri_1)
-    sm_uri_2 = Uri.new(uri: "http://www.assero.co.uk/TSM#67871de3-5e13-42da-9814-e9fc3ce7b2f3")
-    sm_uri_3 = Uri.new(uri: "http://www.assero.co.uk/TSM#a230eecb-1580-4cc9-a1af-5e18a6eb1a19")
-    result = subset.remove(sm_uri_2.to_id)
-    # subset_member = Thesaurus::SubsetMember.find(uri_2)
-    expect(subset.members.to_h).to eq(sm_uri_3.to_h)
+    uri_2 = Uri.new(uri: "http://www.assero.co.uk/TSM#a230eecb-1580-4cc9-a1af-5e18a6eb1a19")
+    uri_3 = Uri.new(uri: "http://www.assero.co.uk/TSM#c2c707b1-c7a2-4ee5-a9ae-bd63a57c5314")
+    uri_4 = Uri.new(uri: "http://www.assero.co.uk/TSM#67871de3-5e13-42da-9814-e9fc3ce7b2f3")
+    sm_2 = Thesaurus::SubsetMember.find(uri_2)
+    sm_3 = Thesaurus::SubsetMember.find(uri_3)
+    sm_4 = Thesaurus::SubsetMember.find(uri_4)
+  byebug
+    result = subset.remove(sm_2.uri.to_id)
+    expect(sm_4.next_member.to_h).to eq(sm_3.to_h)
+  end
+
+  it "allow remove an item from the list, first subset member" do
+    subset_uri_1 = Uri.new(uri: "http://www.assero.co.uk/TS#54176c59-b800-43f5-99c3-d129cb563b79")
+    subset = Thesaurus::Subset.find(subset_uri_1)
+    uri_2 = Uri.new(uri: "http://www.assero.co.uk/TSM#67871de3-5e13-42da-9814-e9fc3ce7b2f3")
+    uri_3 = Uri.new(uri: "http://www.assero.co.uk/TSM#a230eecb-1580-4cc9-a1af-5e18a6eb1a19")
+    sm_2 = Thesaurus::SubsetMember.find(uri_2)
+    sm_3 = Thesaurus::SubsetMember.find(uri_3)
+  byebug
+    result = subset.remove(sm_2.uri.to_id)
+    expect(subset.members).to eq(sm_3.uri)
   end
 
   it "validates a valid object" do
