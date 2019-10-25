f8e00701a59477ba26ae9cf9f7afb80304b77c0d
Samuel Banas <sab@s-cubed.dk>
2019-10-25 08:33:51 +0200

    GLAN-940
    [1] Added a a feature test for new subset

--- spec/features/thesauri_subsets_js_spec.rb
+++ spec/features/thesauri_subsets_js_spec.rb
@@ -13,7 +13,7 @@ describe "Thesauri", :type => :feature do
     before :all do
       schema_files =["ISO11179Types.ttl", "ISO11179Identification.ttl", "ISO11179Registration.ttl",
         "ISO11179Concepts.ttl", "thesaurus.ttl"]
-      data_files = ["CT_SUBSETS.ttl","iso_namespace_real.ttl", "iso_registration_authority_real.ttl"]
+      data_files = ["CT_SUBSETS.ttl", "thesaurus_new_airports.ttl", "iso_namespace_real.ttl", "iso_registration_authority_real.ttl"]
       load_files(schema_files, data_files)
       load_cdisc_term_versions(1..20)
       ua_create
@@ -34,10 +34,10 @@ describe "Thesauri", :type => :feature do
      #Index subsets
     it "index subsets (REQ-MDR-?????)", js:true do
       click_navbar_cdisc_terminology
-      expect(page).to have_content 'History'
       wait_for_ajax(7)
       context_menu_element("history", 5, "2010-03-05 Release", :show)
       expect(page).to have_content '2010-03-05 Release'
+      wait_for_ajax
       ui_child_search("C66726")
       ui_check_table_info('children_table', 1, 1, 1)
       find(:xpath, "//tr[contains(.,'C66726')]/td/a", :text => 'Show').click
@@ -47,6 +47,24 @@ describe "Thesauri", :type => :feature do
       ui_check_table_cell("ssIndexTable", 1, 1, "S000001")
       ui_check_table_cell("ssIndexTable", 2, 1, "S000002")
       click_button "Close"
+    end
+
+    it "adds a new subset (REQ-MDR-?????)", js:true do
+      click_navbar_cdisc_terminology
+      wait_for_ajax
+      context_menu_element("history", 5, "2010-03-05 Release", :show)
+      expect(page).to have_content '2010-03-05 Release'
+      wait_for_ajax
+      find(:xpath, "//tr[contains(.,'C85494')]/td/a", :text => 'Show').click
+      expect(page).to have_content("PKUNIT")
+      click_link "Subsets"
+      expect(page).to have_content("No subsets found.")
+      click_button "+ New subset"
+      expect(page).to have_content("Select Terminology")
+      find(:xpath, "//*[@id='thTable']/tbody/tr[1]/td[1]").click
+      click_button 'Select'
+      wait_for_ajax
+      # Redirect !
     end
 
 
