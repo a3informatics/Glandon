6341e1b1b34ab09f98c6d1c9bb831efce4c13e4d
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-10-25 12:55:22 +0200

    Update application_controller_spec.rb
    [1] Remove tests, no longer required
    [2] Add pending tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,29 +2,17 @@ require 'rails_helper'
 
 describe ApplicationController, type: :controller do
 
-  describe "tests" do
-  	
-    it "params to id, namespace and id, strong_key nil" do
-      params = {namespace: "http://www.example.com/path1/path2", id: "fragment"}
-      actual = @controller.params_to_id(params)
-      expect(actual).to eq("aHR0cDovL3d3dy5leGFtcGxlLmNvbS9wYXRoMS9wYXRoMiNmcmFnbWVudA==")
-    end
+  describe "helper tests" do
 
-    it "params to id, strong_key no nil" do
-      params = { id: "F-ACME_TEST", iso_managed:{namespace:"http://www.assero.co.uk/MDRForms/ACME/V1", current_id:"test"} } 
-      strong_key = {namespace:"http://www.assero.co.uk/MDRForms/ACME/V1", current_id:"test"}
-      actual = @controller.params_to_id(params, strong_key)
-      expect(actual).to eq("aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTURSRm9ybXMvQUNNRS9WMSNGLUFDTUVfVEVTVA==")
+    describe "to turtle"
 
-    end
+    describe "before_action_steps"
+    
+    describe "get_token(mi)"
 
-    it "params to id, no need to convert id, strong_key nil" do
-      params = { id: "aHR0cDovL3d3d==" }
-      actual = @controller.params_to_id(params)
-      expect(actual).to eq("aHR0cDovL3d3d==")
-    end
-
-  end
+    describe "edit_item(item)"
+    
+    describe "after_sign_out_path_for(*)"
 
 end
 
