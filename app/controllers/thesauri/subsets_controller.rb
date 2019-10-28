3d3fb3a562946fb8326b4ab4410909d81d10c7d7
Clarisa <car@s-cubed.dk>
2019-10-28 08:10:05 +0100

    GLAN-952
    [1] Remove byebug

--- app/controllers/thesauri/subsets_controller.rb
+++ app/controllers/thesauri/subsets_controller.rb
@@ -15,7 +15,6 @@ class Thesauri::SubsetsController < ApplicationController
     authorize Thesaurus, :edit?
     subset = Thesaurus::Subset.find(params[:id])
     sm = subset.remove(the_params[:member_id])
-  byebug
     render json: {data: subset.uri.to_id}, status: 200
   end
 
