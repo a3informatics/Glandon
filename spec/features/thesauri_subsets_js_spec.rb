389065383475091a0ed279c9a24437044cefc044
Clarisa <car@s-cubed.dk>
2019-10-31 15:13:04 +0100

    GLAN-938
    [1] Test fixed

--- spec/features/thesauri_subsets_js_spec.rb
+++ spec/features/thesauri_subsets_js_spec.rb
@@ -8,19 +8,37 @@ describe "Thesauri", :type => :feature do
   include PauseHelpers
   include WaitForAjaxHelper
 
+  def sub_dir
+    return "features/thesaurus/subset"
+  end
+
+  # # Prepares a link between the subset, a managed concept, and a test terminology
+  # def init_subset(subset)
+  #   ct = Thesaurus.create({label: "Test Terminology", identifier: "TT"})
+  #   mc = ct.add_child({})
+  #   mc = Thesaurus::ManagedConcept.find_minimum(mc.id)
+  #   mc.add_link(:is_ordered, subset.uri)
+  #   mc.save
+  #   @token = Token.obtain(mc, @user)
+  #   subset
+  # end
+
   describe "The Content Admin User can", :type => :feature do
 
     before :all do
       data_files = ["CT_SUBSETS.ttl", "CT_SUBSETS_new.ttl", "iso_namespace_real.ttl", "iso_registration_authority_real.ttl"]
       load_files(schema_files, data_files)
       load_cdisc_term_versions(1..20)
+      load_local_file_into_triple_store(sub_dir, "subsets_input_4.ttl")
       NameValue.destroy_all
       NameValue.create(name: "thesaurus_parent_identifier", value: "123")
       NameValue.create(name: "thesaurus_child_identifier", value: "456")
       ua_create
+      Token.delete_all
     end
 
     before :each do
+      # Token.delete_all
       ua_curator_login
     end
 
@@ -71,7 +89,7 @@ describe "Thesauri", :type => :feature do
       expect(page).to have_content("Edit Subset")
     end
 
-    it "allows to edit a subset", js:true do
+    it "allows to edit a subset, add, remove and move_after item", js:true do
       click_navbar_cdisc_terminology
       wait_for_ajax(7)
       context_menu_element("history", 5, "2010-03-05 Release", :show)
@@ -81,22 +99,20 @@ describe "Thesauri", :type => :feature do
       ui_child_search("C85494")
       find(:xpath, "//tr[contains(.,'C85494')]/td/a", :text => 'Show').click
       wait_for_ajax(120)
-      # expect(page).to have_content("CDISC SDTM Pharmaceutical Dosage Form Terminology")
       expect(page).to have_link("Subsets")
-      click_link "Subsets"
-
+      click_link "Subsets"    
       context_menu_element("ssIndexTable", 3, "PK Parameter Units of Measure", :edit)
-
-      # find(:xpath, "//*[@id='con-menu-0']").click
-
-      
-
+      find(:xpath, "//*[@id='source_children_table']/tbody/tr[1]/td").click
       wait_for_ajax
-      pause
-      # ui_check_table_cell("ssIndexTable", 1, 2, "S000001")
-      # ui_check_table_cell("ssIndexTable", 2, 2, "S000002")
-      # click_button "Close"
-      
+      ui_check_table_cell("subset_children_table", 4, 2, "Day Times Femtogram per Milliliter\nday*fg/mL (C85583)")
+      ui_check_table_cell("subset_children_table", 3, 2, "Day Times Mole per Milliliter\nday*mol/mL (C85590)")
+      source = page.find(:xpath, "//*[@id='subset_children_table']/tbody/tr[2]")
+      target = page.find(:xpath, "//*[@id='source_children_table']/tbody/tr[3]")
+      source.drag_to(target)
+      ui_check_table_cell("subset_children_table", 2, 2, "Day Times Mole per Milliliter\nday*mol/mL (C85590)")
+      find(:xpath, "//*[@id='source_children_table']/tbody/tr[4]/td").click
+      find(:xpath, "//*[@id='source_children_table']/tbody/tr[1]/td").click
+      ui_check_table_cell("subset_children_table", 4, 2, "Day Times Microgram per Milliliter\nday*ug/mL (C85586)")
     end
 
 
