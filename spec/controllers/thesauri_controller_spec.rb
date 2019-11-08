25c7bc28288c996d31ec08232a0a4af25d7ced65
Clarisa <car@s-cubed.dk>
2019-11-08 14:15:35 +0100

    GLAN-956
    [1] Fixed Extension test

--- spec/controllers/thesauri_controller_spec.rb
+++ spec/controllers/thesauri_controller_spec.rb
@@ -460,12 +460,17 @@ describe ThesauriController do
 
     it "extension" do
       request.env['HTTP_ACCEPT'] = "application/json"
-      post :extension, {thesauri: {}}
+      post :extension, {thesauri: { reference_ct_id: "aHR0cDovL3d3dy5jZGlzYy5vcmcvQ1QvVjIjVEg=", 
+                                    scope_id: IsoRegistrationAuthority.cdisc_scope.id, 
+                                    identifier: CdiscTerm::C_IDENTIFIER, 
+                                    concept_id: "aHR0cDovL3d3dy5jZGlzYy5vcmcvQzY3MTU0L1YyI0M2NzE1NA==" 
+                                  }
+                        }
       expect(response.content_type).to eq("application/json")
       expect(response.code).to eq("200") 
-      expect(JSON.parse(response.body).deep_symbolize_keys[:data]).to eq({items: {}, versions: ["2019-01-01"]})
+      x = JSON.parse(response.body).deep_symbolize_keys
+      expect(x).to hash_equal({:show_path=>"/thesauri/managed_concepts/aHR0cDovL3d3dy5hY21lLXBoYXJtYS5jb20vQzY3MTU0RS9WMSNDNjcxNTRF?managed_concept%5Bcontext_id%5D=aHR0cDovL3d3dy5hY21lLXBoYXJtYS5jb20vQ1QvVjMjVEg%3D&managed_concept%5Breference_ct_id%5D=aHR0cDovL3d3dy5jZGlzYy5vcmcvQ1QvVjIjVEg%3D"})
     end
-
   end
 
   describe "Community Reader" do
@@ -519,7 +524,7 @@ describe ThesauriController do
     end
 
     it "prevents access to a reader, add child" do
-      get :add_child
+      get :add_child, id: 1
       expect(response).to redirect_to("/")
     end
 
