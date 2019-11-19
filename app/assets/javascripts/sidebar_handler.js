ee27d50714ec4dfe78549ce0e509aa90e672578a
Samuel Banas <sab@s-cubed.dk>
2019-11-19 14:07:01 +0100

    GLAN-981
    [1] Reorganized sidebar code
    [2] Fixes issues GLAN-982 and GLAN-983

---
+++ app/assets/javascripts/sidebar_handler.js
@@ -0,0 +1,70 @@
+var sidebar = "#sidebar",
+  sidebarArrow = "#sidebar-arrow",
+  sidebarVerArrow = "#sidebar-v-toggle",
+  sidebarCollapseHorClass = "sidebar-collapsed",
+  sidebarCollapseVerClass = "collapsed-vertical",
+  arrowClass = "arrow-rotate";
+
+
+function isSidebarHorizontal(){
+  return $(document).width() > 767;
+}
+
+function isSidebarOpen(){
+  if (isSidebarHorizontal() && $(sidebar).hasClass(sidebarCollapseHorClass))
+    return false;
+  else if(!isSidebarHorizontal() && $(sidebar).hasClass(sidebarCollapseVerClass))
+    return false;
+  else
+    return true;
+}
+
+function openSidebar(){
+  $(sidebar).removeClass((isSidebarHorizontal() ? sidebarCollapseHorClass : sidebarCollapseVerClass));
+  getArrow().removeClass(arrowClass);
+  handleContentWidth();
+}
+
+function closeSidebar(){
+  $(sidebar).addClass((isSidebarHorizontal() ? sidebarCollapseHorClass : sidebarCollapseVerClass));
+  getArrow().addClass(arrowClass);
+  handleContentWidth();
+}
+
+function toggleSidebar(){
+  if(isSidebarOpen())
+    closeSidebar();
+  else
+    openSidebar();
+}
+
+function getArrow(){
+  if($(sidebarVerArrow).is(":visible"))
+    return $(sidebarVerArrow);
+  else
+    return $(sidebarArrow);
+}
+
+/*
+* Expands / collapses a menu category
+*/
+function sidebarCategoryHandler(item){
+  if (!isSidebarOpen())
+    openSidebar();
+
+  $(item).find('.arrow').toggleClass('arrow-rotate');
+  $(item).parent().toggleClass('collapsed');
+}
+
+function handleContentWidth(){
+  if (!isSidebarHorizontal())
+    return;
+  // Animate main_area width
+  $('#main_area').toggleClass('col-sm-10');
+  $('#main_area').toggleClass('col-sm-11');
+  $('#sidebar').toggleClass('col-sm-2');
+  $('#sidebar').toggleClass('col-sm-1');
+
+  $('#main_area').toggleClass('ma-sb-col');
+  $('#main_area').toggleClass('ma-sb-exp');
+}
