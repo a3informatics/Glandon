37e5dbef0228632c43293c717a0c8363b0eb643f
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-11-20 20:45:43 +0100

    GLAN-978
    [1] Update controller, missed last push

--- app/controllers/thesauri_controller.rb
+++ app/controllers/thesauri_controller.rb
@@ -29,7 +29,6 @@ class ThesauriController < ApplicationController
     render json: {data: Thesaurus.unique.select{|x| x[:scope_id] == owner_scoped_id}}
   end
 
-
   def history
     authorize Thesaurus
     respond_to do |format|
@@ -162,6 +161,22 @@ class ThesauriController < ApplicationController
     end
   end
 
+  def search_current
+    authorize Thesaurus, :show?
+    respond_to do |format|
+      format.html
+        @close_path = thesauri_index_path
+      format.json do
+        if Thesaurus.empty_search?(params)
+          render json: { :draw => params[:draw], :recordsTotal => params[:length], :recordsFiltered => "0", :data => [] }
+        else
+          results = Thesaurus.search_current(params)
+          render json: { :draw => params[:draw], :recordsTotal => params[:length], :recordsFiltered => results[:count].to_s, :data => results[:items] }
+        end
+      end
+    end
+  end
+
   def changes
     authorize Thesaurus, :show?
     @version_count = current_user.max_term_display.to_i
@@ -234,11 +249,6 @@ class ThesauriController < ApplicationController
     thesaurus = edit_item(thesaurus)
     new_object = thesaurus.add_extension(the_params[:concept_id])
     render json: {show_path: thesauri_managed_concept_path({id: new_object.id, managed_concept: {context_id: thesaurus.id, reference_ct_id: the_params[:reference_ct_id]}})}, :status => 200
-  end
-
-  def search_current
-    authorize Thesaurus, :show?
-    @close_path = thesauri_index_path
   end
 
    def export_ttl
