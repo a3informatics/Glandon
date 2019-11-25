3ac47d75aad4d2c7a7b1206e90cd7bc9f9b23a13
Dave Iberson-Hurst <daveih1664@gmail.com>
2019-11-25 12:46:22 +0100

    GLAN-985
    [1] Fix test after merge and changes to symbol

--- spec/helpers/breadcrumbs_helper_spec.rb
+++ spec/helpers/breadcrumbs_helper_spec.rb
@@ -27,18 +27,15 @@ describe BreadcrumbsHelper do
     end
   
   	it "first level" do
-    	helper.first_level_breadcrumb({link: "/test", text: "First"})
-      expected = "<ol class=\"breadcrumb\"><li id=\"breadcrumb_1\" class=\"active\">First</li></ol>"
+    	#helper.first_level_breadcrumb(:users)
+      helper.first_level_breadcrumb(:users)
+      expected = "<ol class=\"breadcrumb\"><li id=\"breadcrumb_1\" class=\"active\">Users</li></ol>"
       expect(session[:breadcrumbs]).to eq(expected)
     end
 
   	it "second level" do
-      item = IsoManagedV2.new
-      item.has_identifier = IsoScopedIdentifierV2.new
-      item.has_identifier.identifier = "XXX"
-      item.has_identifier.has_scope = IsoNamespace.all.first.id
-    	helper.second_level_breadcrumb({link: "/test", text: "First"}, item.has_identifier.has_scope, item.has_identifier.identifier ) 
-      expected = "<ol class=\"breadcrumb\"><li id=\"breadcrumb_1\"><a href=\"/test\">First</a></li><li " + 
+    	helper.second_level_breadcrumb(:users, IsoNamespace.find_by_short_name("BBB").id, "XXX") 
+      expected = "<ol class=\"breadcrumb\"><li id=\"breadcrumb_1\"><a href=\"/users\">Users</a></li><li " + 
       	"id=\"breadcrumb_2\" class=\"active\">BBB, XXX</li></ol>"
       expect(session[:breadcrumbs]).to eq(expected)
     end
@@ -49,8 +46,8 @@ describe BreadcrumbsHelper do
       item.has_identifier.identifier = "BREADCRUMB"
       item.has_identifier.has_scope = IsoNamespace.new(uri: Uri.new(uri: "http://www.assero.co.uk/NS#DDD"), name: "DDD Long", short_name: "DDD", authority: "www.ddd.com")
       item.has_identifier.semantic_version = "1.2.3"
-    	helper.third_level_breadcrumb({link: "/test", text: "First"}, item, "/test/level2")
-      expected = "<ol class=\"breadcrumb\"><li id=\"breadcrumb_1\"><a href=\"/test\">First</a></li><li id=\"breadcrumb_2\"><a " + 
+    	helper.third_level_breadcrumb(:users, item, "/test/level2")
+      expected = "<ol class=\"breadcrumb\"><li id=\"breadcrumb_1\"><a href=\"/users\">Users</a></li><li id=\"breadcrumb_2\"><a " + 
       	"href=\"/test/level2\">DDD, BREADCRUMB</a></li><li id=\"breadcrumb_3\" class=\"active\">V1.2.3</li></ol>"
       expect(session[:breadcrumbs]).to eq(expected)
     end
@@ -61,8 +58,8 @@ describe BreadcrumbsHelper do
       item.has_identifier.identifier = "BREADCRUMB"
       item.has_identifier.has_scope = IsoNamespace.new(uri: Uri.new(uri: "http://www.assero.co.uk/NS#DDD"), name: "DDD Long", short_name: "DDD", authority: "www.ddd.com")
       item.has_identifier.semantic_version = "1.2.3"
-    	helper.fourth_level_breadcrumb({link: "/test", text: "First"}, item, "/test/level2", "/test/level3", "Action 4")
-      expected = "<ol class=\"breadcrumb\"><li id=\"breadcrumb_1\"><a href=\"/test\">First</a></li><li id=\"breadcrumb_2\"><a " + 
+    	helper.fourth_level_breadcrumb(:users, item, "/test/level2", "/test/level3", "Action 4")
+      expected = "<ol class=\"breadcrumb\"><li id=\"breadcrumb_1\"><a href=\"/users\">Users</a></li><li id=\"breadcrumb_2\"><a " + 
       	"href=\"/test/level2\">DDD, BREADCRUMB</a></li><li id=\"breadcrumb_3\"><a href=\"/test/level3\">V1.2.3</a></li><li " + 
       	"id=\"breadcrumb_4\" class=\"active\">Action 4</li></ol>"
       expect(session[:breadcrumbs]).to eq(expected)
