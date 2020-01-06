a88fc492c5b8014cbc28f36d67669e692d58bddf
Samuel Banas <sab@s-cubed.dk>
2020-01-06 13:31:11 +0100

    GLAN-1128
    [1] Removed Item List panel from edit tags

--- app/assets/javascripts/iso_managed/edit_tags.js
+++ app/assets/javascripts/iso_managed/edit_tags.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
 
   var csvp = new ConceptSystemViewPanel(conceptSystemId, 100, nodeSelected);
   var imtlp = new IsoManagedTagListPanel(isoManagedId, isoManagedNamespace);
-  var icl = new IsoConceptList();
 
   function nodeSelected(data) {
     imtlp.selected(data);
-    icl.refresh(data.id);
   }
 
   // Set window resize.
