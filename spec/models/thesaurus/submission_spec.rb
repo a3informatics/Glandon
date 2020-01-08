7e035fd4067d3e42088cec5daa567a162f491245
Dave Iberson-Hurst <daveih1664@gmail.com>
2020-01-08 20:32:24 +0100

    GLAN-1134
    [1] Submission test code improvements
    [2] Changes test
    [3] Helper update

--- spec/models/thesaurus/submission_spec.rb
+++ spec/models/thesaurus/submission_spec.rb
@@ -10,14 +10,10 @@ describe "Thesaurus Submission" do
     return "models/thesaurus/submission"
   end
 
-  def last_version
-    62
-  end
-
   before :all do
     data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl"]
     load_files(schema_files, data_files)
-    load_versions((1..last_version))
+    load_versions(CdiscCtHelpers.version_range)
     @status_map = {:~ => :not_present, :- => :no_change, :C => :created, :U => :updated, :D => :deleted}
   end
 
@@ -63,7 +59,7 @@ describe "Thesaurus Submission" do
   it "submission changes" do
     expected = read_yaml_file(sub_dir, "submission_expected.yaml")
     result = true
-    (1..last_version).each do |version|
+    (CdiscCtHelpers.version_range).each do |version|
       puts "***** V#{version}, #{expected.find{|x| x[:version] == version}[:date]} *****"
       ct = CdiscTerm.find_minimum(Uri.new(uri: "http://www.cdisc.org/CT/V#{version}#TH"))
       actual = ct.submission(1)
