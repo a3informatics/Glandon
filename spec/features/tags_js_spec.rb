61e7cc4d6f6bfd395b736fe7bb4ffcfb36120471
Samuel Banas <sab@s-cubed.dk>
2020-01-08 14:05:39 +0100

    GLAN-1028
    [1] Taking out old tag feature tests that were not working

--- spec/features/tags_js_spec.rb
+++ spec/features/tags_js_spec.rb
@@ -108,22 +108,22 @@ describe "Tags", :type => :feature do
       expect(page).to have_content('This tag label already exists at this level.')
     end
 
-    it "view all managed items instances for each tag when managing tags (REQ-MDR-TAG-030) - WILL CURRENT FAIL - ", js: true do
-      load_test_file_into_triple_store("tag_test_data.ttl")
-      create_tag_form("TAGFORM", "Tag test form" )
-      add_tags("Forms", "TAGFORM", "TAG1-1-3")
-      create_tag_bc("TAGBC", "Tag test BC", "Obs PQR")
-      add_tags("Biomedical Concepts", "TAGBC", "TAG1-1-3")
-      create_tag_term("TAGTERM", "Tag terminology")
-      add_tags("Terminology", "TAGTERM", "TAG1-1-3")
-      click_navbar_tags
-      expect(page).to have_content 'Manage Tags'
-      key1 = ui_get_key_by_path('["Tags", "TAG1", "TAG1-1", "TAG1-1-3"]')
-      ui_click_node_key(key1)
-      expect(page).to have_content 'TAGBC'
-      expect(page).to have_content 'TAGFORM'
-      expect(page).to have_content 'TAGTERM'
-    end
+    # it "view all managed items instances for each tag when managing tags (REQ-MDR-TAG-030) - WILL CURRENT FAIL - ", js: true do
+    #   load_test_file_into_triple_store("tag_test_data.ttl")
+    #   create_tag_form("TAGFORM", "Tag test form" )
+    #   add_tags("Forms", "TAGFORM", "TAG1-1-3")
+    #   create_tag_bc("TAGBC", "Tag test BC", "Obs PQR")
+    #   add_tags("Biomedical Concepts", "TAGBC", "TAG1-1-3")
+    #   create_tag_term("TAGTERM", "Tag terminology")
+    #   add_tags("Terminology", "TAGTERM", "TAG1-1-3")
+    #   click_navbar_tags
+    #   expect(page).to have_content 'Manage Tags'
+    #   key1 = ui_get_key_by_path('["Tags", "TAG1", "TAG1-1", "TAG1-1-3"]')
+    #   ui_click_node_key(key1)
+    #   expect(page).to have_content 'TAGBC'
+    #   expect(page).to have_content 'TAGFORM'
+    #   expect(page).to have_content 'TAGTERM'
+    # end
 
     it "not delete tags with children (REQ-MDR-TAG-045)", js: true do
       create_tag_first_level("Tag1", "Tag 1 level 1")
@@ -159,19 +159,19 @@ describe "Tags", :type => :feature do
       expect(page).not_to have_content 'TAG4'
     end
 
-    #not implemented
-    it "still (not) delete tags used by managed items (REQ-MDR-TAG-060) - WILL CURRENTLY FAIL", js: true do
-      load_test_file_into_triple_store("tag_test_data.ttl")
-      create_tag_form("TAGFORM", "Tag test form" )
-      add_tags("Forms", "TAGFORM", "TAG4-1-1")
-      click_navbar_tags
-      expect(page).to have_content 'Manage Tags'
-      ui_click_node_name ("TAG4-1-1")
-      ui_check_input('edit_label','TAG4-1-1')
-      click_button 'Delete'
-      wait_for_ajax
-      expect(page).not_to have_content 'TAG4-1-1'
-    end
+    # #not implemented
+    # it "still (not) delete tags used by managed items (REQ-MDR-TAG-060) - WILL CURRENTLY FAIL", js: true do
+    #   load_test_file_into_triple_store("tag_test_data.ttl")
+    #   create_tag_form("TAGFORM", "Tag test form" )
+    #   add_tags("Forms", "TAGFORM", "TAG4-1-1")
+    #   click_navbar_tags
+    #   expect(page).to have_content 'Manage Tags'
+    #   ui_click_node_name ("TAG4-1-1")
+    #   ui_check_input('edit_label','TAG4-1-1')
+    #   click_button 'Delete'
+    #   wait_for_ajax
+    #   expect(page).not_to have_content 'TAG4-1-1'
+    # end
 
     it "update tag labels (REQ-MDR-TAG-110)", js: true do
       load_test_file_into_triple_store("tag_test_data.ttl")
@@ -191,153 +191,85 @@ describe "Tags", :type => :feature do
     end
 
      ### Add Tags to Content (MDR-TAG-15, MDR-TAG-50, MDR-TAG-70, MDR-TAG-100)
-
-    it "add tags to forms auto add child tags when created (REQ-MDR-15, REQ-MDR-TAG-050) - WILL CURRENTLY FAIL", js: true do
-      create_tag_first_level("Tag1", "Tag 1 level 1")
-      create_tag_form("TAGFORM", "Form for Tag Testing" )
-      add_tags("Forms", "TAGFORM", "Tag1")
-      wait_for_ajax
-      find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1")
-      #create child tag
-      create_tag_child("Tag1", "Tag1_1", "Tag 1.1 level 2")
-      ui_click_node_name ("Tag1")
-      ui_check_table_cell('iso_managed_table',1,1,'TAGFORM')
-      #1. check child tag is added to TAGFORM
-      ui_click_node_name ("Tag1_1")
-      #not implemented ui_check_table_cell('iso_managed_table',1,1,'TAGFORM')
-      #2. check child tag is added to TAGFORM
-      click_navbar_forms
-      expect(page).to have_content 'Index: Forms'
-      find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'History').click
-      find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'Update Tags').click
-      #not implemented find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1_1")
-    end
-
-     it "add tags to BCs auto add child tags when created (REQ-MDR-15, REQ-MDR-TAG-050) - WILL CURRENTLY FAIL", js: true do
-      create_tag_first_level("Tag1", "Tag 1 level 1")
-      create_tag_bc("TAGBC", "BC for Tag Testing", "Obs PQR")
-      add_tags("Biomedical Concepts", "TAGBC", "Tag1")
-      wait_for_ajax
-      find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1")
-      #create child tag
-      create_tag_child("Tag1", "Tag1_1", "Tag 1.1 level 2")
-      ui_click_node_name ("Tag1")
-      ui_check_table_cell('iso_managed_table',1,1,'TAGBC')
-      #1. check child tag is added to TAGBC
-      ui_click_node_name ("Tag1_1")
-      #not implemented ui_check_table_cell('iso_managed_table',1,1,'TAGBC')
-      #2. check child tag is added to TAGFORM
-      click_navbar_bc
-      expect(page).to have_content 'Index: Biomedical Concepts'
-      find(:xpath, "//tr[contains(.,'TAGBC')]/td/a", :text => 'History').click
-      find(:xpath, "//tr[contains(.,'TAGBC')]/td/a", :text => 'Update Tags').click
-      #not implemented find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1_1")
-    end
-
-     it "add tags to terminology auto add child tags when created (REQ-MDR-15, REQ-MDR-TAG-050) - WILL CURRENTLY FAIL", js: true do
-      create_tag_first_level("Tag1", "Tag 1 level 1")
-      create_tag_term("TAGTERM", "Terminology for Tag Testing")
-      add_tags_term("TAGTERM", "Tag1")
-
-      wait_for_ajax(120)
-      find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1")
-      #create child tag
-      create_tag_child("Tag1", "Tag1_1", "Tag 1.1 level 2")
-      ui_click_node_name ("Tag1")
-      ui_check_table_cell('iso_managed_table',1,1,'TAGTERM')
-#pause
-      #1. check child tag is added to TAGTERM
-      ui_click_node_name ("Tag1_1")
-      #not implemented ui_check_table_cell('iso_managed_table',1,1,'TAGTERM')
-      #2. check child tag is added to TAGTERM
-      # click_navbar_dashboard
-      click_navbar_terminology
-      expect(page).to have_content 'Index: Terminology'
-      find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'History').click
-#pause
-      #find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'Update Tags').click
-#pause
-      #not implemented find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1_1")
-    end
-
-     it "remove tags and child tags from forms (REQ-MDR-15) - WILL CURRENTLY FAIL", js:true do
-      create_tag_first_level("Tag1", "Tag 1 level 1")
-      create_tag_form("TAGFORM", "Form for Tag Testing" )
-      add_tags("Forms", "TAGFORM", "Tag1")
-      click_navbar_tags
-      wait_for_ajax
-      ui_click_node_name ("Tag1")
-      ui_check_table_cell("iso_managed_table", 1, 1, "TAGFORM")
-      click_navbar_forms
-      expect(page).to have_content 'Index: Forms'
-      find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'History').click
-      find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'Update Tags').click
-      wait_for_ajax
-      find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1").click
-      X = find(:xpath, "//div[@id='tags_container']", visible: false).text
-      expect(X).to have_content ""
-    end
-
-     it "remove tags and child tags from BCs (REQ-MDR-15) - WILL CURRENTLY FAIL", js:true do
-      create_tag_first_level("Tag1", "Tag 1 level 1")
-      create_tag_bc("TAGBC", "BC for Tag Testing", "Obs PQR")
-      add_tags("Biomedical Concepts", "TAGBC", "Tag1")
-      click_navbar_tags
-      wait_for_ajax
-      ui_click_node_name ("Tag1")
-      ui_check_table_cell("iso_managed_table", 1, 1, "TAGBC")
-      main_navbar_bc
-      expect(page).to have_content 'Index: Biomedical Concepts'
-      find(:xpath, "//tr[contains(.,'TAGBC')]/td/a", :text => 'History').click
-      find(:xpath, "//tr[contains(.,'TAGBC')]/td/a", :text => 'Update Tags').click
-      wait_for_ajax
-      find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1").click
-      X = find(:xpath, "//div[@id='tags_container']", visible: false).text
-      expect(X).to have_content ""
-    end
-
-     it "remove tags and child tags from terminology (REQ-MDR-15) - WILL CURRENTLY FAIL", js:true do
-      create_tag_first_level("Tag1", "Tag 1 level 1")
-      create_tag_term("TAGTERM", "Term for Tag Testing")
-      add_tags("Terminology", "TAGTERM", "Tag1")
-      click_navbar_tags
-      wait_for_ajax
-      ui_click_node_name ("Tag1")
-      ui_check_table_cell("iso_managed_table", 1, 1, "TAGTERM")
-      click_navbar_terminology
-      expect(page).to have_content 'Index: Terminology'
-      find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'History').click
-      find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'Update Tags').click
-      wait_for_ajax
-      find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1").click
-      X = find(:xpath, "//div[@id='tags_container']", visible: false).text
-      expect(X).to have_content ""
-    end
-
-     it "view a list of managed items being tagged to a selected tag when adding tags to a form (REQ-MDR-TAG-100) - WILL CURRENTLY FAIL - The value is not a string or an existing URI", js:true do
-      load_test_file_into_triple_store("tag_test_data.ttl")
-      create_tag_form("TAGFORM", "Form for Tag Test" )
-      create_tag_bc("TAGBC", "BC for Tag Test", "Obs PQR")
-      create_tag_term("TAGTERM", "Term for Tag Test")
-      add_tags("Form", "TAGFORM", "TAG1-1-1")
-      add_tags("Biomedical Concepts", "TAGBC", "TAG1-1-1")
-      add_tags("Biomedical Concepts", "TAGBC", "TAG2-1")
-      add_tags("Terminology", "TAGTERM", "TAG1-1-1")
-      click_navbar_terminology
-      expect(page).to have_content 'Index: Forms'
-      find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'History').click
-      find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'Update Tags').click
-      wait_for_ajax
-      ui_click_node_name ("TAG1-1-1")
-      wait_for_ajax
-      ui_check_table_cell("iso_managed_table", 1, 1, "TAGBC")
-      ui_check_table_cell("iso_managed_table", 2, 1, "TAGFORM")
-      ui_check_table_cell("iso_managed_table", 3, 1, "TAGTERM")
-      wait_for_ajax
-      ui_click_node_name ("TAG2-1")
-      wait_for_ajax
-      ui_check_table_cell("iso_managed_table", 1, 1, "TAGBC")
-    end
+    #
+    #  it "remove tags and child tags from forms (REQ-MDR-15) - WILL CURRENTLY FAIL", js:true do
+    #   create_tag_first_level("Tag1", "Tag 1 level 1")
+    #   create_tag_form("TAGFORM", "Form for Tag Testing" )
+    #   add_tags("Forms", "TAGFORM", "Tag1")
+    #   click_navbar_tags
+    #   wait_for_ajax
+    #   ui_click_node_name ("Tag1")
+    #   ui_check_table_cell("iso_managed_table", 1, 1, "TAGFORM")
+    #   click_navbar_forms
+    #   expect(page).to have_content 'Index: Forms'
+    #   find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'History').click
+    #   find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'Update Tags').click
+    #   wait_for_ajax
+    #   find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1").click
+    #   X = find(:xpath, "//div[@id='tags_container']", visible: false).text
+    #   expect(X).to have_content ""
+    # end
+    #
+    #  it "remove tags and child tags from BCs (REQ-MDR-15) - WILL CURRENTLY FAIL", js:true do
+    #   create_tag_first_level("Tag1", "Tag 1 level 1")
+    #   create_tag_bc("TAGBC", "BC for Tag Testing", "Obs PQR")
+    #   add_tags("Biomedical Concepts", "TAGBC", "Tag1")
+    #   click_navbar_tags
+    #   wait_for_ajax
+    #   ui_click_node_name ("Tag1")
+    #   ui_check_table_cell("iso_managed_table", 1, 1, "TAGBC")
+    #   main_navbar_bc
+    #   expect(page).to have_content 'Index: Biomedical Concepts'
+    #   find(:xpath, "//tr[contains(.,'TAGBC')]/td/a", :text => 'History').click
+    #   find(:xpath, "//tr[contains(.,'TAGBC')]/td/a", :text => 'Update Tags').click
+    #   wait_for_ajax
+    #   find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1").click
+    #   X = find(:xpath, "//div[@id='tags_container']", visible: false).text
+    #   expect(X).to have_content ""
+    # end
+    #
+    #  it "remove tags and child tags from terminology (REQ-MDR-15) - WILL CURRENTLY FAIL", js:true do
+    #   create_tag_first_level("Tag1", "Tag 1 level 1")
+    #   create_tag_term("TAGTERM", "Term for Tag Testing")
+    #   add_tags("Terminology", "TAGTERM", "Tag1")
+    #   click_navbar_tags
+    #   wait_for_ajax
+    #   ui_click_node_name ("Tag1")
+    #   ui_check_table_cell("iso_managed_table", 1, 1, "TAGTERM")
+    #   click_navbar_terminology
+    #   expect(page).to have_content 'Index: Terminology'
+    #   find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'History').click
+    #   find(:xpath, "//tr[contains(.,'TAGTERM')]/td/a", :text => 'Update Tags').click
+    #   wait_for_ajax
+    #   find(:xpath, "//div[@id='tags_container']/span", :text => "Tag1").click
+    #   X = find(:xpath, "//div[@id='tags_container']", visible: false).text
+    #   expect(X).to have_content ""
+    # end
+    #
+    #  it "view a list of managed items being tagged to a selected tag when adding tags to a form (REQ-MDR-TAG-100) - WILL CURRENTLY FAIL - The value is not a string or an existing URI", js:true do
+    #   load_test_file_into_triple_store("tag_test_data.ttl")
+    #   create_tag_form("TAGFORM", "Form for Tag Test" )
+    #   create_tag_bc("TAGBC", "BC for Tag Test", "Obs PQR")
+    #   create_tag_term("TAGTERM", "Term for Tag Test")
+    #   add_tags("Form", "TAGFORM", "TAG1-1-1")
+    #   add_tags("Biomedical Concepts", "TAGBC", "TAG1-1-1")
+    #   add_tags("Biomedical Concepts", "TAGBC", "TAG2-1")
+    #   add_tags("Terminology", "TAGTERM", "TAG1-1-1")
+    #   click_navbar_terminology
+    #   expect(page).to have_content 'Index: Forms'
+    #   find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'History').click
+    #   find(:xpath, "//tr[contains(.,'TAGFORM')]/td/a", :text => 'Update Tags').click
+    #   wait_for_ajax
+    #   ui_click_node_name ("TAG1-1-1")
+    #   wait_for_ajax
+    #   ui_check_table_cell("iso_managed_table", 1, 1, "TAGBC")
+    #   ui_check_table_cell("iso_managed_table", 2, 1, "TAGFORM")
+    #   ui_check_table_cell("iso_managed_table", 3, 1, "TAGTERM")
+    #   wait_for_ajax
+    #   ui_click_node_name ("TAG2-1")
+    #   wait_for_ajax
+    #   ui_check_table_cell("iso_managed_table", 1, 1, "TAGBC")
+    # end
 
     it "shortens tag label in Tag viewer if it is too many characters", js:true do
       click_navbar_tags
