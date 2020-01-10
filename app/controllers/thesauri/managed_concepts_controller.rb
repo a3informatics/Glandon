3d43acf3d601f604b03e898e01b6d8b3cefc7763
Samuel Banas <sab@s-cubed.dk>
2020-01-10 12:04:53 +0100

    GLAN-1141
    [1] Clean up of the Managed Concept show page
    [2] Changed the format of the Subset Index modal and table
    [3] Thesaurus select modal
    [4] General styling and code  improvements

--- app/controllers/thesauri/managed_concepts_controller.rb
+++ app/controllers/thesauri/managed_concepts_controller.rb
@@ -357,10 +357,11 @@ class Thesauri::ManagedConceptsController < ApplicationController
     subset_tcs = []
     if !subsets.nil?
       subsets.map{|x| x[:s].to_id}.each{|s|
-        mc = Thesaurus::ManagedConcept.find_with_properties(s)
-        t = Thesaurus.unique.select{|x| x[:scope_id] == mc.scope.id}
-        edit_path = edit_subset_thesauri_managed_concept_path(mc, source_mc: tc.id, context_id: params[:context_id])
-        subset_tcs << {:th_label => t[0][:label], :identifier => mc.identifier, :label => mc.label, :edit_path => edit_path}}
+        mc = Thesaurus::ManagedConcept.find_full(s)
+        subset_item = mc.to_h
+        subset_item[:edit_path] = edit_subset_thesauri_managed_concept_path(mc, source_mc: tc.id, context_id: params[:context_id])
+        subset_tcs << subset_item
+      }
     end
     render json: {data: subset_tcs}
   end
