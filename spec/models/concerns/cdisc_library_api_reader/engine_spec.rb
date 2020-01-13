3e4be0c4b22f4f30d10b5fe24cab9524ec42d8f3
Dave Iberson-Hurst <daveih1664@gmail.com>
2020-01-13 21:47:07 +0100

    GLAN-1120
    [1] Tweak test script

--- spec/models/concerns/cdisc_library_api_reader/engine_spec.rb
+++ spec/models/concerns/cdisc_library_api_reader/engine_spec.rb
@@ -43,7 +43,7 @@ describe CDISCLibraryAPIReader::Engine do
     object = CDISCLibraryAPIReader::Engine.new(parent) 
     object.process("/mdr/ct/packages/sendct-2019-09-27")
     result = object.parent_set.map{|k,v| v.to_h}
-    check_file_actual_expected(result, sub_dir, "process_expected_3.yaml", equate_method: :hash_equal, write_file: true)
+    check_file_actual_expected(result, sub_dir, "process_expected_3.yaml", equate_method: :hash_equal)
     check_file_actual_expected(result, sub_dir, "process_check_3.yaml", equate_method: :hash_equal) # Results file from equivalent Excel import.
   end
 
