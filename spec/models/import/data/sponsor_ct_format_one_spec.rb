d4a3f820f85505f40b0a1244f9ace180b8cf53c0
Dave Iberson-Hurst <daveih1664@gmail.com>
2020-02-24 16:26:32 +0100

    GLAN-1212
    [1] Study and V3 initial runs

--- spec/models/import/data/sponsor_ct_format_one_spec.rb
+++ spec/models/import/data/sponsor_ct_format_one_spec.rb
@@ -66,6 +66,18 @@ describe "Import::SponsorTermFormatOne" do
     result
   end
   
+  def cl_list(th)
+    query_string = %Q{
+      SELECT ?cid WHERE 
+      {
+        #{th.uri.to_ref} th:isTopConceptReference/bo:reference ?s .
+        ?s th:identifier ?cid .
+      }
+    }
+    query_results = Sparql::Query.new.query(query_string, "", [:th, :bo]) 
+    query_results.by_object_set([:cid]).map{|x| x[:cid]}
+  end
+
   def cl_info(th, identifier)
     query_string = %Q{
       SELECT ?n (COUNT(?cli) as ?count) WHERE 
@@ -97,7 +109,6 @@ describe "Import::SponsorTermFormatOne" do
     }
     query_results = Sparql::Query.new.query(query_string, "", [:th, :bo]) 
     result = query_results.by_object_set([:c]).map{|x| {uri: x[:c]}}
-byebug
     result
   end
 
@@ -142,6 +153,37 @@ byebug
     check_cl(th, "SN000916", "DLQI12R", 5)
     check_cl(th, "SN001061", "NCIDICTV", 4)
     cl_future(th)
+  end
+
+  it "import version 2.6 study" do
+    ct = Thesaurus.find_minimum(Uri.new(uri: "http://www.cdisc.org/CT/V43#TH"))
+    full_path = db_load_file_path("sponsor_one/ct", "study_v2-6_CDISC_v43.xlsx")
+    fixes = db_load_file_path("sponsor_one/ct", "fixes_study_v2-6.yaml")
+    params = {identifier: "STUDY", version: "1", date: "2020-01-01", files: [full_path], fixes: fixes, version_label: "1.0.0", label: "Version 2-6, Q3 2019", semantic_version: "1.0.0", job: @job, uri: ct.uri}
+    result = @object.import(params)
+    filename = "sponsor_term_format_one_#{@object.id}_errors.yml"
+    #expect(public_file_does_not_exist?("test", filename)).to eq(true)
+    actual = read_public_yaml_file("test", filename)
+  copy_file_from_public_files_rename("test", filename, sub_dir, "import_errors_expected_study_2-6.yaml")
+    check_file_actual_expected(actual, sub_dir, "import_errors_expected_study_2-6.yaml", equate_method: :hash_equal)
+    #copy_file_from_public_files("test", filename, sub_dir)
+    filename = "sponsor_term_format_one_#{@object.id}_load.ttl"
+    #expect(public_file_exists?("test", filename)).to eq(true)
+    copy_file_from_public_files("test", filename, sub_dir)
+  copy_file_from_public_files_rename("test", filename, sub_dir, "import_expected_study_2-6.ttl")
+    check_ttl_fix_v2(filename, "import_expected_study_2-6.ttl", {last_change_date: true})
+    expect(@job.status).to eq("Complete")
+    delete_data_file(sub_dir, filename)
+  end
+ 
+  it "Check 2.6 & 2.6 Study Overlap", :speed => 'slow' do
+    load_local_file_into_triple_store(sub_dir, "import_expected_2-6.ttl")
+    load_local_file_into_triple_store(sub_dir, "import_expected_study_2-6.ttl")
+    uri_1 = Uri.new(uri: "http://www.s-cubed.dk/Q3_2019/V1#TH")
+    uri_2 = Uri.new(uri: "http://www.s-cubed.dk/STUDY/V1#TH")
+    th_26 = Thesaurus.find_minimum(uri_1)
+    th_study = Thesaurus.find_minimum(uri_2)
+    puts colourize("#{cl_list(th_26) & cl_list(th_study)}", "blue") 
   end
 
   it "import version 3.0", :speed => 'slow' do
