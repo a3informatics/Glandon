e072db8dbc0a6ce76adf127a4f74b805b4a48f54
Clarisa <car@s-cubed.dk>
2020-02-26 12:35:33 +0100

    GLAN-reports
    [1] Add report Submission Impact
    [2] Add test

--- spec/models/thesaurus/data_spec.rb
+++ spec/models/thesaurus/data_spec.rb
@@ -294,4 +294,78 @@ describe Thesaurus::ManagedConcept do
 
   end
 
+  describe "for ad hoc reports" do
+
+    def simple_thesaurus_1
+      ct = Thesaurus::ManagedConcept.find_minimum(Uri.new(uri: "http://www.cdisc.org/CT/V50#TH"))
+      @ra = IsoRegistrationAuthority.find_children(Uri.new(uri: "http://www.assero.co.uk/RA#DUNS123456789"))
+      @th_1 = Thesaurus.new
+      @th_1.label = "Test Terminology"
+      @tc_1 = Thesaurus::ManagedConcept.from_h({
+          label: "London Heathrow",
+          identifier: "A00001",
+          definition: "A definition",
+          notation: "ONCRSR"
+        })
+      @tc_1.synonym << Thesaurus::Synonym.new(label:"Heathrow")
+      @tc_1.synonym << Thesaurus::Synonym.new(label:"LHR")
+      @tc_1.preferred_term = Thesaurus::PreferredTerm.new(label:"London Heathrow")
+      @tc_1a = Thesaurus::UnmanagedConcept.from_h({
+          label: "Terminal 5",
+          identifier: "A000011",
+          definition: "The 5th LHR Terminal",
+          notation: "Organ to Heart Weight Ratio"
+        })
+      @tc_1a.synonym << Thesaurus::Synonym.new(label:"T5")
+      @tc_1a.synonym << Thesaurus::Synonym.new(label:"Terminal Five")
+      @tc_1a.synonym << Thesaurus::Synonym.new(label:"BA Terminal")
+      @tc_1a.synonym << Thesaurus::Synonym.new(label:"British Airways Terminal")
+      @tc_1a.preferred_term = Thesaurus::PreferredTerm.new(label:"Terminal 5")
+      @tc_1b = Thesaurus::UnmanagedConcept.from_h({
+          label: "Terminal 1",
+          identifier: "A000012",
+          definition: "The oldest LHR Terminal",
+          notation: "T1"
+        })
+      @tc_1b.preferred_term = Thesaurus::PreferredTerm.new(label:"Terminal 1")
+      @tc_1.narrower << @tc_1a
+      @tc_1.narrower << @tc_1b
+      @tc_1.set_initial("A00001")
+      @tc_2 = Thesaurus::ManagedConcept.new
+      @tc_2.identifier = "A00002"
+      @tc_2.definition = "Copenhagen"
+      @tc_2.extensible = false
+      @tc_2.notation = "CPH"
+      @tc_2.set_initial("A00002")
+      @th_1.is_top_concept_reference << OperationalReferenceV3::TmcReference.from_h({reference: @tc_1.uri, local_label: "", enabled: true, ordinal: 1, optional: true})
+      @th_1.is_top_concept_reference << OperationalReferenceV3::TmcReference.from_h({reference: @tc_2.uri, local_label: "", enabled: true, ordinal: 2, optional: true})
+      @th_1.is_top_concept << @tc_1.uri
+      @th_1.is_top_concept << @tc_2.uri
+      @th_1.reference = OperationalReferenceV3.new(reference: ct)
+      @th_1.set_initial("AIRPORTS")
+    end
+
+    before :all  do
+      IsoHelpers.clear_cache
+    end
+
+    before :each do
+      data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl"]
+      load_files(schema_files, data_files)
+      load_cdisc_term_versions(1..62)
+    end
+
+    it "file" do
+      simple_thesaurus_1
+      sparql = Sparql::Update.new
+      sparql.default_namespace(@th_1.uri.namespace)
+      @th_1.to_sparql(sparql, true)
+      @tc_1.to_sparql(sparql, true)
+      @tc_2.to_sparql(sparql, true)
+      full_path = sparql.to_file
+    copy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "thesaurus_airport.ttl")
+    end 
+
+  end
+
 end
