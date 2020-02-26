7d3d3b5d982dd6b8d33fc593c3f6d0625484e5fd
Samuel Banas <sab@s-cubed.dk>
2020-02-26 10:04:53 +0100

    GLAN-1217
    [1] pause

--- spec/features/scenarios/scenario_9_js_spec.rb
+++ spec/features/scenarios/scenario_9_js_spec.rb
@@ -328,7 +328,7 @@ describe "Scenario 9 - Terminology Release, Clone, Impact and Upgrade", :type =>
       context_menu_element_header :upgrade
       wait_for_ajax 20
       expect(page).to have_content("Upgrade Code Lists CLONE v0.1.0")
-      pause
+      # pause
 
       click_row_contains("changes_cdisc_table", "EPOCH")
       wait_for_ajax 20
@@ -446,7 +446,7 @@ describe "Scenario 9 - Terminology Release, Clone, Impact and Upgrade", :type =>
       # Upgrade Terms
       context_menu_element_header :upgrade
       wait_for_ajax 20
-      pause
+      # pause
 
       ua_logoff
     end
