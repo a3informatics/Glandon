d32c9e7aeddda262e5edf3a3a14e5341621b77c4
Dave Iberson-Hurst <daveih1664@gmail.com>
2020-03-03 20:17:23 +0100

    GLAN-1250
    [1] Updates
    [2] More QC checks

--- spec/models/import/data/sponsor_ct_format_one_spec.rb
+++ spec/models/import/data/sponsor_ct_format_one_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'csv'
 
 describe "Import::SponsorTermFormatOne" do
 	
@@ -42,7 +43,7 @@ describe "Import::SponsorTermFormatOne" do
 
   def count_cl(th)
     query_string = %Q{
-      SELECT (COUNT(?s) as ?count) WHERE 
+      SELECT (COUNT(DISTINCT ?s) as ?count) WHERE 
       {
         #{th.uri.to_ref} th:isTopConceptReference/bo:reference ?s
       }
@@ -66,6 +67,19 @@ describe "Import::SponsorTermFormatOne" do
     result
   end
   
+  def count_distinct_cli(th)
+    query_string = %Q{
+      SELECT (COUNT(DISTINCT ?s) as ?count) WHERE 
+      {
+        #{th.uri.to_ref} th:isTopConceptReference/bo:reference/th:narrower ?s
+      }
+    }
+    query_results = Sparql::Query.new.query(query_string, "", [:th, :bo]) 
+    result = query_results.by_object(:count).first.to_i
+    puts colourize("Total Distinct CLI count=#{result}", "blue")
+    result
+  end
+  
   def cl_list(th)
     query_string = %Q{
       SELECT ?cid WHERE 
@@ -78,46 +92,98 @@ describe "Import::SponsorTermFormatOne" do
     query_results.by_object_set([:cid]).map{|x| x[:cid]}
   end
 
-  def cl_info(th, identifier)
+  def cl_info(th, key)
     query_string = %Q{
       SELECT ?n (COUNT(?cli) as ?count) WHERE 
       {
         #{th.uri.to_ref} th:isTopConceptReference/bo:reference ?s .
-        ?s th:identifier "#{identifier}" .
+        ?s isoC:label "#{key}" .
         ?s th:notation ?n .
         ?s th:narrower ?cli .
       } GROUP BY ?n
     }
-    query_results = Sparql::Query.new.query(query_string, "", [:th, :bo]) 
+    query_results = Sparql::Query.new.query(query_string, "", [:th, :bo, :isoC]) 
     result = query_results.by_object_set([:n, :count]).map{|x| {notation: x[:n], count: x[:count]}}
-    puts colourize("CL #{identifier}=#{result.first}", "blue")
     result.first
   end
 
-  def cl_future(th)
+  def cl_items(th, key)
     query_string = %Q{
-      SELECT ?c WHERE 
+      SELECT DISTINCT ?i WHERE 
       {
-        #{th.uri.to_ref} th:isTopConceptReference/bo:reference ?p .
-        ?p th:identifier ?pid .
-        ?p th:notation ?n .
-        ?p th:narrower ?c .
-        ?c th:identifier ?cid .
-        #{th.uri.to_ref} th:reference ?rth .
-        NOT EXIST (?rth th:isTopConceptReference/bo:reference/th:narrower ?c)
+        #{th.uri.to_ref} th:isTopConceptReference/bo:reference ?s .
+        ?s isoC:label "#{key}" .
+        ?s th:narrower ?cli .
+        ?cli th:identifier ?i
       }
     }
-    query_results = Sparql::Query.new.query(query_string, "", [:th, :bo]) 
-    result = query_results.by_object_set([:c]).map{|x| {uri: x[:c]}}
-    result
+    query_results = Sparql::Query.new.query(query_string, "", [:th, :bo, :isoC]) 
+    query_results.by_object_set([:i]).map{|x| x[:i]}
   end
 
-  def check_cl(th, identifier, notation, count)
-    puts colourize("Checking CL #{identifier}", "blue")
-    result = cl_info(th, identifier)
-    expect(result[:notation]).to eq(notation)
-    expect(result[:count]).to eq("#{count}")
-    puts colourize("OK", "blue")
+  def check_cl(th, long_name, identifier, notation, count, items)
+    result = cl_info(th, long_name)
+    if result.nil?
+      puts colourize("#{long_name} : #{identifier}", "red")
+      puts colourize("Notation: [<not found>, #{notation}] Count: [<not found>, #{count}]\n", "red")
+    elsif result[:notation] == notation && "#{result[:count]}".to_i == "#{count}".to_i
+      #puts colourize("#{long_name} : #{identifier}", "green")
+    else
+      db_items = cl_items(th, long_name)
+      puts colourize("#{long_name} : #{identifier}", "red")
+      puts colourize("Notation: [#{result[:notation]}, #{notation}] Count: [#{result[:count]}, #{count}]", "red")
+      puts colourize("Missing:  #{items - db_items}", "red")
+      puts colourize("Extra:    #{db_items - items}\n", "red")
+    end
+  end
+
+  class CodeListInfo
+    
+    @name =""
+    @identifier =""
+    @short_name
+    @items = nil
+
+    def initialize(name, short_name, identifier)
+      @name = name
+      @short_name = short_name
+      @identifier = identifier
+      @items = []
+    end
+
+    def add(identifier)
+      @items << identifier
+    end
+
+    def to_h
+      {name: @name, short_name: @short_name, identifier: @identifier, items: @items}
+    end
+
+  end
+
+  def cl_target(filename)
+    cls = []
+    item = nil
+    full_path = set_path(sub_dir, filename)
+    results = CSV.read(full_path)
+    code_list_name = ""
+    results[1..results.count-1].each do |result|
+      if result[0] != code_list_name
+        item = CodeListInfo.new(remove_unicode_chars(result[0]), result[1], result[2]) 
+        cls << item
+      end
+      item.add(result[3])
+      code_list_name = result[0]
+    end
+    cls.map{|x| x.to_h}
+  end
+
+  def remove_unicode_chars(text)
+    text = text.gsub(/[\u2013]/, "-")
+    text = text.gsub(/[\u003E]/, ">")
+    text = text.gsub(/[\u003C]/, "<")
+    text = text.gsub(/[\u2018\u2019\u0092]/, "'")
+    text.gsub(/[\u201C\u201D]/, '"')
   end
 
   it "import version 2.6, global & study", :speed => 'slow'  do
@@ -142,17 +208,21 @@ describe "Import::SponsorTermFormatOne" do
   end
  
   it "Import 2.6 Results", :speed => 'slow' do
+    actual = cl_target("import_results_raw_2-6.csv")
+    check_file_actual_expected(actual, sub_dir, "import_results_expected_2-6.yaml", equate_method: :hash_equal)
+  end
+
+  it "Import 2.6 Results", :speed => 'slow' do
     load_local_file_into_triple_store(sub_dir, "import_expected_2-6.ttl")
     uri = Uri.new(uri: "http://www.s-cubed.dk/Q3_2019/V1#TH")
     th = Thesaurus.find_minimum(uri)
-    expect(count_cli(th)).to eq(33185)
-    expect(count_cl(th)).to eq(1725)
-    check_cl(th, "SN000730", "ACNC", 8)
-    check_cl(th, "SN000061", "CDTSTAGE", 17)
-    check_cl(th, "SC132314", "CLMETH", 7)
-    check_cl(th, "SN000916", "DLQI12R", 5)
-    check_cl(th, "SN001061", "NCIDICTV", 4)
-    #cl_future(th)
+    #expect(count_cl(th)).to eq(1727)
+    #expect(count_cli(th)).to eq(33187)
+    #expect(count_distinct_cli(th)).to eq(33185)
+    results = read_yaml_file(sub_dir, "import_results_expected_2-6.yaml")
+    results.each do |x|
+      check_cl(th, x[:name], x[:identifier], x[:short_name], x[:items].count, x[:items])
+    end
   end
 
   it "import version 3.0", :speed => 'slow' do
