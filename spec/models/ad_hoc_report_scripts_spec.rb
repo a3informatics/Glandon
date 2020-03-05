8328c4ec6c18d723d0430572907cbd31c046fce1
Clarisa <car@s-cubed.dk>
2020-03-05 15:30:40 +0100

    GLAN-1268
    [1] Add test to ensure Submission impact report is working (Test failing: Timeout error)
    [2] Add new helper to copy a report to public test folder
    [3] Update Submission Impact Sparql

---
+++ spec/models/ad_hoc_report_scripts_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe AdHocReport, type: :model do
+  
+  include DataHelpers
+  include PublicFileHelpers
+
+	def sub_dir
+    return "models/ad_hoc_report"
+  end
+
+  before :all do
+    data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl", "thesaurus_airport_ad_hoc.ttl"]
+    load_files(schema_files, data_files)
+    load_cdisc_term_versions(1..62)    
+    AdHocReport.delete_all
+    delete_all_public_files
+  end
+
+  after :all do
+    delete_all_public_files
+  end
+
+  it "executes a submission impact report" do
+    copy_report_to_public_files("submission_impact_sparql.yaml", "test")
+    job = Background.create
+    report = AdHocReport.new
+    report.background_id = job.id
+    report.sparql_file = "submission_impact_sparql.yaml"
+    report.results_file = "submission_impact_results_1.yaml"
+    job.start("Rspec test", "Starting...") {report.execute([Uri.new(uri: "http://www.acme-pharma.com/AIRPORTS/V1#TH").to_id])}
+    results = AdHocReportFiles.read("submission_impact_results_1.yaml")
+    check_file_actual_expected(results, sub_dir, "submission_impact_expected_1.yaml")
+  end
+
+  # it "executes a ct references inconsistencies report" do
+  #   copy_report_to_public_files("ct_ref_inconsistencies_sparql.yaml", "test")
+  #   job = Background.create
+  #   report = AdHocReport.new
+  #   report.background_id = job.id
+  #   report.sparql_file = "ct_ref_inconsistencies_sparql.yaml"
+  #   report.results_file = "ct_ref_inconsistencies_results_1.yaml"
+  #   job.start("Rspec test", "Starting...") {report.execute([Uri.new(uri: "http://www.acme-pharma.com/AIRPORTS/V1#TH").to_id])}
+  #   results = AdHocReportFiles.read("ct_ref_inconsistencies_results_1.yaml")
+  #   check_file_actual_expected(results, sub_dir, "ct_ref_inconsistencies_expected_1.yaml")
+  # end
+
+  # it "executes a missing tags report" do
+  #   copy_report_to_public_files("submission_impact_sparql.yaml", "test")
+  #   job = Background.create
+  #   report = AdHocReport.new
+  #   report.background_id = job.id
+  #   report.sparql_file = "submission_impact_sparql.yaml"
+  #   report.results_file = "submission_impact_results_1.yaml"
+  #   job.start("Rspec test", "Starting...") {report.execute([Uri.new(uri: "http://www.acme-pharma.com/AIRPORTS/V1#TH").to_id])}
+  #   results = AdHocReportFiles.read("submission_impact_results_1.yaml")
+  #   check_file_actual_expected(results, sub_dir, "submission_impact_expected_1.yaml")
+  # end
+  
+end
