9daee0f4534206c98a652c92fcfa96bc87c33d10
Clarisa <car@s-cubed.dk>
2020-03-06 08:53:09 +0100

    GLAN-1268
    [1] Add test: CT Ref inconsistencies
    [2] Add yaml file with expected results.

--- spec/models/ad_hoc_report_scripts_spec.rb
+++ spec/models/ad_hoc_report_scripts_spec.rb
@@ -33,28 +33,28 @@ RSpec.describe AdHocReport, type: :model do
     check_file_actual_expected(results, sub_dir, "submission_impact_expected_1.yaml")
   end
 
-  # it "executes a ct references inconsistencies report" do
-  #   copy_report_to_public_files("ct_ref_inconsistencies_sparql.yaml", "test")
-  #   job = Background.create
-  #   report = AdHocReport.new
-  #   report.background_id = job.id
-  #   report.sparql_file = "ct_ref_inconsistencies_sparql.yaml"
-  #   report.results_file = "ct_ref_inconsistencies_results_1.yaml"
-  #   job.start("Rspec test", "Starting...") {report.execute([Uri.new(uri: "http://www.acme-pharma.com/AIRPORTS/V1#TH").to_id])}
-  #   results = AdHocReportFiles.read("ct_ref_inconsistencies_results_1.yaml")
-  #   check_file_actual_expected(results, sub_dir, "ct_ref_inconsistencies_expected_1.yaml")
-  # end
+  it "executes a ct references inconsistencies report" do
+    copy_report_to_public_files("ct_references_inconsistencies_sparql.yaml", "test")
+    job = Background.create
+    report = AdHocReport.new
+    report.background_id = job.id
+    report.sparql_file = "ct_references_inconsistencies_sparql.yaml"
+    report.results_file = "ct_references_inconsistencies_results_1.yaml"
+    job.start("Rspec test", "Starting...") {report.execute([Uri.new(uri: "http://www.acme-pharma.com/AIRPORTS/V1#TH").to_id])}
+    results = AdHocReportFiles.read("ct_references_inconsistencies_results_1.yaml")
+    check_file_actual_expected(results, sub_dir, "ct_references_inconsistencies_expected_1.yaml")
+  end
 
-  # it "executes a missing tags report" do
-  #   copy_report_to_public_files("submission_impact_sparql.yaml", "test")
-  #   job = Background.create
-  #   report = AdHocReport.new
-  #   report.background_id = job.id
-  #   report.sparql_file = "submission_impact_sparql.yaml"
-  #   report.results_file = "submission_impact_results_1.yaml"
-  #   job.start("Rspec test", "Starting...") {report.execute([Uri.new(uri: "http://www.acme-pharma.com/AIRPORTS/V1#TH").to_id])}
-  #   results = AdHocReportFiles.read("submission_impact_results_1.yaml")
-  #   check_file_actual_expected(results, sub_dir, "submission_impact_expected_1.yaml")
-  # end
+  it "executes a missing tags report" do
+    copy_report_to_public_files("missing_tags_sparql.yaml", "test")
+    job = Background.create
+    report = AdHocReport.new
+    report.background_id = job.id
+    report.sparql_file = "missing_tags_sparql.yaml"
+    report.results_file = "missing_tags_results_1.yaml"
+    job.start("Rspec test", "Starting...") {report.execute([Uri.new(uri: "http://www.acme-pharma.com/AIRPORTS/V1#TH").to_id])}
+    results = AdHocReportFiles.read("missing_tags_results_1.yaml")
+    check_file_actual_expected(results, sub_dir, "missing_tags_expected_1.yaml")
+  end
   
 end
