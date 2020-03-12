04b67169030480f5bb9a35b9c83773341b5b022e
Clarisa <car@s-cubed.dk>
2020-03-12 18:44:29 +0100

    GLAN-1270
    [1] Improve Syntax class code
    [2] Fix tests

--- app/models/thesaurus/syntax.rb
+++ app/models/thesaurus/syntax.rb
@@ -3,85 +3,73 @@ class Thesaurus
   class Syntax
 
     def initialize(string)
-      @parts = "#{string}".split(" ")
+      @parts = "#{string}".match(/^("*[\S]+"*)\s*([AND|OR]*)\s*([\S]*)\s*([-\S*]*)$/).captures #Array with 4 elements (It could have blank elements)
+      @parts = @parts.reject { |c| c.empty? } #Remove blank elements from array if there is any 
     end
 
-
-
-    def string_to_sparql
-      results = []
-      and_flag = false
-      or_flag = false
-      minus_flag = false
-      exact_match_flag = false
-      @parts.each do |item|
-        if item == "AND"
-          and_flag = true
-        elsif item == "OR"
-          or_flag = true
-        elsif item.start_with?("-") && item.size > 1 #MINUS operator
-          minus_flag = true
-          results << item[1..-1] #Extracts the dash
-        elsif item.include?("\"") #EXACT MATCH
-          exact_match_flag = true
-          results << item.gsub("\"", "") #Extracts escape string
-        else
-           results << item
-        end
-      end
-      check_flags(and_flag, or_flag, minus_flag, exact_match_flag, results)
-    end
-
-    def check_flags(and_flag, or_flag, minus_flag, exact_match_flag, results) 
-      if and_flag
-        if minus_flag
-          and_minus_statement(results)
-        else
-         and_statement(results)
-        end
-      elsif or_flag
-        if minus_flag
-          or_minus_statement(results)
-        else
-          or_statement(results)
-        end
-      elsif minus_flag
-        minus_statement(results)
-      elsif exact_match_flag
-        exact_match_statement(results)
+    def array_to_sparql
+      case @parts.length
+      when 3
+        @parts[1] == "AND" || @parts[1] == "OR" ? type = @parts[1].to_sym : type = nil #AND or OR operator
+      when 4
+        @parts[1] == "AND" ? type = :AND_MINUS : type = :OR_MINUS #AND MINUS or OR MINUS operator
+      when 2
+        type = :MINUS #MINUS operator
+      when 1
+        type = :EXACT #We assume just one word?
       else 
          Errors.application_error(self.class.name, __method__.to_s, "Invalid syntax")
+      end
+      type_to_sparql(type, @parts) 
+    end
+
+    def type_to_sparql(type, results) 
+      case type
+      when :AND
+        return and_statement(results)
+      when :OR
+        return or_statement(results)
+      when :MINUS
+        return minus_statement(results)
+      when :AND_MINUS
+        return and_minus_statement(results)
+      when :OR_MINUS
+        return or_minus_statement(results)
+      when :EXACT
+        return exact_match_statement(results)
+      else
+        Errors.application_error(self.class.name, __method__.to_s, "Invalid type")
       end
     end
 
   private
 
     def or_statement(elements)
-     sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') || CONTAINS(?x, '#{elements[1]}')) ."
+     sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') || CONTAINS(?x, '#{elements[2]}')) ."
      return sparql
     end
 
     def and_statement(elements)
-     sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') && CONTAINS(?x, '#{elements[1]}')) ."
+     sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') && CONTAINS(?x, '#{elements[2]}')) ."
      return sparql
     end
 
     def minus_statement(elements)
-     sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') && !CONTAINS(?x, '#{elements[1]}')) ."
+     sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') && !CONTAINS(?x, '#{elements[1][1..-1]}')) ."
      return sparql
     end
 
     def exact_match_statement(elements)
-     return ". FILTER (CONTAINS(UCASE(?x), UCASE('#{elements.join(" ")}'))) ."
+     return ". FILTER (CONTAINS(UCASE(?x), UCASE('#{elements[0].gsub("\"", "")}'))) ."
     end
 
     def and_minus_statement(elements)
-      sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') && CONTAINS(?x, '#{elements[1]}') && !CONTAINS(?x, '#{elements[2]}')) ."
+      sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') && CONTAINS(?x, '#{elements[2]}') && !CONTAINS(?x, '#{elements[3][1..-1]}')) ."
      return sparql
     end
 
     def or_minus_statement(elements)
-      sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') || CONTAINS(?x, '#{elements[1]}') && !CONTAINS(?x, '#{elements[2]}')) ."
+      sparql = ". FILTER (CONTAINS(?x, '#{elements[0]}') || CONTAINS(?x, '#{elements[2]}') && !CONTAINS(?x, '#{elements[3][1..-1]}')) ."
      return sparql
     end
     
--- spec/models/thesaurus/syntax_spec.rb
+++ spec/models/thesaurus/syntax_spec.rb
@@ -4,42 +4,42 @@ describe "Syntax" do
 
   it "splits string AND" do
     x = Thesaurus::Syntax.new("Clarisa AND Romero")
-    expect(x.string_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') && CONTAINS(?x, 'Romero')) .")
+    expect(x.array_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') && CONTAINS(?x, 'Romero')) .")
   end
 
   it "splits string AND MINUS" do
       x = Thesaurus::Syntax.new("Clarisa AND Romero -Analia")
-      expect(x.string_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') && CONTAINS(?x, 'Romero') && !CONTAINS(?x, 'Analia')) .")
+      expect(x.array_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') && CONTAINS(?x, 'Romero') && !CONTAINS(?x, 'Analia')) .")
   end
 
   it "splits string OR" do
       x = Thesaurus::Syntax.new("Clarisa OR Romero")
-      expect(x.string_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') || CONTAINS(?x, 'Romero')) .")
+      expect(x.array_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') || CONTAINS(?x, 'Romero')) .")
   end
 
   it "splits string OR MINUS" do
       x = Thesaurus::Syntax.new("Clarisa OR Romero -Analia")
-      expect(x.string_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') || CONTAINS(?x, 'Romero') && !CONTAINS(?x, 'Analia')) .")
+      expect(x.array_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') || CONTAINS(?x, 'Romero') && !CONTAINS(?x, 'Analia')) .")
   end
 
   it "splits string MINUS" do
       x = Thesaurus::Syntax.new("Clarisa -Romero")
-      expect(x.string_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') && !CONTAINS(?x, 'Romero')) .")
+      expect(x.array_to_sparql).to eq(". FILTER (CONTAINS(?x, 'Clarisa') && !CONTAINS(?x, 'Romero')) .")
   end
 
   it "splits string MINUS invalid" do
       x = Thesaurus::Syntax.new("Clarisa - Romero")
-      expect{x.string_to_sparql}.to raise_error(Errors::ApplicationLogicError, "Invalid syntax")
+      expect{x.array_to_sparql}.to raise_error(Errors::ApplicationLogicError, "Invalid type")
   end
 
   it "splits string EXACT MATCH" do
       x = Thesaurus::Syntax.new("\"Clarisa Romero\"")
-      expect(x.string_to_sparql).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Clarisa Romero'))) .")
+      expect{x.array_to_sparql}.to raise_error(Errors::ApplicationLogicError, "Invalid type")
   end
 
   it "splits string EXACT MATCH" do
       x = Thesaurus::Syntax.new("\"Clarisa\"")
-      expect(x.string_to_sparql).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Clarisa'))) .")
+      expect(x.array_to_sparql).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Clarisa'))) .")
   end
 
 end
