9a787e6df67039e8c1aa8ef73dea58b7424f497e
Clarisa <car@s-cubed.dk>
2020-03-15 12:37:38 +0100

    GLAN-1270
    [1] Add Th Identifier and Th version
    [2] Rewrite test files

--- app/models/thesaurus/search.rb
+++ app/models/thesaurus/search.rb
@@ -58,11 +58,11 @@ class Thesaurus
       #    :items which is an array of results.
       def search_multiple(params, uris)
         results = []
-        query_results = Sparql::Query.new.query(search_query_string(params, uris), "", [:bo, :th, :isoC])
-        triples = query_results.by_object_set([:pi, :i, :n, :d, :pt, :sys, :uri])
+        query_results = Sparql::Query.new.query(search_query_string(params, uris), "", [:bo, :th, :isoC, :isoT, :isoI])
+        triples = query_results.by_object_set([:pi, :i, :n, :d, :pt, :sys, :uri, :thi, :thv])
         triples.each do |t|
           results << {id: t[:uri].to_id, uri: t[:uri].to_s, parent_identifier: t[:pi], parent_label: t[:pl], identifier: t[:i], notation: t[:n], 
-            definition: t[:d], preferred_term: t[:pt], synonym: t[:sys], tags: t[:gt]}
+            definition: t[:d], preferred_term: t[:pt], synonym: t[:sys], tags: t[:gt], thesaurus_identifier: t[:thi], thesaurus_version: t[:thv]}
         end
         return { count: search_count(params, uris), items: results }
       end
@@ -72,7 +72,7 @@ class Thesaurus
       # Search resut count
       def search_count(params, uris)
         results = []
-        query_results = Sparql::Query.new.query(search_query_string(params, uris, false), "", [:bo, :th, :isoC])
+        query_results = Sparql::Query.new.query(search_query_string(params, uris, false), "", [:bo, :th, :isoC, :isoT, :isoI])
         query_results.results.count
       end
 
@@ -90,28 +90,27 @@ class Thesaurus
         columns.each do |column|
           next if column[1][:search][:value].blank?
           if column[0] == "7"
-            search_clauses += "  FILTER(UCASE(#{get_order_variable(column[0])}) IN (  #{get_tags_filter(column[1][:search][:value])}  )  ) .\n" #Tags
+            search_clauses += "  FILTER(UCASE(#{get_order_variable(column[0])}) IN (  #{get_tags_filter(column[1][:search][:value])}  )  ) .\n" #Tags filter
           else
             column_filter = Thesaurus::Syntax.new("#{column[1][:search][:value]}")
-            #search_clauses += "  FILTER regex(#{get_order_variable(column[0])}, \"#{column[1][:search][:value]}\", 'i') .\n" 
             search_clauses += column_filter.array_to_sparql(get_order_variable(column[0]))
-            #search_clauses += "  FILTER regex(#{get_order_variable(column[0])}, \"#{column[1][:search][:value]}\", 'i') .\n" 
           end
         end
         if !search[:value].blank?
           overall_filter = Thesaurus::Syntax.new("#{search[:value]}")
-          search_clauses += "  ?uri (th:identifier|th:notation|th:preferredTerm/isoC:label|th:synonym/isoC:label|th:definition|isoC:tagged/isoC:prefLabel) ?x " 
-          search_clauses += overall_filter.array_to_sparql("?x")
+          search_clauses += "  ?uri (th:identifier|th:notation|th:preferredTerm/isoC:label|th:synonym/isoC:label|th:definition|isoC:tagged/isoC:prefLabel) ?x "
+          search_clauses += overall_filter.array_to_sparql("?x") #Advanced overall search
         end
         #search_clauses += "  ?uri (th:identifier|th:notation|th:preferredTerm/isoC:label|th:synonym/isoC:label|th:definition|isoC:tagged/isoC:prefLabel) ?x . FILTER regex(?x, \"" + search[:value] + "\", 'i') . \n" if !search[:value].blank?
-
         # Main SPARQL
         main_part = %Q{
           {
-            SELECT DISTINCT ?pi ?pl ?i ?n ?d ?pt ?uri ?t ?sy
+            SELECT DISTINCT ?pi ?pl ?i ?n ?d ?pt ?uri ?thi ?thv ?t ?sy
             {
               VALUES ?e { #{uris.map{|x| x.to_ref}.join(" ")} }
               ?e th:isTopConceptReference/bo:reference ?mc .
+              ?e isoT:hasIdentifier/isoI:identifier ?thi .
+              ?e isoT:hasIdentifier/isoI:versionLabel ?thv .
               {
                 {
                   ?mc th:narrower+ ?uc .
@@ -143,11 +142,11 @@ class Thesaurus
         }
         tag_grp = %Q{(GROUP_CONCAT(DISTINCT ?t;separator=\"#{IsoConceptSystem.tag_separator} \") as ?gt)}
         synonym_grp = %Q{(GROUP_CONCAT(DISTINCT ?sy;separator=\"#{Thesaurus::ManagedConcept.synonym_separator} \") as ?sys)}
-        query = "SELECT DISTINCT ?pi ?pl ?i ?n ?d ?pt ?uri #{synonym_grp} #{tag_grp} WHERE\n"
+        query = "SELECT DISTINCT ?pi ?pl ?i ?n ?d ?pt ?uri ?thi ?thv #{synonym_grp} #{tag_grp} WHERE\n"
         query += "{\n"
         query += "  #{main_part}"
         query += "}" 
-        query += " GROUP BY ?pi ?pl ?i ?n ?d ?pt ?uri"
+        query += " GROUP BY ?pi ?pl ?i ?n ?d ?pt ?uri ?thi ?thv"
         query += " ORDER BY #{order} (#{variable}) OFFSET #{params[:start]} LIMIT #{params[:length]}" if limit
         query
       end
