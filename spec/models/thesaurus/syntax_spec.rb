8307dc7ab1b98c2a449b3b41d617e2ce1eb13af1
Clarisa <car@s-cubed.dk>
2020-03-17 10:22:05 +0100

    GLAN-1270
    [1] Fix tests
    [2] Add comments

--- spec/models/thesaurus/syntax_spec.rb
+++ spec/models/thesaurus/syntax_spec.rb
@@ -4,27 +4,27 @@ describe "Syntax" do
 
   it "splits string AND" do
     x = Thesaurus::Syntax.new("Harry AND Potter")
-    expect(x.array_to_sparql("?x")).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) && CONTAINS(UCASE(?x), UCASE('Potter'))) .")
+    expect(x.array_to_sparql("?x")).to eq(" FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) && CONTAINS(UCASE(?x), UCASE('Potter'))) . \n ")
   end
 
   it "splits string AND MINUS" do
       x = Thesaurus::Syntax.new("Harry AND Potter -Ron")
-      expect(x.array_to_sparql("?x")).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) && CONTAINS(UCASE(?x), UCASE('Potter')) && !CONTAINS(UCASE(?x), UCASE('Ron'))) .")
+      expect(x.array_to_sparql("?x")).to eq(" FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) && CONTAINS(UCASE(?x), UCASE('Potter')) && !CONTAINS(UCASE(?x), UCASE('Ron'))) . \n ")
   end
 
   it "splits string OR" do
       x = Thesaurus::Syntax.new("Harry OR Potter")
-      expect(x.array_to_sparql("?x")).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) || CONTAINS(UCASE(?x), UCASE('Potter'))) .")
+      expect(x.array_to_sparql("?x")).to eq(" FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) || CONTAINS(UCASE(?x), UCASE('Potter'))) . \n ")
   end
 
   it "splits string OR MINUS" do
       x = Thesaurus::Syntax.new("Harry OR Potter -Ron")
-      expect(x.array_to_sparql("?x")).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) || CONTAINS(UCASE(?x), UCASE('Potter')) && !CONTAINS(UCASE(?x), UCASE('Ron'))) .")
+      expect(x.array_to_sparql("?x")).to eq("FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) || CONTAINS(UCASE(?x), UCASE('Potter')) ) .  \n FILTER (!CONTAINS(UCASE(?x), UCASE('Ron')) ) .  \n")
   end
 
   it "splits string MINUS" do
       x = Thesaurus::Syntax.new("Harry -Potter")
-      expect(x.array_to_sparql("?x")).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) && !CONTAINS(UCASE(?x), UCASE('Potter'))) .")
+      expect(x.array_to_sparql("?x")).to eq(" FILTER (CONTAINS(UCASE(?x), UCASE('Harry')) && !CONTAINS(UCASE(?x), UCASE('Potter'))) . \n ")
   end
 
   # it "splits string MINUS invalid" do
@@ -34,17 +34,22 @@ describe "Syntax" do
 
   it "splits string EXACT MATCH" do
       x = Thesaurus::Syntax.new("\"Harry Potter and The Chamber of Secrets\"")
-      expect(x.array_to_sparql("?x")).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Harry Potter and The Chamber of Secrets'))) .")
+      expect(x.array_to_sparql("?x")).to eq(" FILTER (CONTAINS(UCASE(?x), UCASE('Harry Potter and The Chamber of Secrets'))) . \n ")
   end
 
   it "splits string EXACT MATCH" do
       x = Thesaurus::Syntax.new("\"Harry\"")
-      expect(x.array_to_sparql("?x")).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Harry'))) .")
+      expect(x.array_to_sparql("?x")).to eq(" FILTER (CONTAINS(UCASE(?x), UCASE('Harry'))) . \n ")
   end
 
   it "splits string default" do
       x = Thesaurus::Syntax.new("Harry Potter and the Chamber of Secrets")
-      expect(x.array_to_sparql("?x")).to eq(". FILTER (CONTAINS(UCASE(?x), UCASE('Harry Potter and the Chamber of Secrets'))) .")
+      expect(x.array_to_sparql("?x")).to eq(" FILTER (CONTAINS(UCASE(?x), UCASE('Harry Potter and the Chamber of Secrets'))) . \n ")
+  end
+
+  it "splits string default" do
+      x = Thesaurus::Syntax.new("Harry")
+      expect(x.array_to_sparql("?x")).to eq(" FILTER (CONTAINS(UCASE(?x), UCASE('Harry'))) . \n ")
   end
 
 end
