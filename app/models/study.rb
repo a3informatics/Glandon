73250087dd7bcb030950a5539fc5688948cfffa0
Clarisa <car@s-cubed.dk>
2020-03-23 15:33:52 +0100

    GLAN-1227
    [1] Add Study model
    [2] Add test

--- app/models/study.rb
+++ app/models/study.rb
@@ -3,13 +3,13 @@ class Study < IsoManagedV2
   configure rdf_type: "http://www.assero.co.uk/Protocol#Study",
             uri_suffix: "ST"
 
-  data_property :name
+  object_property :implements, cardinality: :one, model_class: "Protocol"
 
   def protocols
     query_string = %Q{
       SELECT ?s WHERE
       {
-        #{self.uri.to_ref} ^pr:implements ?s .
+        #{self.uri.to_ref} pr:implements ?s .
       }
     }
     query_results = Sparql::Query.new.query(query_string, "", [:pr])
