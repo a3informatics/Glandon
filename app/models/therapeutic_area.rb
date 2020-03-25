4b50ef2f638feff98d15268e93341de681d67634
Dave <dih@s-cubed.dk>
2020-03-25 13:46:26 +0100

    GLAN-1227
    [1] Updats for TA

--- app/models/therapeutic_area.rb
+++ app/models/therapeutic_area.rb
@@ -1,9 +1,8 @@
-class TherapeuticArea < IsoManagedV2
+class TherapeuticArea < IsoConceptV2
 
   configure rdf_type: "http://www.assero.co.uk/Protocol#TherapeuticArea",
             uri_suffix: "TA"
 
   object_property :includes, cardinality: :many, model_class: "Indication"
-
 
 end
