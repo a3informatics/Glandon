ca715b1994d71ea7f6d8a3062491b24f19e3892e
Clarisa <car@s-cubed.dk>
2020-03-25 20:06:39 +0100

    GLAN-1227
    [1] Update model

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -5,7 +5,7 @@ class Objective < IsoManagedV2
 
   data_property :full_text
   object_property :is_assessed_by, cardinality: :one, model_class: "Endpoint"
-  #  object_property :objective_type, cardinality: :one, model_class: "Enumerated"
+  object_property :objective_type, cardinality: :one, model_class: "Enumerated"
   object_property :has_parameter, cardinality: :many, model_class: "Parameter"
 
 end
