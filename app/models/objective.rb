49e7d6c20780bb9e51e840de9b2793f2d84eefb6
Clarisa <car@s-cubed.dk>
2020-03-25 09:01:45 +0100

    GLAN-1227
    [1] Update Schema
    [2] Add endpoint and objective class

---
+++ app/models/objective.rb
@@ -0,0 +1,11 @@
+class Objective < IsoManagedV2
+
+  configure rdf_type: "http://www.assero.co.uk/Protocol#Objective",
+            uri_suffix: "OB"
+
+  data_property :fullText
+  object_property :isAssessedBy, cardinality: :one, model_class: "Endpoint"
+  object_property :type, cardinality: :many, model_class: "Primary"
+  object_property :type, cardinality: :many, model_class: "Secondary"
+
+end
