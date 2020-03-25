7c7a7b9a6c642209f8be8327bb70c5fedc1e16d7
Clarisa <car@s-cubed.dk>
2020-03-25 10:05:56 +0100

    GLAN-1227
    [1] Update Objective
    [2] Add Parameter

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -3,7 +3,7 @@ class Objective < IsoManagedV2
   configure rdf_type: "http://www.assero.co.uk/Protocol#Objective",
             uri_suffix: "OB"
 
-  data_property :fullText
+  data_property :full_text
   object_property :isAssessedBy, cardinality: :one, model_class: "Endpoint"
   object_property :type, cardinality: :many, model_class: ""
 
---
+++ app/models/parameter.rb
@@ -0,0 +1,9 @@
+class Parameter < IsoConceptV2
+
+  configure rdf_type: "http://www.assero.co.uk/Protocol#Parameter",
+            uri_suffix: "PA"
+
+  data_property :parameter_rdf_type
+
+
+end
