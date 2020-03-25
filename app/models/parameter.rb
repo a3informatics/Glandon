a73376733246f01e0dc95eac42c0b9bb860a0b19
Clarisa <car@s-cubed.dk>
2020-03-25 15:36:11 +0100

    GLAN-1227
    [1] Update Parameter model
    [1] Add test

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -3,8 +3,7 @@ class Parameter < IsoConceptV2
   configure rdf_type: "http://www.assero.co.uk/Protocol#Parameter",
             uri_suffix: "PA"
 
-  data_property :parameter_rdf_type
-  data_property :label
+  object_property :parameter_rdf_type, cardinality: :one, model_class: "IsoConceptV2"
 
 
 end
