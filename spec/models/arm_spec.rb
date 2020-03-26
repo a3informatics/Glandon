1773e49c87e1b795cbbcbd439a2e0f7a5f866378
Clarisa <car@s-cubed.dk>
2020-03-26 13:33:09 +0100

    GLAN-1227
    [1] Add arm model
    [2] Add test

---
+++ spec/models/arm_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "Arm" do
+
+  include DataHelpers
+
+  def sub_dir
+    return "models/arm"
+  end
+
+  describe "basic tests" do
+
+    before :each do
+      data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl"]
+      load_files(schema_files, data_files)
+    end
+
+    after :each do
+    end
+
+    it "create an instance" do
+      item = Arm.create(label: "XXX")
+      actual = Arm.find(item.uri)
+      expect(actual.label).to eq("XXX")
+      check_file_actual_expected(actual.to_h, sub_dir, "create_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+  end
+
+end
