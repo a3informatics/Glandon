437ce884707316a5383d6f738856f6c641ddccf9
Clarisa <car@s-cubed.dk>
2020-03-26 09:30:40 +0100

    GLAN-1227
    [1] Add Endpoint class

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -3,7 +3,7 @@ class Endpoint < IsoManagedV2
   configure rdf_type: "http://www.assero.co.uk/Protocol#Endpoint",
             uri_suffix: "EN"
 
-  data_property :fullText
+  data_property :full_text
   object_property :hasMultiple, cardinality: :many, model_class: "Endpoint"
 
 end
