5cd62447928e935f1c85100fa1957671bd252cad
Clarisa <car@s-cubed.dk>
2020-03-27 10:22:46 +0100

    GLAN-1227
    [1] Add description and arm type
    [2] Update test and test file
    [3] Update protocol

--- app/models/arm.rb
+++ app/models/arm.rb
@@ -3,6 +3,8 @@ class Arm < IsoConceptV2
   configure rdf_type: "http://www.assero.co.uk/Protocol#Arm",
             uri_suffix: "AR"
 
+  data_property :description
+  data_property :arm_type
   #object_property :has_sequence_of, cardinality: :one, model_class: "Intervention"
   #object_property :derived_from, cardinality: :one, model_class: "DesignMatrixComplex"
 
