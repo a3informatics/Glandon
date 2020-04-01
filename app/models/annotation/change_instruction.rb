bf2ab3687d84c5bdc0834339283241272ec1f6a1
Clarisa <car@s-cubed.dk>
2020-04-01 09:45:31 +0200

    GLAN-1286
    [1] Fix model

--- app/models/annotation/change_instruction.rb
+++ app/models/annotation/change_instruction.rb
@@ -21,84 +21,83 @@ class Annotation::ChangeInstruction < Annotation
   # @option params [Array] :current the current item for which the change instruction is relevant
   # @option params [Array] :previous the previous item for which the change instruction is relevant
   # @return [Annotation::ChangeInstruction] the change instruction, may contain errors.
-  def self.create(params)
+  def self.create
     ci = Annotation::ChangeInstruction.new
     ci.uri = ci.create_uri(ci.class.base_uri)
     ci.by_authority = IsoRegistrationAuthority.owner.uri
-    ci.reference = params[:reference]
-    ci.description = params[:description]
-    ci.semantic = params[:semantic]
-    params[:previous].each_with_index do |p, index| 
-      uri = Uri.new(id: p)
-      ci.previous_push(ci.add_op_reference(uri, index))
-    end
-    params[:current].each_with_index do |c, index|
-      uri = Uri.new(id: c)
-      ci.current_push(ci.add_op_reference(uri, index+10000))
-    end
+    ci.reference = "Not set"
+    ci.description = "Not set"
+    ci.semantic = "Not set"
     ci.save
     ci
   end
 
-  # Update
-  #
-  # @param [Hash] params the parameters hash
-  # @option params [String] :semantic the ...
-  # @option params [String] :description the change instruction description
-  # @option params [String] :reference any references
-  # @return [Annotation::ChangeInstruction] the change instruction, may contain errors.
-  # def update(params)
-  #   #self.reference = params[:reference]
-  #   #self.description = params[:description]
-  #   #self.semantic = params[:semantic]
-  #   #self.save
-  #   #self
-  #   super(params)
+  # # Delete. Delete the change instruction and the associated references.
+  # #
+  # # @return [Integer] count of items deleted
+  # def delete_change_instruction
+  #   self.current_objects
+  #   op_ref = OperationalReferenceV3.find(self.current.first.uri) #will we have more than one OP?
+  #   transaction_begin
+  #   op_ref.delete
+  #   super
+  #   transaction_execute
+  #   1
+  #       query_string = %Q{
+  #       DELETE 
+  #       {
+  #         ?s ?p ?o
+  #       } 
+  #       WHERE 
+  #       {
+  #         {
+  #           #{self.uri.to_ref} (th:members/th:memberNext*) ?s .
+  #           ?s ?p ?o .
+  #         }     
+  #         UNION
+  #         { 
+  #           #{self.uri.to_ref} (^th:isOrdered) ?s .
+  #           ?s th:narrower ?o
+  #           BIND ( th:narrower as ?p ) .
+  #         } 
+  #       }
+  #     }
+  #     partial_update(query_string, [:th])
   # end
 
-#   # Delete. Delete the change instruction and the associated references.
-#   #
-#   # @return [Integer] count of items deleted
-#   def delete_change_instruction
-#     self.current_objects
-#     op_ref = OperationalReferenceV3.find(self.current.first.uri) #will we have more than one OP?
-#     transaction_begin
-#     op_ref.delete
-#     super
-#     transaction_execute
-#     1
-#   end
-
-  def remove_reference(id)
-    uri = Uri.new(id: id)
-  byebug
-    self.current_objects
-    op_ref = OperationalReferenceV3.find(self.current.first.uri)
+  def remove_reference(params)
+    if params[:type] == "previous"
+      set = self.previous_objects
+    else
+      set = self.current_objects
+    end
+    object = set.find{|x| x.reference == Uri.new(id: params[:id])}   
     transaction_begin
-    op_ref.delete
+    object.delete
+    self.save
     transaction_execute
     1
   end
 
   def add_references(params)
-    params[:previous].each do |p| 
-      self.previous_push(self.add_op_reference(Uri.new(id: p), self.previous.count))
+    transaction_begin
+    if !params[:previous].empty?
+      params[:previous].each_with_index do |p, index| 
+        self.previous_push(add_op_reference(Uri.new(id: p), index))
+      end
     end
-    params[:current].each do |c|
-      self.current_push(self.add_op_reference(Uri.new(id: c), self.current.count+10000))
+    if !params[:current].empty?
+      params[:current].each_with_index do |c, index|
+        self.current_push(add_op_reference(Uri.new(id: c), index+10000))
+      end
     end
-     self.save
-     self
+    self.save
+    transaction_execute
+    self
   end
 
   def add_op_reference(uri, index)
-    object = OperationalReferenceV3.new
-    object.ordinal = index + 1
-    object.uri = object.create_uri(self.uri)
-    object.reference = uri
-    object.enabled = true
-    object.optional = false
-    object
+    OperationalReferenceV3.create({reference: uri, context: nil, ordinal: index + 1}, self)
   end
 
   def add_previous(ct, reference)
