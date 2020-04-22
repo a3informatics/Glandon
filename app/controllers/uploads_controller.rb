c53acf7fe351f8783d448beca198d34b0df51635
Samuel Banas <sab@s-cubed.dk>
2020-04-22 09:36:53 +0200

    GLAN-1302
    [1] Delete multiple/all uploads, working version

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -10,9 +10,9 @@ class UploadsController < ApplicationController
     authorize Upload
   	@upload = Upload.new
     @public_files = upload_files("*").map do |f|
-      file = f.split('/')[-1]
-      { :name => file.include?('.') ? file.split('.')[0] : file,
-        :extension => file.include?('.') ? file.split('.')[-1] : "" }
+      file = f.rpartition('/').last
+      { :name => file.include?('.') ? file.rpartition('.').first : file,
+        :extension => file.include?('.') ? file.rpartition('.').last : "" }
     end
   end
 
@@ -29,25 +29,23 @@ class UploadsController < ApplicationController
   end
 
   def destroy_multiple
-    authorize Upload, :delete?
+    authorize Upload, :destroy?
     item = Upload.new
     item.delete_multiple(the_params)
-    set_flash('Files succesfully deleted.')
-    redirect_to uploads_path
+    render json: {data: []}
   end
 
   def destroy_all
-    authorize Upload, :delete?
+    authorize Upload, :destroy?
     item = Upload.new
     item.delete_all
-    set_flash('All files deleted.')
-    redirect_to uploads_path
+    render json: {data: []}
   end
 
 private
 
   def set_flash(msg)
-    if item.errors.empty? 
+    if item.errors.empty?
       flash[:success] = msg
     else
       flash[:error] = item.error.full_messages.to_sentence
