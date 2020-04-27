0cd5302aeae43a0ed22eb26854c346cd8d4e6f24
Samuel Banas <sab@s-cubed.dk>
2020-04-27 13:56:53 +0200

    GLAN-1302
    [1] Controller tests for imports

--- spec/controllers/imports/sponsor_term_format_two_controller_spec.rb
+++ spec/controllers/imports/sponsor_term_format_two_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe Imports::TermsController do
+describe Imports::SponsorTermFormatTwoController do
 
   include DataHelpers
   include PublicFileHelpers
-  
+
   describe "Authorized User" do
-    
+
     def sub_dir
       return "controllers/imports/terms"
     end
@@ -48,29 +48,9 @@ describe Imports::TermsController do
       delete_all_public_test_files
     end
 
-    it "items" do
-      test_files
-      request.env['HTTP_ACCEPT'] = "application/json"
-      expected = [{something: "X"}, {something: "Y"}]
-      expect_any_instance_of(Import::Term).to receive(:list).and_return(expected)
-      expected.each {|x| x[:filename] = @file_1}
-      get :items, {imports: {file_type: 1, files: [@file_1]}}
-      expect(response.content_type).to eq("application/json")
-      expect(response.code).to eq("200")
-      expect(response.body).to eq("{\"data\":#{expected.to_json}}")
-    end
-
-    it "items, no params" do
-      request.env['HTTP_ACCEPT'] = "application/json"
-      get :items, {imports: {}}
-      expect(response.content_type).to eq("application/json")
-      expect(response.code).to eq("200")
-      expect(response.body).to eq("{\"data\":[]}")
-    end
-
     it "create, json request" do
       request.env['HTTP_ACCEPT'] = "application/json"
-      expect_any_instance_of(Import::Term).to receive(:create)
+      expect_any_instance_of(Import::SponsorTermFormatTwo).to receive(:create)
       post :create, {imports: {identifier: "AAA", files: [@file_1]}}
       expect(response.content_type).to eq("application/json")
       expect(response.code).to eq("200")
@@ -78,8 +58,8 @@ describe Imports::TermsController do
     end
 
     it "create, http request" do
-      id = 1 
-      expect_any_instance_of(Import::Term).to receive(:create) do |arg|
+      id = 1
+      expect_any_instance_of(Import::SponsorTermFormatTwo).to receive(:create) do |arg|
         arg.id = id
         arg.save
       end
@@ -90,16 +70,11 @@ describe Imports::TermsController do
   end
 
   describe "Reader User" do
-    
+
     login_reader
 
     it "new" do
       get :new
-      expect(response).to redirect_to("/")
-    end
-
-    it "items" do
-      delete :items
       expect(response).to redirect_to("/")
     end
 
@@ -108,17 +83,12 @@ describe Imports::TermsController do
       expect(response).to redirect_to("/")
     end
 
-  end 
-    
+  end
+
   describe "Unauthorized User" do
-    
+
     it "new" do
       get :new
-      expect(response).to redirect_to("/users/sign_in")
-    end
-
-    it "items" do
-      get :items
       expect(response).to redirect_to("/users/sign_in")
     end
 
