a357b32bfd62e422b4a54b3e65497bf22db1d327
Samuel Banas <sab@s-cubed.dk>
2020-05-20 15:47:20 +0200

    GLAN-1318
    [1] Added Rank partials into CL, Subset and Extension Edit pages (HTML + JS)
    [2] Other syntax improvements

--- app/assets/javascripts/thesauri/managed_concepts/edit.js
+++ app/assets/javascripts/thesauri/managed_concepts/edit.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-  ep = new EditorPanel(children_path, update_path, parent_id, lockCallback);
-  np = new NewPanel(add_child_path, 'managed_concept', callback);
-  timer = new Timer($('#edit_lock_token').val(), "imh_header", $('#warning_timeout').val());
-  unload = new UnloadV2($('#edit_lock_token').val(), false, unloadCallback);
+  var ep = new EditorPanel(childrenPath, updatePath, parentId, lockCallback);
+  var np = new NewPanel(addChildPath, 'managed_concept', callback);
+  var timer = new Timer($('#edit_lock_token').val(), "imh_header", $('#warning_timeout').val());
+  var unload = new UnloadV2($('#edit_lock_token').val(), false, unloadCallback);
+  var rankModal = new RankModal(lockCallback);
 
   $("#edit-properties-button").on("click", function(){
     new EditProperties(epItemcodelist, "codelist", "ManagedConcept", null).show();
   });
+
 
   function callback(data) {
     ep.add(data);
