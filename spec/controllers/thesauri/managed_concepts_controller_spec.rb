967a7dfb1e89765234b9d7e8d02e970b92cc42dd
Samuel Banas <sab@s-cubed.dk>
2020-05-27 13:36:20 +0200

    GLAN-1318
    [1] Controller test fixes

--- spec/controllers/thesauri/managed_concepts_controller_spec.rb
+++ spec/controllers/thesauri/managed_concepts_controller_spec.rb
@@ -675,17 +675,18 @@ describe Thesauri::ManagedConceptsController do
 
     it "update rank" do
       request.env['HTTP_ACCEPT'] = "application/json"
-      content = {cli_id:Uri.new(uri:"http://www.cdisc.org/C66741/V20#C66741_C87054").to_id, rank: 2}
-      rank = Thesaurus::Rank.find(Uri.new(uri: "http://www.assero.co.uk/TRC#e0c80ddd-2f1c-4832-885e-9283e87d6bd8"))
-      put :update_rank, {id: rank.id, managed_concept: {children_ranks: [content]}}
+      tc = Thesaurus::ManagedConcept.find_minimum(Uri.new(uri: "http://www.cdisc.org/C66741/V20#C66741"))
+      content = {cli_id: Uri.new(uri:"http://www.cdisc.org/C66741/V20#C66741_C87054").to_id, rank: 2}
+      put :update_rank, {id: tc.id, managed_concept: {children_ranks: [content]}}
       expect(response.content_type).to eq("application/json")
       expect(response.code).to eq("200")
     end
 
     it "remove rank" do
       request.env['HTTP_ACCEPT'] = "application/json"
+      tc = Thesaurus::ManagedConcept.find_minimum(Uri.new(uri: "http://www.cdisc.org/C66741/V20#C66741"))
       rank = Thesaurus::Rank.find(Uri.new(uri: "http://www.assero.co.uk/TRC#e0c80ddd-2f1c-4832-885e-9283e87d6bd8"))
-      delete :remove_rank, {id: rank.id}
+      delete :remove_rank, {id: tc.id}
       expect(response.content_type).to eq("application/json")
       expect(response.code).to eq("200")
     end
