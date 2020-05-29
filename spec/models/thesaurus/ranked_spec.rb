e9ead2aa7c03328841cd6f6062cd3eebfdc8142b
daveih <dave.iberson-hurst@assero.co.uk>
2020-05-29 13:29:13 +0200

    GLAN-1318
    [1] Update tests

--- spec/models/thesaurus/ranked_spec.rb
+++ spec/models/thesaurus/ranked_spec.rb
@@ -12,12 +12,22 @@ describe "Thesaurus::Ranked" do
   describe "schema load" do
 
     before :all do
-      load_files(schema_files, [])
+      data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl"]
+      load_files(schema_files, data_files)
     end
 
-    it "check thesaurus schema migration one" do
-      result = triple_store.subject_triples(Uri.new(uri:"http://www.assero.co.uk/Thesaurus#Subset"))
-      check_file_actual_expected(result, sub_dir, "schema_expected_1.yaml")
+    it "check ranked" do
+      tc = Thesaurus::ManagedConcept.create
+      other = Thesaurus::ManagedConcept.find(tc.uri)
+      expect(other.ranked?).to eq(false)
+      tc.is_ranked = Uri.new(uri: "http://www.example.com/A/B#C")
+      tc.save
+      other = Thesaurus::ManagedConcept.find(tc.uri)
+      expect(other.ranked?).to eq(true)
+      tc.is_ranked = nil
+      tc.save
+      other = Thesaurus::ManagedConcept.find(tc.uri)
+      expect(other.ranked?).to eq(false)
     end
 
   end
