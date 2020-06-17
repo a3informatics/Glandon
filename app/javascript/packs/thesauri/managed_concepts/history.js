0fea34633d36e57162e194a49ad46c73aebbb6a4
Samuel Banas <sab@s-cubed.dk>
2020-06-17 15:48:33 +0200

    GLAN-1342
    [1] Managed Concept Index and History pages updated

---
+++ app/javascript/packs/thesauri/managed_concepts/history.js
@@ -0,0 +1,11 @@
+import HistoryPanel from "shared/iso_managed/history_panel";
+
+$(document).ready( () => {
+
+  let ip = new HistoryPanel({
+    url: historyDataUrl,
+    param: "managed_concept",
+    cache: false
+  });
+
+});
---
+++ app/javascript/packs/thesauri/managed_concepts/index.js
@@ -0,0 +1,21 @@
+import IndexPanel from "shared/iso_managed/index_panel";
+import { dtIndicatorsColumn } from "shared/helpers/dt_columns";
+import { $post } from "shared/helpers/ajax";
+
+$(document).ready( () => {
+
+  let ip = new IndexPanel({
+    url: indexDataUrl,
+    param: "managed_concept",
+    extraColumns: [ { data: "notation" }, dtIndicatorsColumn()]
+  });
+
+  // Create a new Code List
+  $("#tnb_new_button").on('click', () => {
+    $post({
+      url: createCLUrl,
+      done: (data) => location.href = data.history_path
+    })
+  });
+
+});
