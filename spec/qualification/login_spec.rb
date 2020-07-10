d581abf61704720eb2d44040d76d726e70a4b0ef
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-07-10 09:52:00 +0200

    GLAN-1387
    [1] Examples of before and after blocks

--- spec/qualification/login_spec.rb
+++ spec/qualification/login_spec.rb
@@ -7,7 +7,7 @@ describe "Tests validation server", :type => :feature, :remote=> true do
   include WaitForAjaxHelper
   include RemoteServerHelpers
 
-  RemoteServerHelpers.switch_to_remote
+  #RemoteServerHelpers.switch_to_remote
 
   describe "Login", :type => :feature do
 
@@ -20,6 +20,34 @@ describe "Tests validation server", :type => :feature, :remote=> true do
     end
 
     it "Login test - REQ12345", js: true do
+    end
+
+  end
+
+  describe "Login", :type => :feature do
+
+    before :all do
+      puts 'before all'
+    end
+
+    before :each do
+      puts 'before each'
+    end
+    
+    after :all do
+      puts 'after all'
+    end
+
+    after :each do
+      puts 'after each'
+    end
+    
+    it "Login test - REQ12333", js: true do
+      puts '12333'
+    end
+
+    it "Login test - REQ12334", js: true do
+      puts '12334'
     end
 
   end
