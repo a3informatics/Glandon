96c3de16b2ef596593b144aaa070f98e6de8ff56
Samuel Banas <sab@s-cubed.dk>
2020-07-15 13:24:32 +0200

    GLAN-1391
    [1] Updated Terminology references to a new format for HTML display + other types (raw string output)

--- app/javascript/shared/ui/strings.mjs
+++ app/javascript/shared/ui/strings.mjs
@@ -2,21 +2,21 @@
 
 /**
  * Returns String representation of an UnmangedConcept
- * @param {Object} uc UnmanagedConcept data object, fields: (identifier || notation)
- * @param {Object} mc ManagedConcept data object, fields: (identifier || notation, semantic_version)
+ * @param {Object} uc UnmanagedConcept data object, fields: (notation || label, identifier)
+ * @param {Object} mc ManagedConcept data object, fields: (notation || label, identifier, semantic_version)
  * @return {string} UnmanagedConcept as a string reference
  */
 function unmanagedConceptRef(uc, mc) {
-  return `${uc.notation || uc.identifier} (Code List: ${managedConceptRef(mc)})`;
+  return `${uc.notation || uc.label} ${uc.identifier} <span class="text-xtiny">(${managedConceptRef(mc)})</span>`;
 }
 
 /**
  * Returns String representation of a ManagedConcept
- * @param {Object} mc ManagedConcept data object, fields: (identifier || notation, semantic_version)
+ * @param {Object} mc ManagedConcept data object, fields: (notation || label, identifier, semantic_version)
  * @return {string} ManagedConcept as a string reference
  */
 function managedConceptRef(mc) {
-  return `${mc.notation || mc.identifier} v${mc.semantic_version}`;
+  return `${mc.notation || mc.label} ${mc.identifier} v${mc.semantic_version}`;
 }
 
 export {
