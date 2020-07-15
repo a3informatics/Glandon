099883157c7bad656cf4b40fc3b0941c6026ecbf
Samuel Banas <sab@s-cubed.dk>
2020-07-15 14:06:10 +0200

    GLAN-1391
    [1] Improvements to code
    [2] Remove pagination from form show panel

--- app/javascript/shared/forms/show_panel.mjs
+++ app/javascript/shared/forms/show_panel.mjs
@@ -29,7 +29,7 @@ export default class ShowPanel extends TablePanel {
       selector,
       url,
       param,
-      paginated: true,
+      paginated: false,
       order: [[0, "asc"]],
       buttons: [csvExportBtn(), excelExportBtn()]
     });
