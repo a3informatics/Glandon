b75f3b87121b0d9568e4ea3a9fd69ed16f6be99a
Clarisa <car@s-cubed.dk>
2020-07-21 10:11:42 +0200

    GLAN-1402
    [1] Update test

--- spec/controllers/biomedical_concept_templates_controller_spec.rb
+++ spec/controllers/biomedical_concept_templates_controller_spec.rb
@@ -24,10 +24,30 @@ describe BiomedicalConceptTemplatesController do
 
     it "index, JSON" do  
       request.env['HTTP_ACCEPT'] = "application/json"
-      get :index_data
+      get :index
       actual = check_good_json_response(response)
-      check_file_actual_expected(actual[:data], sub_dir, "index_expected_1.yaml", equate_method: :hash_equal, write_file: true)
+      check_file_actual_expected(actual[:data], sub_dir, "index_expected_1.yaml", equate_method: :hash_equal)
     end
+
+    # it "shows the history, page" do
+    #   instance = BiomedicalConceptTemplate.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/HEIGHT/V1#BCI"))
+    #   request.env['HTTP_ACCEPT'] = "application/json"
+    #   expect(BiomedicalConceptInstance).to receive(:history_pagination).with({identifier: instance.has_identifier.identifier, scope: an_instance_of(IsoNamespace), offset: "20", count: "20"}).and_return([instance])
+    #   get :history, params:{biomedical_concept_instance: {identifier: instance.has_identifier.identifier, scope_id: "aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTlMjU0NVQkVE", count: 20, offset: 20}}
+    #   expect(response.content_type).to eq("application/json")
+    #   expect(response.code).to eq("200")
+    #   actual = JSON.parse(response.body).deep_symbolize_keys[:data]
+    #   check_file_actual_expected(actual, sub_dir, "history_expected_1.yaml", equate_method: :hash_equal)
+    # end
+
+    # it "shows the history, initial view" do
+    #   params = {}
+    #   expect(BiomedicalConceptInstance).to receive(:latest).and_return(BiomedicalConceptTemplate.new)
+    #   get :history, params:{biomedical_concept_instance: {identifier: "HEIGHT", scope_id: "aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTlMjU0NVQkVE"}}
+    #   expect(assigns(:identifier)).to eq("HEIGHT")
+    #   expect(assigns(:scope_id)).to eq("aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTlMjU0NVQkVE")
+    #   expect(response).to render_template("history")
+    # end
 
     # it "lists all unique templates, HTML" do
     #   get :index
