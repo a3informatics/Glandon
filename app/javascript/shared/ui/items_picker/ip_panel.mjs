2ec7982b91babdfc9ac8b3b87a6096f0350c7650
Samuel Banas <sab@s-cubed.dk>
2020-07-22 13:54:11 +0200

    GLAN-1396
    [1] Wrote a Items Picker Panel module
    [2] Wrote a Managed Items Selector module for the Items Picker

---
+++ app/javascript/shared/ui/items_picker/ip_panel.mjs
@@ -0,0 +1,67 @@
+import SelectablePanel from 'shared/base/selectable_panel'
+
+/**
+ * Custom Item Picker Panel implementation
+ * @description A selectable panel that can be used in the Item Picker modal with custom DT init settings
+ * @extends SelectablePanel class from shared/base/selectable_panel
+ * @author Samuel Banas <sab@s-cubed.dk>
+ */
+export default class IPPanel extends SelectablePanel {
+
+  /**
+   * Create a Item Picker Panel
+   * @param {Object} params Instance parameters
+   * @param {string} params.selector JQuery selector of the target table
+   * @param {string} params.url Url of source data
+   * @param {string} params.param Strict parameter name required for the controller params
+   * @param {int} params.count Count of items fetched in one request
+   * @param {Array} params.extraColumns Additional column definitions
+   * @param {boolean} params.paginated Specify if the loadData call should be paginated. Optional, default = true
+   * @param {Array} params.order DataTables deafult ordering specification, optional. Defaults to first column, descending
+   * @param {function} params.loadCallback Callback to data fully loaded, optional
+   * @param {boolean} params.multiple Enable / disable selection of multiple rows [default = false]
+   * @param {boolean} params.showSelectionInfo Enable / disable selection info on the table
+   * @param {boolean} params.ownershipColorBadge Enable / disable showing a color-coded ownership badge
+   * @param {function} params.onSelect Callback on row(s) selected, optional
+   * @param {function} params.onDeselect Callback on row(s) deselected, optional
+   */
+  constructor({
+    selector,
+    url,
+    param,
+    count,
+    extraColumns = [],
+    paginated = true,
+    order = [[0, "desc"]],
+    loadCallback = () => {},
+    multiple = false,
+    showSelectionInfo = true,
+    ownershipColorBadge = false,
+    onSelect = () => { },
+    onDeselect = () => { }
+  }) {
+    super({ selector, url, param, count, extraColumns, deferLoading: true, paginated, order, loadCallback,
+      multiple, showSelectionInfo, ownershipColorBadge, onSelect, onDeselect });
+  }
+
+
+  /** Private **/
+
+
+  /**
+   * Extend default SelectablePanel init options with UI changes
+   * @return {Object} DataTable options object
+   */
+  get _tableOpts() {
+    const options = super._tableOpts;
+
+    options.pageLength = 10;
+    options.lengthChange = false;
+    options.scrollY = 400;
+    options.scrollCollapse = true;
+    options.autoWidth = true;
+
+    return options;
+  }
+
+}
---
+++ app/javascript/shared/ui/items_picker/managed_item_selector.mjs
@@ -0,0 +1,179 @@
+import Cacheable from 'shared/base/cacheable'
+
+import IPPanel from 'shared/ui/items_picker/ip_panel'
+import { makeHistoryUrl } from 'shared/helpers/urls'
+import { tableInteraction } from 'shared/helpers/utils'
+import { dtIndexColumns, dtSimpleHistoryColumns } from 'shared/helpers/dt/dt_column_collections'
+import { dtIndicatorsColumn } from 'shared/helpers/dt/dt_columns'
+
+/**
+ * Managed Item Selector
+ * @description Allows for a selection of a specific version of a Managed Item
+ * @requires index HTML table
+ * @requires history HTML table
+ * @author Samuel Banas <sab@s-cubed.dk>
+ */
+export default class ManagedItemSelector extends Cacheable {
+
+  /**
+   * Create a Managed Item Selector
+   * @param {Object} params Instance parameters
+   * @param {string} params.selector JQuery selector of the target table
+   * @param {Object} params.urls Object containing the history and index data url bases
+   * @param {string} params.param Strict parameter name required for the controller params
+   * @param {boolean} params.multiple Enable / disable selection of multiple rows [default = false]
+   * @param {function} params.onSelect Callback on row(s) selected, optional
+   * @param {function} params.onDeselect Callback on row(s) deselected, optional
+   */
+  constructor({
+    selector,
+    urls,
+    param,
+    multiple = false,
+    onSelect = () => { },
+    onDeselect = () => { }
+  }) {
+    super();
+
+    Object.assign(this, { selector, urls: urls[param], param, multiple, onSelect, onDeselect })
+    this._initialize();
+  }
+
+  /**
+   * Refreshes the selector to its initial state
+   */
+  refresh() {
+    this.indexPanel.refresh();
+    this.historyPanel.clear();
+  }
+
+
+  /** Private **/
+
+
+  /**
+   * Initializes the required panels for Selector type
+   */
+  _initialize() {
+
+    // Initializes Selectable Index Panel
+    this.indexPanel = new IPPanel({
+      url: this.urls.index,
+      selector: `${this.selector} table#index`,
+      param: this.param,
+      count: 500,
+      extraColumns: this._indexColumns,
+      showSelectionInfo: false,
+      ownershipColorBadge: true,
+      onSelect: () => this._loadHistoryData(),
+      onDeselect: () => this.historyPanel.clear(),
+      loadCallback: () => this._toggleInteractivity(true)
+    });
+
+    // Initializes Selectable History Panel
+    this.historyPanel = new IPPanel({
+      selector: `${this.selector} table#history`,
+      param: this.param,
+      count: 100,
+      extraColumns: this._historyColumns,
+      multiple: this.multiple,
+      loadCallback: (t) => {
+        this._cacheItemHistory( t.rows().data().toArray() );
+        this._toggleInteractivity(true);
+      }
+    });
+
+    // Load Index
+    this._loadIndexData();
+  }
+
+  /**
+   * Generates a cache key and saves the History data into the local cache
+   * @param {Array} data Table item data array to be cached
+   */
+  _cacheItemHistory(data) {
+    // Generate cache key from the currently selected row in the Index panel
+    const cacheKey = this._makeCacheKey(this.indexPanel.selectedData[0]);
+
+    // Store current state of History panel in the cache
+    this._saveToCache( cacheKey , data )
+  }
+
+  /**
+   * Loads and renders Index data
+   */
+  _loadIndexData() {
+    this._toggleInteractivity(false);
+    this.indexPanel.loadData();
+  }
+
+  /**
+   * Loads and renders History data for item selected in Index panel
+   */
+  _loadHistoryData() {
+    const selectedItem = this.indexPanel.selectedData[0];
+    // Generate cache key from the currently selected row in the Index panel
+    const cacheKey = this._makeCacheKey(selectedItem);
+
+    // Try loading data from the cache
+    if (this._getFromCache(cacheKey) !== null)
+      this.historyPanel._render( this._getFromCache(cacheKey), true );
+
+    // Otherwise load from the server
+    else {
+      this._toggleInteractivity(false);
+      this.historyPanel.loadData( makeHistoryUrl(this.urls.history, selectedItem) );
+    }
+  }
+
+  /**
+   * Generates a unique cache key from item data
+   * @param {Object} data Item's data object containing identifier and scope_id
+   * @return {string} cache key dependent on data argument
+   */
+  _makeCacheKey(data) {
+    return `${data.identifier}:${data.scope_id}`;
+  }
+
+  /**
+   * Toggles interactivity on instance's panels
+   * @param {boolean} enable Set to true to enable interactivity, false to disable it
+   */
+  _toggleInteractivity(enable) {
+    // Make tables selector
+    const targets = `${this.indexPanel.selector}, ${this.historyPanel.selector}`;
+
+    if (enable) 
+      tableInteraction.enable(targets);
+    else
+      tableInteraction.disable(targets);
+  }
+
+  /**
+   * Get DT Index panel column definitions for instance's 'param' type
+   * @return {Array} DT Index panel column definitions
+   */
+  get _indexColumns() {
+    switch (this.param) {
+      case "managed_concept":
+        return [...dtIndexColumns(), { data: "notation" }, dtIndicatorsColumn()];
+        break;
+      default:
+        return dtIndexColumns();
+        break;
+    }
+  }
+
+  /**
+   * Get DT History panel column definitions for instance's 'param' type
+   * @return {Array} DT History panel column definitions
+   */
+  get _historyColumns() {
+    switch (this.param) {
+      default:
+        return dtSimpleHistoryColumns();
+        break;
+    }
+  }
+
+}
