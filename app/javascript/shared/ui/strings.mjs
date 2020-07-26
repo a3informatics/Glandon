a292dee13c9de598d2a9359d215649e09c786391
Samuel Banas <sab@s-cubed.dk>
2020-07-26 16:12:57 +0200

    GLAN-1396
    [1] Updated modal view base module (renamed function)
    [2] Added a helper function to strings module that will handle data items which have required properties stored within has_identifier sub-object
    [3] Updated js_urls partial helper

--- app/javascript/shared/ui/strings.mjs
+++ app/javascript/shared/ui/strings.mjs
@@ -7,6 +7,8 @@
  * @return {string} UnmanagedConcept as a string reference
  */
 function unmanagedConceptRef(uc, mc) {
+  _handleNestedProperties(uc);
+
   return `${uc.notation || uc.label} ${uc.identifier} <span class="text-xtiny">(${managedConceptRef(mc)})</span>`;
 }
 
@@ -16,7 +18,25 @@ function unmanagedConceptRef(uc, mc) {
  * @return {string} ManagedConcept as a string reference
  */
 function managedConceptRef(mc) {
+  _handleNestedProperties(mc);
+
   return `${mc.notation || mc.label} ${mc.identifier} v${mc.semantic_version}`;
+}
+
+
+/** Private **/
+
+
+/**
+ * Helper, transforms data structure by bringing semantic_version and identifier fields to object's base level from the has_identifier property
+ * @param {Object} data item data object
+ * @return {Object} transformed object with properties in base level
+ */
+function _handleNestedProperties(data) {
+  if (data.has_identifier) {
+    data.semantic_version = data.has_identifier.semantic_version;
+    data.identifier = data.has_identifier.identifier;
+  }
 }
 
 export {
