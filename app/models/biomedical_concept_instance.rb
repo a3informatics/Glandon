1ecb0ffdcc275cbd89a0630c846a7d89c25cf3d5
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-07-27 08:11:48 +0200

    GLAN-1405
    [1] Fix BCI read paths and test

--- app/models/biomedical_concept_instance.rb
+++ app/models/biomedical_concept_instance.rb
@@ -7,6 +7,6 @@ class BiomedicalConceptInstance < BiomedicalConcept
   configure rdf_type: "http://www.assero.co.uk/BiomedicalConcept#BiomedicalConceptInstance",
             uri_suffix: "BCI"
 
-  object_property :based_on, cardinality: :one, model_class: "BiomedicalConceptTemplate"
+  object_property :based_on, cardinality: :one, model_class: BiomedicalConceptTemplate, delete_exclude: true, read_exclude: true
 
 end
