19b83e3f2b6bf65e3df339482340a5498e5c2ace
Clarisa <car@s-cubed.dk>
2020-07-30 14:50:35 +0200

    GLAN-1401
    [1] Improve Subsets controller

--- app/controllers/thesauri/subsets_controller.rb
+++ app/controllers/thesauri/subsets_controller.rb
@@ -1,67 +1,59 @@
-class Thesauri::SubsetsController < ApplicationController
+class Thesauri::SubsetsController < ManagedItemsController
 
   before_action :authenticate_user!
 
-  C_CLASS_NAME = "ThesaurusConceptsController"
-
   def add
     authorize Thesaurus, :edit?
-    subset = Thesaurus::Subset.find(params[:id])
-    if check_token_valid? (subset)
-      subset.add(the_params[:cli_ids])
-      render json: { }, status: 200
-    end
+    subset = Thesaurus::Subset.find(protect_from_bad_id(params))
+    parent_mc = Thesaurus::ManagedConcept.find_minimum(subset.find_mc.id)
+    return true unless check_lock_for_item(parent_mc)
+    subset.add(the_params[:cli_ids])
+    return true if lock_item_errors
+    render json: { }, status: 200
   end
 
   def remove
     authorize Thesaurus, :edit?
-    subset = Thesaurus::Subset.find(params[:id])
-    if check_token_valid? (subset)
-      sm = subset.remove(the_params[:member_id])
-      render json: { }, status: 200
-    end
+    subset = Thesaurus::Subset.find(protect_from_bad_id(params))
+    parent_mc = Thesaurus::ManagedConcept.find_minimum(subset.find_mc.id)
+    return true unless check_lock_for_item(parent_mc)
+    sm = subset.remove(the_params[:member_id])
+    return true if lock_item_errors
+    render json: { }, status: 200
   end
 
   def remove_all
     authorize Thesaurus, :edit?
-    subset = Thesaurus::Subset.find(params[:id])
-    if check_token_valid? (subset)
-      sm = subset.remove_all
-      render json: { }, status: 200
-    end
+    subset = Thesaurus::Subset.find(protect_from_bad_id(params))
+    parent_mc = Thesaurus::ManagedConcept.find_minimum(subset.find_mc.id)
+    return true unless check_lock_for_item(parent_mc)
+    sm = subset.remove_all
+    return true if lock_item_errors
+    render json: { }, status: 200
   end
 
 
   def move_after
     authorize Thesaurus, :edit?
-    subset = Thesaurus::Subset.find(params[:id])
-    if check_token_valid? (subset)
-      sm = subset.move_after(the_params[:member_id], the_params[:after_id])
-      render json: { }, status: 200
-    end
+    subset = Thesaurus::Subset.find(protect_from_bad_id(params))
+    parent_mc = Thesaurus::ManagedConcept.find_minimum(subset.find_mc.id)
+    return true unless check_lock_for_item(parent_mc)
+    sm = subset.move_after(the_params[:member_id], the_params[:after_id])
+    return true if lock_item_errors 
+    render json: { }, status: 200
   end
 
   def list_children
     authorize Thesaurus, :show?
-    subset = Thesaurus::Subset.find(params[:id])
+    subset = Thesaurus::Subset.find(protect_from_bad_id(params))
     lp = subset.list_pagination(params)
-    render json: {data: lp, offset: params[:offset] , count: lp.count }, status: 200
+    render json: {data: lp, offset: params[:offset], count: lp.count}, status: 200
   end
 
   private
+
     def the_params
       params.require(:subset).permit(:member_id, :after_id, :cli_ids => [])
-    end
-
-    def check_token_valid?(subset)
-      parent_mc = Thesaurus::ManagedConcept.find_minimum(subset.find_mc.id)
-      token = Token.find_token(parent_mc, current_user)
-      if token.nil?
-        render :json => {:errors => ["The edit lock has timed out."] }, :status => 422
-        false
-      else
-        true
-      end
     end
 
 end
