eece1a5843240a6ec80ed739850447e6243a4581
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-07-31 13:52:08 +0200

    GLAN-1394
    [1] File missed from last push

--- app/controllers/biomedical_concept_instances_controller.rb
+++ app/controllers/biomedical_concept_instances_controller.rb
@@ -25,54 +25,11 @@ class BiomedicalConceptInstancesController < ManagedItemsController
   def show_data
     @bc = BiomedicalConceptInstance.find_minimum(protect_from_bad_id(params))
     items = @bc.get_properties(true)
-    items = items.each do |x|
-      x[:has_complex_datatype][:has_property][:has_coded_value].each do |cv|
-        cv.reverse_merge!({show_path: thesauri_unmanaged_concept_path({id: cv[:reference][:id], unmanaged_concept: {parent_id: cv[:context][:id], context_id: ""}})})
-      end
-    end
+    add_tc_paths_to_items(items)
     render json: {data: items}, status: 200
   end
 
-  # def editable
-  #   authorize BiomedicalConcept, :index?
-  #   results = {:data => []}
-  #   bcs = BiomedicalConcept.unique
-  #   bcs.each do |bc|
-  #     history = BiomedicalConcept.history({identifier: bc[:identifier], scope: IsoNamespace.find(bc[:scope_id])})
-  #     if history.length > 0
-  #       results[:data] << history[0].to_json if history[0].edit?
-  #     end
-  #   end
-  #   respond_to do |format|
-  #     format.json do
-  #       render json: results
-  #     end
-  #   end
-  # end
-
-
-  # def list
-  #   authorize BiomedicalConcept
-  #   @bcs = BiomedicalConcept.list
-  #   respond_to do |format|
-  #     format.json do
-  #       results = {:data => []}
-  #       @bcs.each { |x| results[:data] << x.to_json }
-  #       render json: results
-  #     end
-  #   end
-  # end
-  # def new
-  #   authorize BiomedicalConceptInstance, :new?
-  #   @bcts = BiomedicalConceptTemplate.all
-  # end
-
-  # def update
-
-  # end
-
   def create_from_template
-    authorize BiomedicalConceptInstance, :create?
     template = BiomedicalConceptTemplate.find_full(protect_from_bad_id(template_id))
     instance = BiomedicalConceptInstance.create_from_template(the_params, template)
     if instance.errors.empty?
@@ -85,42 +42,30 @@ class BiomedicalConceptInstancesController < ManagedItemsController
   end
 
   def edit
-    authorize BiomedicalConceptInstance
     @bc = BiomedicalConcept.find_minimum(protect_from_bad_id(params))
-    # Lock item and get token
     respond_to do |format|
       format.html do
+        return true unless edit_lock(@bc)
+        @bc = @edit.item
         @data_path = show_data_biomedical_concept_instance_path(@bc)
         @close_path = history_biomedical_concept_instances_path({ biomedical_concept_instance:
-            { identifier: @bc.has_identifier.identifier, scope_id: @bc.scope } })
+            { identifier: @bc.scoped_identifier, scope_id: @bc.scope } })
       end
       format.json do
-        # Return item data for the table
+        return true unless edit_lock(@bc, :json)
+        @bc = @edit.item
+        render :json => {data: @bc.to_h}, :status => 200 
       end
     end
   end
 
-  # def edit_lock
-  #   authorize BiomedicalConcept, :edit?
-  #   @bc = BiomedicalConcept.find(params[:id], the_params[:namespace])
-  #   if @bc.new_version?
-  #     json = @bc.to_operation
-  #     new_bc = BiomedicalConcept.create(json)
-  #     @bc = BiomedicalConcept.find(new_bc.id, new_bc.namespace)
-  #   end
-  #   @token = Token.obtain(@bc, current_user)
-  #   if @token.nil?
-  #     render :json => {}, :status => 422
-  #   else
-  #     render :json => { bc: @bc.to_json, token: @token.id }, :status => 200
-  #   end
-  # end
-
-  # def edit_multiple
-  #   authorize BiomedicalConcept, :edit?
-  #   @bcts = BiomedicalConceptTemplate.all
-  #   @close_path = biomedical_concepts_path
-  # end
+  def edit_data
+    bc = BiomedicalConcept.find_full(protect_from_bad_id(params))
+    return true unless get_lock_for_item(bc)
+    items = bc.get_properties(true)
+    add_tc_paths_to_items(items)
+    render :json => {data: items}, :status => 200 
+  end
 
   # def clone
   #   authorize BiomedicalConcept
@@ -167,12 +112,6 @@ class BiomedicalConceptInstancesController < ManagedItemsController
   #   render json: @bc.to_json
   # end
 
-  # def export_json
-  #   authorize BiomedicalConcept
-  #   @bc = BiomedicalConcept.find(params[:id], the_params[:namespace])
-  #   send_data @bc.to_json.to_json, filename: "#{@bc.owner_short_name}_#{@bc.identifier}.json", :type => 'application/json; header=present', disposition: "attachment"
-  # end
-
   # def upgrade
   #   authorize BiomedicalConcept
   #   @bc = BiomedicalConcept.find(params[:id], the_params[:namespace])
@@ -183,10 +122,22 @@ class BiomedicalConceptInstancesController < ManagedItemsController
 
 private
 
+  # Add paths to terminology references
+  def add_tc_paths_to_items(items)
+    items = items.each do |x|
+      x[:has_complex_datatype][:has_property][:has_coded_value].each do |cv|
+        cv.reverse_merge!({show_path: thesauri_unmanaged_concept_path({id: cv[:reference][:id], unmanaged_concept: {parent_id: cv[:context][:id], context_id: ""}})})
+      end
+    end
+    items
+  end
+
+  # Strong parameters
   def the_params
     params.require(:biomedical_concept_instance).permit(:identifier, :label, :offset, :count, :scope_id, :template_id)
   end
 
+  # Get the template id from the params
   def template_id
     {id: the_params[:template_id]}
   end
@@ -203,14 +154,17 @@ private
     end
   end
 
+  # Model class 
   def model_klass
     BiomedicalConceptInstance
   end
 
+  # History path
   def history_path_for(identifier, scope_id)
     return {history_path: history_biomedical_concept_instances_path({biomedical_concept_instance:{identifier: identifier, scope_id: scope_id}})}
   end
 
+  # Close path
   def close_path_for
     biomedical_concept_instances_path
   end
