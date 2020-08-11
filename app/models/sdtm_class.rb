828280ab2a6b44ab3dcf488fae593a00d1794498
Clarisa <car@s-cubed.dk>
2020-08-11 12:46:08 +0200

    GLAN-1408
    [1] Update query and rewrite test file

--- app/models/sdtm_class.rb
+++ app/models/sdtm_class.rb
@@ -12,7 +12,7 @@ class SdtmClass < Tabulation
   # @return [Array] array of objects
   def get_children
     results = []
-    query_string = %Q{SELECT DISTINCT ?var ?ordinal ?type ?label ?typedAs ?name ?desc ?classifiedAs WHERE
+    query_string = %Q{SELECT DISTINCT ?var ?ordinal ?type ?label ?typedas ?name ?desc ?classifiedas WHERE
 {
   #{self.uri.to_ref} bd:includesColumn ?c .
   ?c bd:ordinal ?ordinal .
@@ -21,13 +21,16 @@ class SdtmClass < Tabulation
   ?var isoC:label ?label. 
   ?var bd:name ?name .
   ?var bd:description ?desc .
-
+  ?var bd:typedAs ?tas .
+  ?tas isoC:label ?typedas .
+  ?var bd:classifiedAs ?cas .
+  ?cas isoC:label ?classifiedas .  
 } ORDER BY ?ordinal
 }
     query_results = Sparql::Query.new.query(query_string, "", [:isoC, :bd])
-    query_results.by_object_set([:var, :ordinal, :type, :label, :typedAs, :name, :desc, :classifiedAs]).each do |x|
+    query_results.by_object_set([:var, :ordinal, :type, :label, :typedas, :name, :desc, :classifiedas]).each do |x|
       results << {uri: x[:var], ordinal: x[:ordinal], rdf_type: x[:type].to_s, label: x[:label], name: x[:name],
-                  description: x[:desc], typedAs: x[:typedAs], classifiedAs: x[:classifiedAs]}
+                  description: x[:desc], typed_as: x[:typedas], classified_as: x[:classifiedas]}
     end
     results
   end
