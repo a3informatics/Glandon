7305cfba9277dc00d23a3b2468ffd11bf1a9f9f4
Clarisa <car@s-cubed.dk>
2020-08-17 08:40:22 +0200

    GLAN-1414
    [1] Add tests

--- spec/controllers/adam_ig_datasets_controller_spec.rb
+++ spec/controllers/adam_ig_datasets_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe AdamIgsController do
+describe AdamIgDatasetsController do
 
   include DataHelpers
   include PauseHelpers
@@ -14,7 +14,7 @@ describe AdamIgsController do
     login_reader
 
     def sub_dir
-      return "controllers/adam_igs"
+      return "controllers/adam_ig_datasets"
     end
 
     before :all do
@@ -31,15 +31,15 @@ describe AdamIgsController do
     end
 
     it "show" do
-      adam_ig = AdamIg.find_minimum(Uri.new(uri: "http://www.assero.co.uk/MDRAdamIgT/CDISC/V1#AIG-CDISC_ADAMIG"))
-      get :show, params: { :id => adam_ig.id}
+      adam_ig_dataset = AdamIgDataset.find_minimum(Uri.new(uri: "http://www.assero.co.uk/MDRAdamIgT/CDISC/V1#AIG-CDISC_ADAMIGBDS"))
+      get :show, params: { :id => adam_ig_dataset.id}
       expect(response).to render_template("show")
     end
 
     it "show results" do
-      adam_ig = AdamIg.find_minimum(Uri.new(uri: "http://www.assero.co.uk/MDRAdamIgT/CDISC/V1#AIG-CDISC_ADAMIG"))
+      adam_ig_dataset = AdamIgDataset.find_minimum(Uri.new(uri: "http://www.assero.co.uk/MDRAdamIgT/CDISC/V1#AIG-CDISC_ADAMIGBDS"))
       request.env['HTTP_ACCEPT'] = "application/json"
-      get :show_data, params:{id: adam_ig.id, adam_ig:{count: 10, offset: 0}}
+      get :show_data, params:{id: adam_ig_dataset.id, adam_ig_dataset:{count: 10, offset: 0}}
       expect(response.content_type).to eq("application/json")
       expect(response.code).to eq("200")
       actual = JSON.parse(response.body).deep_symbolize_keys[:data]
@@ -47,10 +47,10 @@ describe AdamIgsController do
     end
 
     it "shows the history, page" do
-      adam_ig = AdamIg.find_minimum(Uri.new(uri: "http://www.assero.co.uk/MDRAdamIgT/CDISC/V1#AIG-CDISC_ADAMIG"))
+      adam_ig_dataset = AdamIgDataset.find_minimum(Uri.new(uri: "http://www.assero.co.uk/MDRAdamIgT/CDISC/V1#AIG-CDISC_ADAMIGBDS"))
       request.env['HTTP_ACCEPT'] = "application/json"
-      expect(AdamIg).to receive(:history_pagination).with({identifier: adam_ig.has_identifier.identifier, scope: an_instance_of(IsoNamespace), offset: "20", count: "20"}).and_return([adam_ig])
-      get :history, params:{adam_ig: {identifier: adam_ig.has_identifier.identifier, scope_id: "aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTlMjU0NVQkVE", count: 20, offset: 20}}
+      expect(AdamIgDataset).to receive(:history_pagination).with({identifier: adam_ig_dataset.has_identifier.identifier, scope: an_instance_of(IsoNamespace), offset: "20", count: "20"}).and_return([adam_ig_dataset])
+      get :history, params:{adam_ig_dataset: {identifier: adam_ig_dataset.has_identifier.identifier, scope_id: "aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTlMjU0NVQkVE", count: 20, offset: 20}}
       expect(response.content_type).to eq("application/json")
       expect(response.code).to eq("200")
       actual = JSON.parse(response.body).deep_symbolize_keys[:data]
@@ -59,9 +59,9 @@ describe AdamIgsController do
 
     it "shows the history, initial view" do
       params = {}
-      expect(AdamIg).to receive(:latest).and_return(AdamIg.new)
-      get :history, params:{adam_ig: {identifier: "ADAM IG", scope_id: "aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTlMjU0NVQkVE"}}
-      expect(assigns(:identifier)).to eq("ADAM IG")
+      expect(AdamIgDataset).to receive(:latest).and_return(AdamIgDataset.new)
+      get :history, params:{adam_ig_dataset: {identifier: "ADAMIG BDS", scope_id: "aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTlMjU0NVQkVE"}}
+      expect(assigns(:identifier)).to eq("ADAMIG BDS")
       expect(assigns(:scope_id)).to eq("aHR0cDovL3d3dy5hc3Nlcm8uY28udWsvTlMjU0NVQkVE")
       expect(response).to render_template("history")
     end
