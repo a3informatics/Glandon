4114ed25942fffa77312f5c9483d8a48787775e6
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-09-01 08:12:48 +0200

    GLAN-1419
    [1] Fix tests

--- spec/models/adam_ig_dataset_spec.rb
+++ spec/models/adam_ig_dataset_spec.rb
@@ -26,41 +26,4 @@ describe AdamIgDataset do
     check_file_actual_expected(actual, sub_dir, "find_children.yaml", equate_method: :hash_equal)
   end
 
-# =begin
-#   def check_model(result, expected)
-#     expect(result[:children].count).to eq(expected[:children].count)
-#     result[:children].each do |r|
-#       item = expected[:children].find { |e| e[:id] == r[:id] }
-#       expect(item).to_not be_nil
-#       expect(r).to eq(item)
-#     end
-#   end
-# =end
-
-#   it "validates a valid object" do
-#     item = AdamIgDataset.new
-#     ra = IsoRegistrationAuthority.new
-#     ra.uri = "na" # Bit naughty
-#     ra.organization_identifier = "123456789"
-#     ra.international_code_designator = "DUNS"
-#     ra.ra_namespace = IsoNamespace.find(Uri.new(uri:"http://www.assero.co.uk/NS#ACME"))
-#     item.registrationState.registrationAuthority = ra
-#     si = IsoScopedIdentifier.new
-#     si.identifier = "X FACTOR"
-#     item.scopedIdentifier = si
-#     item.ordinal = 1
-#     result = item.valid?
-#     expect(item.rdf_type).to eq("http://www.assero.co.uk/BusinessDomain#IgDataset")
-#     expect(item.errors.full_messages.to_sentence).to eq("")
-#     expect(result).to eq(true)
-#   end
-
-#   it "builds an object" do
-#     input = read_yaml_file(sub_dir, "build_input_1.yaml")
-#     result = AdamIgDataset.build(input)
-#   #Xwrite_yaml_file(result.to_json, sub_dir, "build_expected_1.yaml")
-#     expected = read_yaml_file(sub_dir, "build_expected_1.yaml")
-#     expect(result.to_json).to hash_equal(expected)
-#   end
-
 end
