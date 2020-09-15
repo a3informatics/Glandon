4f45edbd4b5ee2dea93d7f1c84018d457ba016a5
Clarisa <car@s-cubed.dk>
2020-09-15 13:42:54 +0200

    GLAN-1416
    [1] Add update action and tests

--- spec/controllers/forms_controller_spec.rb
+++ spec/controllers/forms_controller_spec.rb
@@ -334,6 +334,113 @@ describe FormsController do
 
   end
 
+    describe "Update" do
+
+    login_curator
+
+    def sub_dir
+      return "controllers/forms"
+    end
+
+    before :all do
+      data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl", "forms/FN000150.ttl"]
+      load_files(schema_files, data_files)
+      load_cdisc_term_versions(1..15)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      @lock_user = ua_add_user(email: "lock@example.com")
+      Token.delete_all
+      @form = Form.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F"))
+    end
+
+    after :all do
+      ua_remove_user("lock@example.com")
+    end
+
+    it "update" do
+      update_params = {label:"label u", note:"note u", completion:"completion u"} 
+      request.env['HTTP_ACCEPT'] = "application/json"
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :update, params:{id: @form.id, form: update_params}
+      expect(AuditTrail.count).to eq(audit_count+1)
+      actual = check_good_json_response(response)
+      check_file_actual_expected(actual, sub_dir, "update_form_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+    it 'update, second update so no audit' do
+      update_params = {label:"label u", note:"note u", completion:"completion u"} 
+      request.env['HTTP_ACCEPT'] = "application/json"
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :update, params:{id: @form.id, form: update_params}
+      expect(AuditTrail.count).to eq(audit_count+1)
+      put :update, params:{id: @form.id, form: update_params}
+      expect(AuditTrail.count).to eq(audit_count+1)
+      actual = check_good_json_response(response)
+      check_file_actual_expected(actual, sub_dir, "update_form_expected_2.yaml", equate_method: :hash_equal)
+    end
+
+    it 'update, locked by another user' do
+      update_params = {label:"label u", note:"note u", completion:"completion u"} 
+      request.env['HTTP_ACCEPT'] = "application/json"
+      token = Token.obtain(@form, @lock_user)
+      audit_count = AuditTrail.count
+      put :update, params:{id: @form.id, form: update_params}
+      expect(AuditTrail.count).to eq(audit_count)
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual, sub_dir, "update_form_expected_3.yaml", equate_method: :hash_equal)
+    end
+
+    it 'update, errors' do
+      update_params = {label:"label ±±±", note:"note ±±±", completion:"completion ±±±"} 
+      request.env['HTTP_ACCEPT'] = "application/json"
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :update, params:{id: @form.id, form: update_params}
+      expect(AuditTrail.count).to eq(audit_count)
+      actual = check_good_json_response(response)
+      check_file_actual_expected(actual, sub_dir, "update_form_expected_4.yaml", equate_method: :hash_equal)
+    end
+
+  end
+
+  # describe "Add child" do
+
+  #   login_curator
+
+  #   def sub_dir
+  #     return "controllers/forms"
+  #   end
+
+  #   before :all do
+  #     data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl", "forms/FN000150.ttl"]
+  #     load_files(schema_files, data_files)
+  #     load_cdisc_term_versions(1..15)
+  #     load_data_file_into_triple_store("mdr_identification.ttl")
+  #     @lock_user = ua_add_user(email: "lock@example.com")
+  #     Token.delete_all
+  #   end
+
+  #   after :all do
+  #     ua_remove_user("lock@example.com")
+  #   end
+
+  #   it 'Add normal group' do
+  #     form = Form.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F"))
+  #     request.env['HTTP_ACCEPT'] = "application/json"
+  #     audit_count = AuditTrail.count
+  #     token = Token.obtain(form, @user)
+  #     post :add_child, params:{id: form.id, form:{type: "normal_group"} }
+  #     expect(response.content_type).to eq("application/json")
+  #     expect(response.code).to eq("200")
+  #     expect(AuditTrail.count).to eq(audit_count + 1)
+  #     expect(JSON.parse(response.body).deep_symbolize_keys[:errors]).to eq(nil)
+  #     actual = JSON.parse(response.body).deep_symbolize_keys[:data]
+  #     check_file_actual_expected(actual.to_h, sub_dir, "add_child_expected_1.yaml", equate_method: :hash_equal, write_file: true)
+  #   end
+
+  # end
+
   # describe "Unauthorized User" do
     
   #   login_reader
