c4368e2e103e70c733351a4f65db07e5e58734e5
Clarisa <car@s-cubed.dk>
2020-09-15 14:39:16 +0200

    GLAN-1416
    [1] Add add child method

--- app/models/form/group.rb
+++ app/models/form/group.rb
@@ -22,4 +22,22 @@ private
     return "<tr><td colspan=\"3\"><h5>#{text}</h5></td></tr>"
   end
 
+  # Next Ordinal. Get the next ordinal for a managed item collection
+  #
+  # @param [String] name the name of the property holding the collection
+  # @return [Integer] the next ordinal
+  def next_ordinal(name)
+    predicate = self.properties.property(name).predicate
+    query_string = %Q{
+      SELECT (MAX(?ordinal) AS ?max)
+      {
+        #{self.uri.to_ref} #{predicate.to_ref} ?s .
+        ?s bf:ordinal ?ordinal
+      }
+    }
+    query_results = Sparql::Query.new.query(query_string, "", [:bf])
+    return 1 if query_results.empty?
+    query_results.by_object(:max).first.to_i + 1
+  end
+
 end
--- spec/models/form/group/normal_spec.rb
+++ spec/models/form/group/normal_spec.rb
@@ -9,103 +9,112 @@ describe Form::Group::Normal do
     return "models/form/group/normal"
   end
 
-  before :all do
-    data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl", "forms/VSTADIABETES.ttl"]
-    load_files(schema_files, data_files)
-    load_cdisc_term_versions(1..65)
-    load_data_file_into_triple_store("mdr_identification.ttl")
+  describe "Validation tests" do
+
+    before :all do
+      data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl", "forms/VSTADIABETES.ttl"]
+      load_files(schema_files, data_files)
+      load_cdisc_term_versions(1..65)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+    end
+
+    it "get items" do
+      group = Form::Group::Normal.find_full(Uri.new(uri: "http://www.s-cubed.dk/VSTADIABETES/V1#F_NG1"))
+      check_file_actual_expected(group.get_item, sub_dir, "get_items_expected.yaml", equate_method: :hash_equal)
+    end
+
+    it "validates a valid object" do
+      item = Form::Group::Normal.new
+      item.uri = Uri.new(uri: "http://www.example.com/A#X")
+      item.note = "OK"
+      item.completion = "Draft 123"
+      item.ordinal = 1
+      result = item.valid?
+      expect(item.errors.full_messages.to_sentence).to eq("")
+      expect(result).to eq(true)
+    end
+
+    it "does not validate an invalid object, completion" do
+      item = Form::Group::Normal.new
+      item.uri = Uri.new(uri: "http://www.example.com/A#X")
+      item.note = "OK"
+      item.completion = "Draft 123£"
+      item.ordinal = 1
+      result = item.valid?
+      expect(item.errors.full_messages.to_sentence).to eq("Completion contains invalid markdown")
+      expect(result).to eq(false)
+    end
+
+    it "does not validate an invalid object, note" do
+      item = Form::Group::Normal.new
+      item.uri = Uri.new(uri: "http://www.example.com/A#X")
+      item.note = "OK±"
+      item.completion = "Draft 123"
+      item.ordinal = 1
+      result = item.valid?
+      expect(item.errors.full_messages.to_sentence).to eq("Note contains invalid markdown")
+      expect(result).to eq(false)
+    end
+
+    it "does not validate an invalid object, repeating" do
+      item = Form::Group::Normal.new
+      item.uri = Uri.new(uri: "http://www.example.com/A#X")
+      item.note = "OK"
+      item.completion = "Draft 123"
+      item.repeating = ""
+      item.ordinal = 1
+      result = item.valid?
+      expect(item.errors.full_messages.to_sentence).to eq("Repeating contains an invalid boolean value")
+      expect(result).to eq(false)
+    end
+
   end
 
-  it "get items" do
-    group = Form::Group::Normal.find_full(Uri.new(uri: "http://www.s-cubed.dk/VSTADIABETES/V1#F_NG1"))
-    check_file_actual_expected(group.get_item, sub_dir, "get_items_expected.yaml", equate_method: :hash_equal)
-  end
+  describe "Add child" do
+    
+    before :each do
+      data_files = ["forms/FN000150.ttl", "forms/VSTADIABETES.ttl","forms/FN000120.ttl", "forms/CRF TEST 1.ttl","biomedical_concept_instances.ttl", "biomedical_concept_templates.ttl" ]
+      load_files(schema_files, data_files)
+      load_cdisc_term_versions(1..15)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+    end
 
-  it "validates a valid object" do
-    item = Form::Group::Normal.new
-    item.uri = Uri.new(uri: "http://www.example.com/A#X")
-    item.note = "OK"
-    item.completion = "Draft 123"
-    item.ordinal = 1
-    result = item.valid?
-    expect(item.errors.full_messages.to_sentence).to eq("")
-    expect(result).to eq(true)
-  end
+    it "add child I, add normal groups" do
+      normal = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      result = normal.add_child({type:"normal_group"})
+      check_file_actual_expected(result.to_h, sub_dir, "add_child_expected.yaml", equate_method: :hash_equal)
+      normal = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      result = normal.add_child({type:"normal_group"})
+      check_file_actual_expected(result.to_h, sub_dir, "add_child_expected_2.yaml", equate_method: :hash_equal)
+    end
 
-  it "does not validate an invalid object, completion" do
-    item = Form::Group::Normal.new
-    item.uri = Uri.new(uri: "http://www.example.com/A#X")
-    item.note = "OK"
-    item.completion = "Draft 123£"
-    item.ordinal = 1
-    result = item.valid?
-    expect(item.errors.full_messages.to_sentence).to eq("Completion contains invalid markdown")
-    expect(result).to eq(false)
-  end
+    it "add child II, error" do
+      normal = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      expect{normal.add_child({type:"x_group"})}.to raise_error(Errors::ApplicationLogicError, "Attempting to add an invalid child type")
+    end
 
-  it "does not validate an invalid object, note" do
-    item = Form::Group::Normal.new
-    item.uri = Uri.new(uri: "http://www.example.com/A#X")
-    item.note = "OK±"
-    item.completion = "Draft 123"
-    item.ordinal = 1
-    result = item.valid?
-    expect(item.errors.full_messages.to_sentence).to eq("Note contains invalid markdown")
-    expect(result).to eq(false)
-  end
+    it "add child III, bc groups" do
+      bci_1 = BiomedicalConceptInstance.find(Uri.new(uri: "http://www.s-cubed.dk/WEIGHT/V1#BCI"))
+      bci_2 = BiomedicalConceptInstance.find(Uri.new(uri: "http://www.s-cubed.dk/BMI/V1#BCI"))
+      bci_3 = BiomedicalConceptInstance.find(Uri.new(uri: "http://www.s-cubed.dk/RACE/V1#BCI"))
+      normal = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      result = normal.add_child({type:"biomedical_concept_instance", id_set:[bci_1.id]})
+      check_file_actual_expected(result, sub_dir, "add_child_expected_3.yaml", equate_method: :hash_equal)
+      normal = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      result = normal.add_child({type:"biomedical_concept_instance", id_set:[bci_2.id, bci_3.id ]})
+      check_file_actual_expected(result, sub_dir, "add_child_expected_4.yaml", equate_method: :hash_equal)
+    end
 
-  it "does not validate an invalid object, repeating" do
-    item = Form::Group::Normal.new
-    item.uri = Uri.new(uri: "http://www.example.com/A#X")
-    item.note = "OK"
-    item.completion = "Draft 123"
-    item.repeating = ""
-    item.ordinal = 1
-    result = item.valid?
-    expect(item.errors.full_messages.to_sentence).to eq("Repeating contains an invalid boolean value")
-    expect(result).to eq(false)
-  end
+    it "add child IV, items" do
+      normal = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      result = normal.add_child({type:"question"})
+      check_file_actual_expected(result.to_h, sub_dir, "add_child_expected_5.yaml", equate_method: :hash_equal)
+      normal = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      result = normal.add_child({type:"placeholder"})
+      check_file_actual_expected(result.to_h, sub_dir, "add_child_expected_6.yaml", equate_method: :hash_equal)
+    end
 
-  # it "allows an object to be exported as XML, no children" do
-  # 	odm = add_root
-  #   study = add_study(odm.root)
-  #   mdv = add_mdv(study)
-  #   form = add_form(mdv)
-  #   item = Form::Group::Normal.new
-  #   item.id = "G-TEST"
-  #   item.label = "test label"
-  #   item.ordinal = 119
-		# item.to_xml(mdv, form)
-		# xml = odm.to_xml
-  # #write_text_file_2(xml, sub_dir, "to_xml_expected_1.xml")
-  #   expected = read_text_file_2(sub_dir, "to_xml_expected_1.xml")
-  #   odm_fix_datetimes(xml, expected)
-  #   odm_fix_system_version(xml, expected)
-  #   expect(xml).to eq(expected)
-  # end
-  
-  # it "allows an object to be exported as XML, children" do
-  # 	odm = add_root
-  #   study = add_study(odm.root)
-  #   mdv = add_mdv(study)
-  #   form = add_form(mdv)
-  #   item_c = Form::Group::Normal.new
-  #   item_c.id = "G-TEST-CHILD"
-  #   item_c.label = "test label child"
-  #   item_c.ordinal = 1
-		# item = Form::Group::Normal.new
-  #   item.id = "G-TEST"
-  #   item.label = "test label"
-  #   item.ordinal = 119
-		# item.groups << item_c
-		# item.to_xml(mdv, form)
-		# xml = odm.to_xml
-  # #write_text_file_2(xml, sub_dir, "to_xml_expected_1.xml")
-  #   expected = read_text_file_2(sub_dir, "to_xml_expected_2.xml")
-  #   odm_fix_datetimes(xml, expected)
-  #   odm_fix_system_version(xml, expected)
-  #   expect(xml).to eq(expected)
-  # end
+  end
   
 end
   
