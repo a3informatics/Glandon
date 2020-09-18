1c5f71842fdbe24af2c500660e995f53d0ebf2a1
Clarisa <car@s-cubed.dk>
2020-09-18 14:30:27 +0200

    GLAN-1416
    [1] Add tests

--- spec/controllers/forms/groups/normal_groups_controller_spec.rb
+++ spec/controllers/forms/groups/normal_groups_controller_spec.rb
@@ -136,4 +136,66 @@ describe Forms::Groups::NormalGroupsController do
 
   end
 
+  describe "Move up/down" do
+    
+    login_curator
+
+    def sub_dir
+      return "controllers/forms/groups"
+    end
+
+    after :all do
+      ua_remove_user("lock@example.com")
+    end
+
+    before :all do
+      data_files = ["forms/FN000150.ttl","forms/FN000120.ttl", "forms/CRF TEST 1.ttl","biomedical_concept_instances.ttl", "biomedical_concept_templates.ttl" ]
+      load_files(schema_files, data_files)
+      load_cdisc_term_versions(1..62)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      @lock_user = ua_add_user(email: "lock@example.com")
+      Token.delete_all
+      @form = Form.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/FN000120/V1#F"))
+    end
+
+    it "Move up I" do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000120/V1#F_NG12"))
+      parent = Form.find(Uri.new(uri: "http://www.s-cubed.dk/FN000120/V1#F"))
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :move_up, params:{id: item.id, normal_group: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count+1)
+      actual = check_good_json_response(response)
+    end
+
+    it "Move up I, Error" do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000120/V1#F_NG1"))
+      parent = Form.find(Uri.new(uri: "http://www.s-cubed.dk/FN000120/V1#F"))
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :move_up, params:{id: item.id, normal_group: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count)
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_up_normal_error_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+    it 'Move up I, Error locked by another user' do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Group::Normal.find(Uri.new(uri: "http://www.s-cubed.dk/FN000120/V1#F_NG12"))
+      parent = Form.find(Uri.new(uri: "http://www.s-cubed.dk/FN000120/V1#F"))
+      token = Token.obtain(@form, @lock_user)
+      audit_count = AuditTrail.count
+      put :move_up, params:{id: item.id, normal_group: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count)
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_up_normal_error_expected_2.yaml", equate_method: :hash_equal)
+    end
+
+  end
+
 end
