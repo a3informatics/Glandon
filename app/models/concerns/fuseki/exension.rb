ccb3302e5c6f7cd5e000ac9b791f959280d0b066
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-09-28 13:47:31 +0200

    GLAN-1424
    [1] Updates

--- app/models/concerns/fuseki/exension.rb
+++ app/models/concerns/fuseki/exension.rb
@@ -16,20 +16,21 @@ module Fuseki
 
     end
 
-    def add_extension(name, datatype, label, definition)
+    def add_extension(name, datatype, label, definition, default)
       sparql = Sparql::Update.new
       uri = Uri.new(namespace: self.class.rdf_type.namespace, fragment: "#{name}")
-      add_extension_definition(sparql, uri, name, datatype, label, definition)
+      add_extension_definition(sparql, uri, datatype, label, definition, default)
     end
 
   private
 
-    def add_extension_definition()
+    def add_extension_definition(sparql, uri, datatype, label, definition, default)
       sparql.add({uri: uri}, {prefix: :rdf, fragment: "type"}, {prefix: :owl, fragment: "DatatypeProperty" })
-      sparql.add({uri: uri}, {prefix: :rdfs, fragment: "domain"}, {uri: self.class.rdf_type.namespace})
-      sparql.add({uri: uri}, {prefix: :rdfs, fragment: "range"}, {uri: datatype})
-      sparql.add({uri: uri}, {prefix: :rdfs, fragment: "label"}, {{literal: "#{datatype.to_literal(label)}", primitive_type: "string"}})
-      sparql.add({uri: uri}, {prefix: :rdfs, fragment: "definition"}, {{literal: "#{datatype.to_literal(definition)}", primitive_type: "string"}})
+      sparql.add({uri: uri}, {prefix: :rdfs, fragment: "domain"}, {uri: self.class.rdf_type})
+      sparql.add({uri: uri}, {prefix: :rdfs, fragment: "range"}, {uri: datatype.to_uri.to_ref})
+      sparql.add({uri: uri}, {prefix: :rdfs, fragment: "label"}, {{literal: label, primitive_type: XSDDatatype.string}})
+      sparql.add({uri: uri}, {prefix: :rdfs, fragment: "definition"}, {{literal: "definition}", primitive_type: XSDDatatype.string}})
+      sparql.add({uri: uri}, {prefix: :isoC, fragment: "extensionPropertyDefaultValue"}, {{literal: "#{datatype.to_literal(default)}", primitive_type: datatype}})
     end
 
     def add_extension_defaults()
@@ -45,6 +46,8 @@ module Fuseki
         WHERE { ?s rdf:type #{self.class.rdf_type.to_ref} }
       }
     end
+
+    
 
   end
 
