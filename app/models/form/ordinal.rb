9e5852476907c0ecd59b56e4bcce1b171694ee80
Clarisa <car@s-cubed.dk>
2020-09-28 14:24:22 +0200

    GLAN-1416
    [1] Tweak method

--- app/models/form/ordinal.rb
+++ app/models/form/ordinal.rb
@@ -3,19 +3,21 @@ class Form
   module Ordinal
     
     def reset_ordinals(parent)
-      string_uris = ""
-      uris_ordered(parent).each_with_index do |s, index|
-        string_uris += "#{s.to_ref} bf:ordinal #{index+1} . "
+      unless uris_ordered(parent).empty?
+        string_uris = ""
+        uris_ordered(parent).each_with_index do |s, index|
+          string_uris += "#{s.to_ref} bf:ordinal #{index+1} . "
+        end
+        query_string = %Q{
+          DELETE 
+          { ?s bf:ordinal ?x . }
+          INSERT
+          { #{string_uris} }
+          WHERE 
+          { ?s bf:ordinal ?x . }
+        }
+        results = Sparql::Update.new.sparql_update(query_string, "", [:bf])
       end
-      query_string = %Q{
-        DELETE 
-        { ?s bf:ordinal ?x . }
-        INSERT
-        { #{string_uris} }
-        WHERE 
-        { ?s bf:ordinal ?x . }
-      }
-      results = Sparql::Update.new.sparql_update(query_string, "", [:bf])
     end
 
     def uris_ordered(parent)
