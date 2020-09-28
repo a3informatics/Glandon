7de5fc5a76e6b9cfba4d97796928b217ee776534
Clarisa <car@s-cubed.dk>
2020-09-28 11:16:34 +0200

    GLAN-1416
    [1] Update test

--- spec/controllers/forms_controller_spec.rb
+++ spec/controllers/forms_controller_spec.rb
@@ -109,60 +109,6 @@ describe FormsController do
       expect(flash[:error]).to match(/The item is locked for editing by user: lock@example.com./)
       expect(response).to redirect_to("/path")
     end
-    # it "provides a new object" do
-    #   get :new
-    #   result = assigns[:form]
-    #   expected = Form.new
-    #   expected.creationDate = result.creationDate
-    #   expected.lastChangeDate = result.lastChangeDate
-    #   expect(result.to_json).to eq(expected.to_json)
-    #   expect(response).to render_template("new")
-    # end
-
-    # it "lists all unique forms, HTML" do
-    #   get :index
-    #   expect(assigns[:forms].count).to eq(4)
-    #   expect(response).to render_template("index")
-    # end
-    
-    # it "lists all unique forms, JSON" do  
-    #   request.env['HTTP_ACCEPT'] = "application/json"
-    #   get :index
-    #   expect(response.content_type).to eq("application/json")
-    #   expect(response.code).to eq("200")
-    # #Xwrite_text_file_2(response.body, sub_dir, "forms_controller_index.txt")
-    #   expected = read_text_file_2(sub_dir, "forms_controller_index.txt")
-    #   expect(response.body).to eq(expected)
-
-    # end
-
-    # it "shows the history" do
-    #   get :history, { :identifier => "DM1 01", :scope_id => IsoRegistrationAuthority.owner.ra_namespace.id }
-    #   expect(response).to render_template("history")
-    # end
-
-    # it "shows the history, redirects when empty" do
-    #   get :history, { :identifier => "DM1 01X", :scope_id => IsoRegistrationAuthority.owner.ra_namespace.id }
-    #   expect(response).to redirect_to("/forms")
-    # end
-
-    # it "initiates the creation of a new placeholder form" do
-    #   get :placeholder_new
-    #   expect(assigns[:form].to_json).to eq(Form.new.to_json)
-    #   expect(response).to render_template("placeholder_new")
-    # end
-
-    # it "creates the placeholder form" do
-    #   audit_count = AuditTrail.count
-    #   form_count = Form.all.count
-    #   post :placeholder_create, form: { :identifier => "NEW TH", :label => "New TH Form", :freeText => "* List Item 1\n* List Item 2\n\nThis form is required to do the following:\n\n* Collect the date" }
-    #   form = assigns(:form)
-    #   expect(form.errors.count).to eq(0)
-    #   expect(Form.unique.count).to eq(form_count + 1) 
-    #   expect(flash[:success]).to be_present
-    #   expect(AuditTrail.count).to eq(audit_count + 1)
-    #   expect(response).to redirect_to("/forms")
-    # end
 
     # it "edit, no next version" do
     #   get :edit, { :id => "F-ACME_NEWTH", :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
@@ -234,74 +180,6 @@ describe FormsController do
     #   expect(form.errors.count).to eq(1)
     #   expect(flash[:error]).to be_present
     #   expect(response).to redirect_to("/forms/clone?id=F-ACME_DM101&namespace=http%3A%2F%2Fwww.assero.co.uk%2FMDRForms%2FACME%2FV1")
-    # end
-
-    # it "initiates the branching of a form" do
-    #   get :branch, { :id => "F-ACME_DM1BRANCH", :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
-    #   form = assigns(:form)
-    #   expect(form.id).to eq("F-ACME_DM1BRANCH")
-    #   expect(response).to render_template("branch")
-    # end
-
-    # it "branches a form" do
-    #   audit_count = AuditTrail.count
-    #   form_count = Form.unique.count
-    #   post :branch_create,  { form: { :identifier => "BRANCH", :label => "New Branch" }, :form_id => "F-ACME_DM1BRANCH", :form_namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
-    #   form = assigns(:form)
-    #   expect(form.errors.count).to eq(0)
-    #   expect(Form.unique.count).to eq(form_count + 1) 
-    #   expect(flash[:success]).to be_present
-    #   expect(AuditTrail.count).to eq(audit_count + 1)
-    #   expect(response).to redirect_to("/forms")
-    #   item = Form.find("F-ACME_BRANCH", "http://www.assero.co.uk/MDRForms/ACME/V1")
-    #   expect(item.is_a_branch?).to eq(true)
-    # end
-
-    # it "branches a form, error duplicate" do
-    #   audit_count = AuditTrail.count
-    #   form_count = Form.all.count
-    #   post :branch_create,  { form: { :identifier => "BRANCH", :label => "New Branch" }, :form_id => "F-ACME_DM1BRANCH", :form_namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
-    #   form = assigns(:form)
-    #   expect(form.errors.count).to eq(1)
-    #   expect(flash[:error]).to be_present
-    #   expect(response).to redirect_to("/forms/branch?id=F-ACME_DM1BRANCH&namespace=http%3A%2F%2Fwww.assero.co.uk%2FMDRForms%2FACME%2FV1")
-    # end
-
-    # it "creates"
-    
-    # it "updates"
-
-    # it "destroy" do
-    #   @request.env['HTTP_REFERER'] = 'http://test.host/forms'
-    #   audit_count = AuditTrail.count
-    #   form_count = Form.all.count
-    #   token_count = Token.all.count
-    #   delete :destroy, { :id => "F-ACME_CLONE", :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
-    #   expect(Form.all.count).to eq(form_count - 1)
-    #   expect(AuditTrail.count).to eq(audit_count + 1)
-    #   expect(Token.count).to eq(token_count)
-    # end
-
-    # it "show" do
-    #   get :show, { :id => "F-ACME_DM101", :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
-    #   expect(response).to render_template("show")
-    # end
-
-    # it "view " do
-    #   get :view, { :id => "F-ACME_DM101", :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
-    #   expect(response).to render_template("view")
-    # end
-
-    # it "export_ttl" do
-    #   get :export_ttl, { :id => "F-ACME_DM101", :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
-    # end
-
-    # it "export_json" do
-    #   get :export_json, { :id => "F-ACME_DM101", :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
-    # end
-
-    # it "export_odm" do
-    #   get :export_odm, { :id => "F-ACME_DM101", :namespace => "http://www.assero.co.uk/MDRForms/ACME/V1" }
     # end
     
     # it "presents acrf as pdf" do
@@ -434,7 +312,7 @@ describe FormsController do
       expect(AuditTrail.count).to eq(audit_count + 1)
       expect(JSON.parse(response.body).deep_symbolize_keys[:errors]).to eq(nil)
       actual = JSON.parse(response.body).deep_symbolize_keys[:data]
-      check_file_actual_expected(actual.to_h, sub_dir, "add_child_expected_1.yaml", equate_method: :hash_equal, write_file: true)
+      check_file_actual_expected(actual.to_h, sub_dir, "add_child_expected_1.yaml", equate_method: :hash_equal)
     end
 
   end
