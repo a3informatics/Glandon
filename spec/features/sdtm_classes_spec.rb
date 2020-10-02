a811d4fefe17005ffed00037fe3bcef5d7a409d4
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-10-02 14:05:24 +0200

    GLAN-1416
    [1] Fix feature tests resulting from improved data
    [2] Remove old data files

--- spec/features/sdtm_classes_spec.rb
+++ spec/features/sdtm_classes_spec.rb
@@ -11,8 +11,13 @@ describe "SDTM Classes", :type => :feature do
   describe "Basic Operations, curator", :type => :feature, js:true do
 
     before :all do
-      data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl", "sdtm/SDTM_Model_1-4.ttl"]
+      data_files = []
       load_files(schema_files, data_files)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      load_data_file_into_triple_store("mdr_iso_concept_systems.ttl")
+      load_data_file_into_triple_store("mdr_iso_concept_systems_migration_1.ttl")
+      load_data_file_into_triple_store("mdr_iso_concept_systems_migration_2.ttl")
+      load_data_file_into_triple_store("cdisc/sdtm_model/SDTM_MODEL_V3.ttl")
       ua_create
     end
 
@@ -33,35 +38,35 @@ describe "SDTM Classes", :type => :feature do
       wait_for_ajax 20
       expect(page).to have_content 'Index: SDTM Classes'
       find(:xpath, "//th[contains(.,'Identifier')]").click # Order
-      ui_check_table_info("index", 1, 7, 7)
-      ui_check_table_cell("index", 3, 2, "SDTMMODEL FINDINGS ABOUT")
-      ui_check_table_cell("index", 3, 3, "Findings About")
+      ui_check_table_info("index", 1, 10, 18)
+      ui_check_table_cell("index", 3, 2, "SDTM MODEL EVENTS")
+      ui_check_table_cell("index", 3, 3, "SDTM MODEL EVENTS")
     end
 
     it "allows the history page to be viewed", js:true do
       click_navbar_sdtm_class
       wait_for_ajax 10
-      ui_table_search('index', 'SDTMMODEL EVENTS')
-      find(:xpath, "//tr[contains(.,'SDTMMODEL EVENTS')]/td/a", :text => 'History').click
+      ui_table_search('index', 'SDTM MODEL EVENTS')
+      find(:xpath, "//tr[contains(.,'SDTM MODEL EVENTS')]/td/a", :text => 'History').click
       wait_for_ajax 10
-      expect(page).to have_content 'Version History of \'SDTMMODEL EVENTS\''
+      expect(page).to have_content 'Version History of \'SDTM MODEL EVENTS\''
       ui_check_table_cell("history", 1, 1, "1.4.0")
-      ui_check_table_cell("history", 1, 5, "Events")
+      ui_check_table_cell("history", 1, 5, "SDTM MODEL EVENTS")
       ui_check_table_cell("history", 1, 7, "Standard")
     end
 
     it "history allows the show page to be viewed (REQ-MDR-BC-010)", js:true do
       click_navbar_sdtm_class
       wait_for_ajax 10
-      ui_table_search('index', 'SDTMMODEL EVENTS')
-      find(:xpath, "//tr[contains(.,'SDTMMODEL EVENTS')]/td/a", :text => 'History').click
+      ui_table_search('index', 'SDTM MODEL EVENTS')
+      find(:xpath, "//tr[contains(.,'SDTM MODEL EVENTS')]/td/a", :text => 'History').click
       wait_for_ajax 10
-      context_menu_element('history', 4, 'SDTMMODEL EVENTS', :show)
+      context_menu_element('history', 4, 'SDTM MODEL EVENTS', :show)
       wait_for_ajax 10
       expect(page).to have_content 'Show: SDTM Class'
-      ui_check_table_info("show", 1, 10, 85)
-      ui_check_table_cell("show", 5, 2, "SPDEVID")
-      ui_check_table_cell("show", 5, 3, "Sponsor Device Identifier")
+      ui_check_table_info("show", 1, 10, 81)
+      ui_check_table_cell("show", 5, 2, "--SPID")
+      ui_check_table_cell("show", 5, 3, "Sponsor-Defined Identifier")
     end
 
   end
--- spec/features/sdtm_ig_domains_spec.rb
+++ spec/features/sdtm_ig_domains_spec.rb
@@ -11,8 +11,13 @@ describe "SDTM IG Domains", :type => :feature do
   describe "Basic Operations, curator", :type => :feature, js:true do
 
     before :all do
-      data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl", "sdtm/SDTM_IG_3-2.ttl"]
+      data_files = []
       load_files(schema_files, data_files)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      load_data_file_into_triple_store("mdr_iso_concept_systems.ttl")
+      load_data_file_into_triple_store("mdr_iso_concept_systems_migration_1.ttl")
+      load_data_file_into_triple_store("mdr_iso_concept_systems_migration_2.ttl")
+      load_data_file_into_triple_store("cdisc/sdtm_ig/SDTM_IG_V3.ttl")
       ua_create
     end
 
@@ -32,7 +37,7 @@ describe "SDTM IG Domains", :type => :feature do
       click_navbar_ig_domain
       wait_for_ajax 20
       expect(page).to have_content 'Index: SDTM IG Domains'
-      ui_check_table_info("index", 1, 10, 41)
+      ui_check_table_info("index", 1, 10, 48)
       find(:xpath, "//th[contains(.,'Identifier')]").click # Order
       ui_check_table_cell("index", 1, 2, "SDTM IG AE")
       ui_check_table_cell("index", 1, 3, "Adverse Events")
