b4980b97f2b96b9d2759af969a07b29543f65303
Clarisa <car@s-cubed.dk>
2020-10-02 14:38:46 +0200

    GLAN-1416
    [1] Add move down controller tests

--- spec/controllers/forms/items/questions_controller_spec.rb
+++ spec/controllers/forms/items/questions_controller_spec.rb
@@ -124,7 +124,7 @@ describe Forms::Items::QuestionsController do
 
   end
 
-  describe "Move up/down" do
+  describe "Move up" do
     
     login_curator
 
@@ -183,6 +183,69 @@ describe Forms::Items::QuestionsController do
       expect(AuditTrail.count).to eq(audit_count)
       actual = check_error_json_response(response)
       check_file_actual_expected(actual.to_h, sub_dir, "move_up_error_expected_2.yaml", equate_method: :hash_equal)
+    end
+
+  end
+
+  describe "Move down" do
+    
+    login_curator
+
+    def sub_dir
+      return "controllers/forms/items"
+    end
+
+    after :all do
+      ua_remove_user("lock@example.com")
+    end
+
+    before :all do
+      data_files = ["forms/FN000150.ttl","forms/FN000120.ttl", "forms/CRF TEST 1.ttl","biomedical_concept_instances.ttl", "biomedical_concept_templates.ttl" ]
+      load_files(schema_files, data_files)
+      load_cdisc_term_versions(1..62)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      @lock_user = ua_add_user(email: "lock@example.com")
+      Token.delete_all
+      @form = Form.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F"))
+    end
+
+    it "Move down I, Question" do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Question.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1_Q1"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :move_down, params:{id: item.id, question: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count+1)
+      actual = check_good_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_down_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+    it "Move down I, Question, Error" do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Question.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1_Q4"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :move_down, params:{id: item.id, question: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count)
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_down_error_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+    it 'Move down I, Question, Error locked by another user' do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Question.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1_PL2"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/FN000150/V1#F_NG1"))
+      token = Token.obtain(@form, @lock_user)
+      audit_count = AuditTrail.count
+      put :move_down, params:{id: item.id, question: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count)
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_down_error_expected_2.yaml", equate_method: :hash_equal)
     end
 
   end
