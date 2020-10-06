0429cb4e9408134c6d57c124c6ece85a58cf8c52
Clarisa <car@s-cubed.dk>
2020-10-06 10:17:01 +0200

    GLAN-1416
    [1] Update mappings controller
    [2] Add move up/down controller tests

--- app/controllers/forms/items/mappings_controller.rb
+++ app/controllers/forms/items/mappings_controller.rb
@@ -26,12 +26,13 @@ class Forms::Items::MappingsController < ManagedItemsController
     form = Form.find_minimum(the_params[:form_id])
     return true unless check_lock_for_item(form)
     mapping = Form::Item::Mapping.find(protect_from_bad_id(params))
-    mapping = mapping.move_up(the_params[:parent_id])
-    if mapping.errors.empty?
+    parent = IsoConceptV2.find(the_params[:parent_id])
+    result = parent.move_up(mapping)
+    if parent.errors.empty?
       AuditTrail.update_item_event(current_user, form, form.audit_message(:updated)) if @lock.first_update?
       render :json => {data: ""}, :status => 200
     else
-      render :json => {:errors => mapping.errors.full_messages}, :status => 422
+      render :json => {:errors => parent.errors.full_messages}, :status => 422
     end
   end
 
@@ -39,12 +40,13 @@ class Forms::Items::MappingsController < ManagedItemsController
     form = Form.find_minimum(the_params[:form_id])
     return true unless check_lock_for_item(form)
     mapping = Form::Item::Mapping.find(protect_from_bad_id(params))
-    mapping = mapping.move_down(the_params[:parent_id])
-    if mapping.errors.empty?
+    parent = IsoConceptV2.find(the_params[:parent_id])
+    result = parent.move_down(mapping)
+    if parent.errors.empty?
       AuditTrail.update_item_event(current_user, form, form.audit_message(:updated)) if @lock.first_update?
       render :json => {data: ""}, :status => 200
     else
-      render :json => {:errors => mapping.errors.full_messages}, :status => 422
+      render :json => {:errors => parent.errors.full_messages}, :status => 422
     end
   end
 
--- spec/controllers/forms/items/mappings_controller_spec.rb
+++ spec/controllers/forms/items/mappings_controller_spec.rb
@@ -82,4 +82,132 @@ describe Forms::Items::MappingsController do
 
   end
 
+  describe "Move up" do
+    
+    login_curator
+
+    def sub_dir
+      return "controllers/forms/items"
+    end
+
+    after :all do
+      ua_remove_user("lock@example.com")
+    end
+
+    before :all do
+      data_files = ["forms/CRF TEST 1.ttl","biomedical_concept_instances.ttl", "biomedical_concept_templates.ttl" ]
+      load_files(schema_files, data_files)
+      load_cdisc_term_versions(1..62)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      @lock_user = ua_add_user(email: "lock@example.com")
+      Token.delete_all
+      @form = Form.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F"))
+    end
+
+    it "Move up I" do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Mapping.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3_MA3"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3"))
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :move_up, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count+1)
+      actual = check_good_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_up_mapping_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+    it "Move up I, Error" do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Mapping.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3_MA3"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3"))
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :move_up, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      put :move_up, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      put :move_up, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_up_mapping_error_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+    it 'Move up I, Error locked by another user' do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Mapping.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3_MA3"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3"))
+      token = Token.obtain(@form, @lock_user)
+      audit_count = AuditTrail.count
+      put :move_up, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count)
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_up_mapping_error_expected_2.yaml", equate_method: :hash_equal)
+    end
+
+  end
+
+  describe "Move down" do
+    
+    login_curator
+
+    def sub_dir
+      return "controllers/forms/items"
+    end
+
+    after :all do
+      ua_remove_user("lock@example.com")
+    end
+
+    before :all do
+      data_files = ["forms/CRF TEST 1.ttl","biomedical_concept_instances.ttl", "biomedical_concept_templates.ttl" ]
+      load_files(schema_files, data_files)
+      load_cdisc_term_versions(1..62)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      @lock_user = ua_add_user(email: "lock@example.com")
+      Token.delete_all
+      @form = Form.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F"))
+    end
+
+    it "Move up I" do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Mapping.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3_MA3"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3"))
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :move_down, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count+1)
+      actual = check_good_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_down_mapping_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+    it "Move up I, Error" do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Mapping.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3_MA3"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3"))
+      token = Token.obtain(@form, @user)
+      audit_count = AuditTrail.count
+      put :move_down, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      put :move_down, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      put :move_down, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_down_mapping_error_expected_1.yaml", equate_method: :hash_equal)
+    end
+
+    it 'Move up I, Error locked by another user' do
+      request.env['HTTP_ACCEPT'] = "application/json"
+      request.content_type = 'application/json'
+      item = Form::Item::Mapping.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3_MA3"))
+      parent = Form::Group.find(Uri.new(uri: "http://www.s-cubed.dk/CRF_TEST_1/V1#F_NG3"))
+      token = Token.obtain(@form, @lock_user)
+      audit_count = AuditTrail.count
+      put :move_down, params:{id: item.id, mapping: {parent_id: parent.id , form_id: @form.id}}
+      expect(AuditTrail.count).to eq(audit_count)
+      actual = check_error_json_response(response)
+      check_file_actual_expected(actual.to_h, sub_dir, "move_down_mapping_error_expected_2.yaml", equate_method: :hash_equal)
+    end
+
+  end
+
 end
