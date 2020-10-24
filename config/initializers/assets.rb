b7aa48bc3e48d99dd29679d5b651725719c9f220
Samuel Banas <sab@s-cubed.dk>
2020-10-24 12:15:41 +0200

    GLAN-1444
    [1] Clean-up, removed unused files, references

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,7 +14,7 @@ Rails.application.config.assets.version = '1.0'
   shared/datatables_loading
   biomedical_concepts_editor
   shared/d3/d3_tree
-  homes panel_collapse print spinner tags
+  homes print spinner tags
  ).each do |filename|
   Rails.application.config.assets.precompile += ["#{filename}.css"]
 end
@@ -25,7 +25,6 @@ end
     export/start_panel
     forms/form_utility forms/form_viewer
     import/crfs/new import/terms/new import/show
-    iso_concept_systems/edit_tags iso_concept_systems/index iso_concept_systems/managed_tags_panel_v2
     iso_managed/impact iso_managed/iso_managed_attributes iso_managed/iso_managed_comment_edit iso_managed/iso_managed_select_panel
     iso_managed/list_change_notes iso_managed/status
     markdown_engines/markdown_editor
@@ -37,10 +36,9 @@ end
 
     shared/annotation/change_instruction_edit shared/annotation/change_instructions_html shared/annotation/change_instruction_modal shared/annotation/change_notes_modal
     shared/cdisc_term/cdisc_selector_modal shared/cdisc_term/index_panel
-    shared/d3/d3_impact_graph shared/d3/d3_tree_v2 shared/d3/d3_editor_new shared/d3/d3_editor shared/d3/d3_tree
+    shared/d3/d3_impact_graph shared/d3/d3_editor shared/d3/d3_tree
     shared/impact/changes_cdisc_panel shared/impact/impact_graph
     shared/import/crf_files_panel shared/import/items_panel
-    shared/iso_concept_systems/concept_system_view_panel_v2 shared/iso_concept_systems/iso_concept_list shared/iso_concept_systems/iso_concept_tagging
     shared/iso_managed/children_panel shared/iso_managed/comments_panel shared/iso_managed/managed_children_overview shared/iso_managed/managed_children_panel
     shared/iso_managed/managed_children_select shared/iso_managed/managed_item_ico_list shared/iso_managed/managed_item_version_picker shared/iso_managed/managed_item_select_modal
     shared/items_selector/items_selector_modal shared/items_selector/managed_item_selector shared/items_selector/unmanaged_item_selector
@@ -54,7 +52,7 @@ end
     shared/list_change_notes_panel shared/tabs_layout shared/timer
 
     ajax_requests colour field_validation
-    locked_items panel_collapse rspec_helper sidebar_handler spinner standard_datatable thesauri_field_editor
+    locked_items rspec_helper sidebar_handler spinner standard_datatable thesauri_field_editor
     token_timer unload_v2 unload
 
   ).each do |filename|
