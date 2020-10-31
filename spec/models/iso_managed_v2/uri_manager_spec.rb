247c7bdd30c1add09d02dd0caa507b658c256ef2
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-10-31 12:13:46 +0100

    GLAN-1446
    [1] Add URI management and tests

--- spec/models/iso_managed_v2/uri_manager_spec.rb
+++ spec/models/iso_managed_v2/uri_manager_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe UriManager do
+describe IsoManagedV2::UriManager do
 
 	include DataHelpers
 
 	def sub_dir
-    return "models/uri_manager"
+    return "models/iso_managed_v2/uri_manager"
   end
 
   describe "basic tests" do
@@ -15,22 +15,21 @@ describe UriManager do
     end
 
     it "create a class, empty hash" do
-      uri_manager = UriManager.new
-      check_file_actual_expected(uri_manager.to_h, sub_dir, "new_expected_1.yaml")
+      uri_manager = IsoManagedV2::UriManager.new
+      check_file_actual_expected(uri_manager.to_ids, sub_dir, "new_expected_1.yaml")
     end
 
     it "adds uris and converts to hash" do
-      uris = UriManager.new
+      uris = IsoManagedV2::UriManager.new
       old_uri_1 = Uri.new(uri: "http://www.assero.co.uk/A#old1")
       new_uri_1 = Uri.new(uri: "http://www.assero.co.uk/A#new1")
       old_uri_2 = Uri.new(uri: "http://www.assero.co.uk/A#old2")
       new_uri_2 = Uri.new(uri: "http://www.assero.co.uk/A#new2")
       uris.add(old_uri_1, new_uri_1)
       uris.add(old_uri_2, new_uri_2)
-      check_file_actual_expected(uris.to_h, sub_dir, "to_h_expected_1.yaml", equate_method: :hash_equal)
+      check_file_actual_expected(uris.to_ids, sub_dir, "to_ids_expected_1.yaml", equate_method: :hash_equal)
     end
 
   end
-
 
 end
