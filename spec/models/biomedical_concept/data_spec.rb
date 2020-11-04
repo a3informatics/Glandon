a3825339557683bfe5690842c22865d43ccbec80
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-11-04 11:31:14 +0100

    GLAN-1439
    [1] Push test script

--- spec/models/biomedical_concept/data_spec.rb
+++ spec/models/biomedical_concept/data_spec.rb
@@ -129,7 +129,7 @@ describe BiomedicalConcept do
     sparql.default_namespace(results.first.uri.namespace)
     results.each{|x| x.to_sparql(sparql, true)}
     full_path = sparql.to_file
-  copy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "biomedical_concept_templates.ttl")
+  #Xcopy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "biomedical_concept_templates.ttl")
 	end
 
   it "create instances" do
