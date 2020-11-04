7afc10c735f0d65a005bf4dd7a18999801630e15
Samuel Banas <sab@s-cubed.dk>
2020-11-04 13:34:03 +0100

    GLAN-1424
    [1] Subset Edit page - moved editor HTML into a partial
    [2] Edit JS pack - added initializers for rank and edit properties classes

--- app/javascript/packs/thesauri/managed_concepts/subsets/edit.js
+++ app/javascript/packs/thesauri/managed_concepts/subsets/edit.js
@@ -4,15 +4,27 @@ import SubsetEditor from 'shared/custom/thesauri/managed_concepts/subsets/editor
 
 $(document).ready(() => {
 
+  // Tabs layout inititalize (once)
   TabsLayout.initialize();
 
+  // Edit Lock timer start
   let tt = new TokenTimer({
     tokenId: timerTokenId,
     warningTime: timerWarning
   });
 
-  let set = new SubsetEditor({
-    urls: subsetEditorUrls
+  // Subset Editor init
+  let se = new SubsetEditor({
+    urls: subsetEditorUrls,
+    onEdit: () => tt.extend()
   })
+
+  // Rank Handler init TODO: Convert to module
+  let rank = new RankModal( () => tt.extend() );
+
+  // Edit Properties on click handler TODO: Convert to module 
+  $( '#edit-properties' ).on( 'click', () =>
+      new EditProperties( epItemsubset, 'subset', 'ManagedConcept', null).show()
+  );
 
 });
