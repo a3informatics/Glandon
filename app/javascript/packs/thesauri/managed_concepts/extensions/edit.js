8c5e2cfcd879b0ad6594a047a5a77b6ea6eb61e6
Samuel Banas <sab@s-cubed.dk>
2020-11-05 16:14:53 +0100

    GLAN-1424
    [1] Revamp and restructure of the Edit Extension page
    [2] Added an ExtensionEditor module which extends the CodeList Editor module (code reusability)

--- app/javascript/packs/thesauri/managed_concepts/extensions/edit.js
+++ app/javascript/packs/thesauri/managed_concepts/extensions/edit.js
@@ -1,5 +1,6 @@
 import TokenTimer from 'shared/custom/tokens/token_timer'
 import PairHandler from 'shared/custom/thesauri/managed_concepts/pair_handler'
+import ExtensionEditor from 'shared/custom/thesauri/managed_concepts/extensions/editor'
 //TODO: Convert dependencies to ES6 modules
 
 $(document).ready(() => {
@@ -11,30 +12,22 @@ $(document).ready(() => {
   });
 
   // Extension Editor
-  const editUrls = {
-    loadUrl: showDataUrl,
-    updateUrl: extensionUrl,
-    newChildUrl: newChildUrl,
-    newSynChildUrl: newChildFromSynUrl,
-    destroyChildUrl: destroyChildUrl
-  };
-
-  let eep = new EditExtensionPanel(editUrls, extensionId, 1000, () => tt.extend() );
-
-  // Search and select
-  let sm = new TermSearchModal(eep.addToExtension.bind(eep), searchUrl);
-  let mis = new ManagedItemsSelect( (v) => setTimeout(sm.initAndShow.bind(sm, v), 600) );
+  let ee = new ExtensionEditor({
+    id: extensionId,
+    urls: extensionEditorUrls,
+    onEdited: () => tt.extend()
+  });
 
   // Init Rank
   let rankModal = new RankModal( () => tt.extend() );
 
-  // Edit Extension Properties
-  $("#edit-properties-button").on("click", () => {
-    new EditProperties(epItemextension, "extension", "ManagedConcept", null).show();
-  });
+  // Edit Properties on click handler TODO: Convert to module
+  $( '#edit-properties-button' ).on( 'click', () =>
+    new EditProperties( epItemextension, 'extension', 'ManagedConcept', null).show()
+  );
 
   // Init Pairing handler
-  let ps = new PairHandler({
+  let ph = new PairHandler({
     pairUrl: pairSelectPath,
     unpairUrl: unpairPath,
     isPaired: isItemPaired
