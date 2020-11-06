0a33c693c5764763487d410e3070acee5f9d1c8c
Samuel Banas <sab@s-cubed.dk>
2020-11-06 11:05:25 +0100

    GLAN-1424
    [1] Added Properties Editor to Extension Edit page

--- app/javascript/packs/thesauri/managed_concepts/extensions/edit.js
+++ app/javascript/packs/thesauri/managed_concepts/extensions/edit.js
@@ -1,6 +1,7 @@
 import TokenTimer from 'shared/custom/tokens/token_timer'
 import PairHandler from 'shared/custom/thesauri/managed_concepts/pair_handler'
 import ExtensionEditor from 'shared/custom/thesauri/managed_concepts/extensions/editor'
+import PropertiesEditor from 'shared/custom/thesauri/managed_concepts/properties_editor'
 //TODO: Convert dependencies to ES6 modules
 
 $(document).ready(() => {
@@ -21,10 +22,10 @@ $(document).ready(() => {
   // Init Rank
   let rankModal = new RankModal( () => tt.extend() );
 
-  // Edit Properties on click handler TODO: Convert to module
-  $( '#edit-properties-button' ).on( 'click', () =>
-    new EditProperties( epItemextension, 'extension', 'ManagedConcept', null).show()
-  );
+  // Extension Properties Editor 
+  let pe = new PropertiesEditor({
+    data: JSON.parse( editItemPropertiesData )
+  });
 
   // Init Pairing handler
   let ph = new PairHandler({
