f4a5e88c833a32e38f3b01ed8e15b6f1cc3ce835
Clarisa <car@s-cubed.dk>
2020-11-09 11:51:31 +0100

    GLAN-1448
    [1] Update data_spec.rb
    [2] Add templates
    [3] Update instances.ttl

--- spec/models/biomedical_concept/data_spec.rb
+++ spec/models/biomedical_concept/data_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe BiomedicalConcept do
-	
-	include DataHelpers
+  
+  include DataHelpers
   include PublicFileHelpers
 
   def sub_dir
@@ -48,16 +48,46 @@ describe BiomedicalConcept do
     bc_template.has_item.find{|x| x.label == label}
   end
 
-  def create_property(params, t_cdt)
+  def create_complex_datatype(params, cdt_template, t_item)
+    t_cdt = nil
+    template = t_item.nil? ? true : false
+    cdt = BiomedicalConcept::ComplexDatatype.new(label: cdt_template.short_name)
+    cdt.is_complex_datatype = cdt_template
+    unless t_item.nil?
+      t_item.has_complex_datatype_objects
+      t_cdt = t_item.has_complex_datatype.find{|x| x.label == cdt_template.short_name}
+    else
+      t_cdt = cdt_template
+    end
+    params[:has_property].each do |property|
+      cdt.has_property_push(create_property(property, t_cdt, template))
+    end
+    puts "CDT Alias=#{cdt.to_h[:has_property].map{|x| x[:alias]}}"
+    cdt
+  end
+
+  def find_complex_datatype(short_name)
+    return @cdt_set[short_name] if @cdt_set.key?(short_name)
+    cdt = ComplexDatatype.where(short_name: short_name)
+    cdt = ComplexDatatype.find_children(cdt.first.uri)
+    @cdt_set[short_name] = cdt
+    cdt
+  end
+
+  def create_property(params, t_cdt, template)
     params = params.merge(format: "", question_test: "", prompt_text: "") if t_cdt.nil?
     #puts "CP Params=#{params}"
-    params = params.merge(alias: "xxx") unless t_cdt.nil?
+    params = params.merge(alias: "xxx") unless template
     refs = params[:has_coded_value].blank? ? [] : params[:has_coded_value].dup
     params[:has_coded_value] = []
     property = BiomedicalConcept::PropertyX.new(label: params[:label], question_text: params[:question_text], prompt_text: params[:prompt_text], format: params[:format], alias: params[:alias])
-    if t_cdt.nil?
+    if template
       ref = CanonicalReference.where(label: params[:is_a])
-      property.is_a = ref.first.uri
+      property.is_a = ref.first.uri unless ref.empty?
+      puts colourize("***** Error finding Canonical ref: #{params[:alias]} #{params[:is_a]} *****", "red") if ref.empty?
+      cdt_property = t_cdt.has_property.find{|x| x.label == params[:label]} 
+      property.is_complex_datatype_property = cdt_property
+      puts colourize("***** Error finding CDT property: #{params[:alias]} #{params[:label]} *****", "red") if cdt_property.nil?
     else
       t_cdt.has_property_objects
       t_property = t_cdt.has_property.find{|x| x.label == params[:label]} 
@@ -83,28 +113,6 @@ describe BiomedicalConcept do
     property
   end
 
-  def create_complex_datatype(params, cdt_template, t_item)
-    cdt = BiomedicalConcept::ComplexDatatype.new(label: cdt_template.short_name)
-    cdt.is_complex_datatype = cdt_template
-    if !t_item.nil?
-      t_item.has_complex_datatype_objects
-      t_cdt = t_item.has_complex_datatype.find{|x| x.label == cdt_template.short_name} 
-    end
-    params[:has_property].each do |property|
-      cdt.has_property_push(create_property(property, t_cdt))
-    end
-    puts "CDT Alias=#{cdt.to_h[:has_property].map{|x| x[:alias]}}"
-    cdt
-  end
-
-  def find_complex_datatype(short_name)
-    return @cdt_set[short_name] if @cdt_set.key?(short_name)
-    cdt = ComplexDatatype.where(short_name: short_name)
-    cdt = ComplexDatatype.find_children(cdt.first.uri)
-    @cdt_set[short_name] = cdt
-    cdt
-  end
-
   it "create templates" do
     results = []
     templates = read_yaml_file(sub_dir, "templates.yaml")
@@ -124,14 +132,13 @@ describe BiomedicalConcept do
     results.each{|x| x.to_sparql(sparql, true)}
     full_path = sparql.to_file
   #Xcopy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "biomedical_concept_templates.ttl")
-	end
+  end
 
   it "create instances" do
     load_local_file_into_triple_store(sub_dir, "biomedical_concept_templates.ttl")
     results = []
     instances = read_yaml_file(sub_dir, "instances.yaml")
     instances.each do |instance|
-      #template = BiomedicalConceptTemplate.find_children(Uri.new(uri: instance[:based_on]))
       template = BiomedicalConceptTemplate.find_full(Uri.new(uri: instance[:based_on]))
       object = BiomedicalConceptInstance.new(label: instance[:label])
       object.based_on = template.uri
@@ -139,7 +146,6 @@ describe BiomedicalConcept do
       object.has_item_push(id_item)
       object.identified_by = id_item
       instance[:has_items].each_with_index do |item, index| 
-        #next if !item[:enabled]
         object.has_item_push(create_item(item, index+2, template))
       end
       #puts "Obj Final Alias=#{object.to_h[:has_item].map{|z| z[:has_complex_datatype].map{|x| x[:has_property].map{|y| y[:alias]}}}}"
@@ -156,7 +162,7 @@ describe BiomedicalConcept do
   it "check data" do
     load_local_file_into_triple_store(sub_dir, "biomedical_concept_templates.ttl")
     load_local_file_into_triple_store(sub_dir, "biomedical_concept_instances.ttl")
-    expect(BiomedicalConceptTemplate.unique.count).to eq(1)
+    expect(BiomedicalConceptTemplate.unique.count).to eq(6)
     expect(BiomedicalConceptInstance.unique.count).to eq(14)
   end
 
