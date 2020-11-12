7fc90643c0f2a64bd95a800ee34ce5e1b7a761ce
Dave IH <dave.iberson-hurst@assero.co.uk>
2020-11-12 12:38:03 +0100

    GLAN-1446
    [1] Fix test

--- spec/models/iso_managed_v2/uri_manager_spec.rb
+++ spec/models/iso_managed_v2/uri_manager_spec.rb
@@ -12,15 +12,16 @@ describe IsoManagedV2::UriManager do
 
     before :each do
       load_files(schema_files, [])
+      @parent = IsoConceptV2.new(label: "Parent")
     end
 
     it "create a class, empty hash" do
-      uri_manager = IsoManagedV2::UriManager.new
+      uri_manager = IsoManagedV2::UriManager.new(@parent)
       check_file_actual_expected(uri_manager.to_ids, sub_dir, "new_expected_1.yaml")
     end
 
     it "adds uris and converts to hash" do
-      uris = IsoManagedV2::UriManager.new
+      uris = IsoManagedV2::UriManager.new(@parent)
       old_uri_1 = Uri.new(uri: "http://www.assero.co.uk/A#old1")
       new_uri_1 = Uri.new(uri: "http://www.assero.co.uk/A#new1")
       old_uri_2 = Uri.new(uri: "http://www.assero.co.uk/A#old2")
