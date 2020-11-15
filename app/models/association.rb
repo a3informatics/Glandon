22b26c01297cb43c37eddfc26c5396e332564680
Clarisa <car@s-cubed.dk>
2020-11-15 12:06:24 +0100

    GLAN-1452
    [1] Add height and weight associations
    [1] Add semantic data property to association class

--- app/models/association.rb
+++ app/models/association.rb
@@ -8,6 +8,7 @@ class Association < IsoConceptV2
             base_uri: "http://#{EnvironmentVariable.read("url_authority")}/ASSOC",
             uri_unique: true
 
+  data_property :semantic
   object_property :the_subject, cardinality: :one, model_class: "IsoManagedV2", delete_exclude: true, read_exclude: true
   object_property :associated_with, cardinality: :many, model_class: "IsoManagedV2", delete_exclude: true, read_exclude: true
   
---
+++ spec/models/association/data_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe Association do
+	
+	include DataHelpers
+  include PublicFileHelpers
+
+  def sub_dir
+    return "models/association/data"
+  end
+
+  describe "create data" do
+
+    before :all do
+      data_files = ["biomedical_concept_instances.ttl", "biomedical_concept_templates.ttl" ]
+      load_files(schema_files, data_files)
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      load_data_file_into_triple_store("canonical_references.ttl")
+      load_data_file_into_triple_store("complex_datatypes.ttl")
+      load_data_file_into_triple_store("cdisc/sdtm_ig/SDTM_IG_V4.ttl")
+      load_data_file_into_triple_store("cdisc/sdtm_model/SDTM_MODEL_V7.ttl")      
+    end
+
+    after :all do
+      delete_all_public_test_files
+    end
+
+    it "create data, HEIGHT BC" do
+      association = Association.new()
+      association.uri = association.create_uri(Uri.new(uri: "http://www.example.com/path#a")) 
+      bc = BiomedicalConceptInstance.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/HEIGHT/V1#BCI"))
+      sdtm_ig_domain = SdtmIgDomain.find_minimum(Uri.new(uri: "http://www.cdisc.org/SDTM_IG_VS/V4#IGD"))
+      association.the_subject = bc
+      association.associated_with = [sdtm_ig_domain]
+      association.semantic = "BC SDTM Association"
+      results = []
+      results << association
+      sparql = Sparql::Update.new
+      sparql.default_namespace(results.first.uri.namespace)
+      results.each{|x| x.to_sparql(sparql, true)}
+      full_path = sparql.to_file
+    #Xcopy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "height_association.ttl")
+  	end
+
+    it "create data, WEIGHT BC" do
+      association = Association.new()
+      association.uri = association.create_uri(Uri.new(uri: "http://www.example.com/path#a")) 
+      bc = BiomedicalConceptInstance.find_minimum(Uri.new(uri: "http://www.s-cubed.dk/WEIGHT/V1#BCI"))
+      sdtm_ig_domain = SdtmIgDomain.find_minimum(Uri.new(uri: "http://www.cdisc.org/SDTM_IG_VS/V4#IGD"))
+      association.the_subject = bc
+      association.associated_with = [sdtm_ig_domain]
+      association.semantic = "BC SDTM Association"
+      results = []
+      results << association
+      sparql = Sparql::Update.new
+      sparql.default_namespace(results.first.uri.namespace)
+      results.each{|x| x.to_sparql(sparql, true)}
+      full_path = sparql.to_file
+    #Xcopy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "weight_association.ttl")
+    end
+
+  end
+
+end
