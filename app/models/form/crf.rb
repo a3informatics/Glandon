dba116874d83e242fe5664a510ce7eb9e6a1b474
Clarisa <car@s-cubed.dk>
2020-11-18 10:17:28 +0100

    GLAN-1452
    [1] Update css

--- app/models/form/crf.rb
+++ app/models/form/crf.rb
@@ -12,10 +12,11 @@ class Form
 
     def get_css
       html = "<style>"
-      html += "table.crf-input-field { border-left: 1px solid black; border-right: 1px solid black; border-bottom: 1px solid black;}\n"
-      html += "table.crf-input-field tr td { font-family: Arial, \"Helvetica Neue\", Helvetica, sans-serif; font-size: 8pt; text-align: center; "
-      html += "vertical-align: center; padding: 5px; }\n"
-      html += "table.crf-input-field td:not(:last-child){border-right: 1px dashed}\n"
+      html += "table.crf-input-field tr td { border-right: 1px dashed; border-left: 1px dashed; border-bottom: 1px solid;}\n"
+      html += "table.crf-input-field tr td { font-family: Arial, \"Helvetica Neue\", Helvetica, sans-serif; font-size: 9pt; text-align: center; "
+      html += "vertical-align: center; padding: 5px 10px; }\n"
+      html += "table.crf-input-field td:last-child {border-right: 1px solid}\n"
+      html += "table.crf-input-field td:first-child {border-left: 1px solid}\n"
       html += "h4.domain-1 {border-radius: 5px; background: #A3E4D7; padding: 5px; }\n"
       html += "p.domain-1 {border-radius: 5px; background: #A3E4D7; padding: 5px; }\n"
       html += "h4.domain-2 {border-radius: 5px; background: #AED6F1; padding: 5px; }\n"
@@ -30,135 +31,6 @@ class Form
       html += "p.domain-other {border-radius: 5px; background: #BDC3C7; padding: 5px; }\n"
       html += "</style>"
     end
-
-    # # Is a Question only group
-    # def is_question_only_group?
-    #   if self.class == Form::Group::Normal
-    #     self.has_sub_group.each do |sg|
-    #       sg.is_question_only_group? if sg.class == Form::Group::Normal
-    #     end
-    #   end
-    #   self.has_item.each do |item|
-    #     return true if item.class == Form::Item::Question || item.class == Form::Item::Mapping || item.class == Form::Item::TextLabel 
-    #   end
-    #   return false
-    # end
-
-    # # Is a BC only group
-    # def is_bc_only_group?
-    #   self.has_item.each do |item|
-    #     return false if item.class != Form::Item::BcProperty
-    #   end
-    #   if self.class == Form::Group::Normal
-    #     self.has_sub_group.each do |sg|
-    #       sg.is_bc_only_group? if sg.class == Form::Group::Normal
-    #     end
-    #   end
-    #   return true
-    # end
-
-    # # Repeating Question group
-    # def repeating_question_group(annotations)
-    #   html = ""
-    #   # Put the labels and mappings out first
-    #   self.has_sub_group.sort_by {|x| x.ordinal}.each do |sg|
-    #     html += sg.repeating_question_group(annotations)
-    #   end
-    #   self.has_item.sort_by {|x| x.ordinal}.each do |item|
-    #     html += item.to_crf unless item.class == Form::Item::Question
-    #   end
-    #   # Now the questions
-    #   html += '<td colspan="3"><table class="table table-striped table-bordered table-condensed">'
-    #   html += '<tr>'
-    #   self.has_item.sort_by {|x| x.ordinal}.each do |item|
-    #     html += item.question_cell(item.question_text) if item.class == Form::Item::Question
-    #   end
-    #   html += '</tr>'
-
-    #   unless annotations.nil?
-    #     html += '<tr>'
-    #     self.has_item.sort_by {|x| x.ordinal}.each do |item|
-    #       if item.class == Form::Item::Question
-    #         qa = item.question_annotations(annotations)
-    #         html += mapping_cell(qa, annotations)
-    #       else
-    #         html += empty_cell
-    #       end
-    #     end
-    #     html += '</tr>'
-    #   end
-
-    #   html += '<tr>'
-    #   self.has_item.sort_by {|x| x.ordinal}.each do |item|
-    #     html += input_field(item) if item.class == Form::Item::Question
-    #   end
-    #   html += '</tr>'
-    #   html += '</table></td>' 
-    #   return html
-    # end
-
-    # # Repeating BC group
-    # def repeating_bc_group(annotations)
-    #   html = ""
-    #   html += '<td colspan="3"><table class="table table-striped table-bordered table-condensed">'
-    #   html += '<tr>'
-    #   columns = {}
-    #   self.has_sub_group.sort_by {|x| x.ordinal}.each do |sg|
-    #     sg.has_item.sort_by {|x| x.ordinal}.each do |item|
-    #       property = BiomedicalConcept::PropertyX.find(item.has_property.reference)
-    #       #if property.enabled && property.collect
-    #         if !columns.has_key?(property.is_a.to_s)
-    #           columns[property.is_a.to_s] = property.is_a.to_s
-    #         end
-    #       #end
-    #     end
-    #   end
-    #   # Question text
-    #   html += start_row(false)
-    #   self.has_sub_group.first.has_item.sort_by {|x| x.ordinal}.each do |item|
-    #     property = BiomedicalConcept::PropertyX.find(item.has_property.reference)
-    #     if columns.has_key?(property.is_a.to_s)
-    #       html += item.question_cell(property.question_text)
-    #     end
-    #   end
-    #   html += end_row
-
-    #   # Annotation. Commented out, gives a block of annotations
-    #   #html += start_row(false)
-    #   #columns.each do |key, bridg_path|
-    #   #  pa = ""
-    #   #  node[:children].each do |bc_node|
-    #   #    bc_node[:children].each do |property_node|
-    #   #      if property_node[:bridg_path] == bridg_path
-    #   #        pa += property_annotations(property_node[:id], annotations, options)
-    #   #      end
-    #   #    end
-    #   #  end
-    #   #  html += mapping_cell(pa, options)
-    #   #end
-    #   #html += end_row
-      
-    #   # BCs and the input fields
-    #   self.has_sub_group.sort_by {|x| x.ordinal}.each do |sg|
-    #     html += start_row(false)
-    #     sg.has_item.sort_by {|x| x.ordinal}.each do |item|
-    #       property = BiomedicalConcept::PropertyX.find(item.has_property.reference)
-    #       if columns.has_key?(property.is_a.to_s)
-    #         if property.has_coded_value.length == 0
-    #           html += input_field(property)
-    #         else
-    #           html += terminology_cell(item)
-    #         end
-    #       end
-    #     end
-    #     html += end_row
-    #     html += start_row(false)
-    #     html += end_row
-    #   end
-    #   html += '</tr>'
-    #   html += '</table></td>'
-    #   return html
-    # end
 
     # Format input field
     def input_field(item)
@@ -208,9 +80,17 @@ class Form
     def field_table(cell_content)
       html = "<table class=\"crf-input-field\"><tr>"
       cell_content.each do |cell|
-        html += "<td>#{cell}</td>"
+        if separators.include? cell
+          html += "<td class=\"no-border\">#{cell}</td>"
+        else
+          html += "<td>#{cell}</td>"
+        end
       end
       html += "</tr></table>"
+    end
+
+    def separators
+      [".", ":", "/"]
     end
 
     def terminology_cell(item)
