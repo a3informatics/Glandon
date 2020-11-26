69b7de97969bc7a65b988964e387e0a9ebe5fb0e
Clarisa <car@s-cubed.dk>
2020-11-26 16:13:37 +0100

    GLAN-1457
    [1] Update models

--- app/models/sdtm_sponsor_domain/variable.rb
+++ app/models/sdtm_sponsor_domain/variable.rb
@@ -3,13 +3,10 @@ class SdtmSponsorDomain::Variable < SdtmIgDomain::Variable
   configure rdf_type: "http://www.assero.co.uk/Tabulation#SdtmSponsorDomainVariable",
             uri_property: :name
 
-  # data_property :name
-  # data_property :description
-  # data_property :format
-  # data_property :ct_and_format
-  # object_property :compliance, cardinality: :one, model_class: "IsoConceptSystem::Node", delete_exclude: true
-  # object_property :ct_reference, cardinality: :many, model_class: "OperationalReferenceV3::TmcReference"
-  # object_property :based_on_class_variable, cardinality: :one, model_class: "SdtmClass::Variable"
-  # object_property :is_a, cardinality: :one, model_class: "CanonicalReference", delete_exclude: true
+  data_property :comment
+  data_property :used
+  data_property :notes
+  object_property :typed_as, cardinality: :one, model_class: "IsoConceptSystem::Node", delete_exclude: true
+  object_property :based_on_ig_variable, cardinality: :one, model_class: "SdtmIgDomain::Variable"
 
 end
