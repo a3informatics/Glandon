d7c04f9fa9626907267a307fd8b6c53a9f82fe2e
Samuel Banas <sab@s-cubed.dk>
2020-12-11 17:07:47 +0100

    GLAN-1409
    [1] Fixed indentation + structure in Impact Panel module

--- app/javascript/shared/custom/iso_managed/impact/impact_panel.mjs
+++ app/javascript/shared/custom/iso_managed/impact/impact_panel.mjs
@@ -2,44 +2,49 @@ import TabsLayout from 'shared/ui/tabs_layout'
 import ImpactGraph from './impact_graph.mjs'
 import ManagedItemsPanel from '../managed_items_panel'
 
+import { csvExportBtn } from 'shared/helpers/dt/utils'
+
 /**
  * Impact Panel
  * @description Tab-based panel containing an Impact Graph and Table
  * @author Samuel Banas <sab@s-cubed.dk>
  */
 export default class ImpactPanel {
+
+  constructor({
+    selector = '#impact-panel'
+  } = {}) {
+
+    Object.assign( this, { selector } )
     
-    /**
-     * Create an Impact Panel instance 
-     * @param {Object} params Instance parameters
-     * @param {string} params.selector JQuery selector of the panel 
-     */
-    constructor({
-        selector = '#impact-panel'
-    } = {}) {
+    Object.assign( this, {
+      impactGraph: this._initImpactGraph(),
+      impactTable: this._initImpactTable()
+    })
 
-        Object.assign( this, { 
-            selector,
-            // Impact Graph instance
-            impactGraph: new ImpactGraph({
-                selector: `${ selector } #impact-graph`,
-                dataUrl: impactDataUrl,
-                onDataLoaded: () => 
-                    this.impactTable._render(this.impactGraph.rawData.nodes, true)
-            }),
-            // Impact table instance
-            impactTable: new ManagedItemsPanel({
-                selector: selector,
-                deferLoading: true 
-            })
-        })
+    TabsLayout.initialize()
 
-        TabsLayout.initialize()
+  }
 
-    }
-  
+  _initImpactGraph() {
 
-    /** Private **/
+    return new ImpactGraph({
+      selector: `${ this.selector } #impact-graph`,
+      dataUrl: impactDataUrl,
+      onDataLoaded: () => 
+        this.impactTable._render( this.impactGraph.rawData.nodes, true )
+    })
 
+  }
+
+  _initImpactTable() {
+
+    return new ManagedItemsPanel({
+      selector: this.selector,
+      deferLoading: true,
+      buttons: [ csvExportBtn ]
+    })
+
+  }
 
 }
