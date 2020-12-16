fcde98cbf7409dcf732f0c5bd2c79a342ac48575
Clarisa <car@s-cubed.dk>
2020-12-16 10:23:53 +0100

    GLAN-1457
    [1] Tweak query
    [2] Update test files

--- app/models/sdtm_ig_domain.rb
+++ app/models/sdtm_ig_domain.rb
@@ -13,7 +13,7 @@ class SdtmIgDomain < Tabulation
 
   # Get Children.
   #
-  # @return [Array] array of objects
+  # @return [Array] array of objects SDTM Variables
   def get_children
     results = []
     query_string = %Q{
@@ -21,32 +21,24 @@ class SdtmIgDomain < Tabulation
       {         
         #{self.uri.to_ref} bd:includesColumn ?c .         
         ?c bd:ordinal ?ordinal .         
-        ?c rdf:type ?type .         
-        ?c isoC:label ?label .          
-        ?c bd:name ?name .         
+        ?c rdf:type ?type .    
+        ?c isoC:label ?label .   
+        ?c bd:name ?name .
         ?c bd:description ?description .         
         ?c bd:ctAndFormat ?format .
         ?c bd:compliance ?compliance .
         ?compliance isoC:prefLabel ?compliance_label .
-        BIND (EXISTS {?c bd:basedOnIgVariable|bd:basedOnClassVariable ?o} as ?standard)
-        OPTIONAL {                   
-          ?c bd:basedOnClassVariable/bd:typedAs ?typed_as .
-          ?typed_as isoC:prefLabel ?typed_as_label .         
-        }
-        OPTIONAL {                   
-          ?c bd:basedOnClassVariable/bd:classifiedAs ?classification .
-          ?classification isoC:prefLabel ?classification_label .         
-        }             
+        ?c bd:basedOnClassVariable/bd:typedAs|bd:basedOnIgVariable/bd:basedOnClassVariable/bd:typedAs ?typed_as .
+        ?typed_as isoC:prefLabel ?typed_as_label .                 
+        ?c bd:basedOnClassVariable/bd:classifiedAs|bd:basedOnIgVariable/bd:basedOnClassVariable/bd:classifiedAs ?classification .
+        ?classification isoC:prefLabel ?classification_label .
+        BIND (EXISTS {?c bd:basedOnIgVariable|bd:basedOnClassVariable ?o} as ?standard)           
       } ORDER BY ?ordinal
     }
     query_results = Sparql::Query.new.query(query_string, "", [:isoC, :bd])
     query_results.by_object_set([:ordinal, :c, :type, :label, :name, :format, :description, :compliance, :compliance_label, :typed_as, :typed_as_label, :classification, :classification_label, :standard]).each do |x|
-      x[:classification] = x[:classification].to_id unless x[:classification].empty?
-      x[:classification_label].nil? ? x[:classification_label] = "" : x[:classification_label]
-      x[:typed_as] = x[:typed_as].to_id unless x[:typed_as].empty?
-      x[:typed_as_label].nil? ? x[:typed_as_label] = "" : x[:typed_as_label]
       results << {id: x[:c].to_id ,uri: x[:c].to_s, ordinal: x[:ordinal].to_i, rdf_type: x[:type].to_s, standard: x[:standard], label: x[:label], name: x[:name],
-                  format: x[:format], description: x[:description], compliance: {id: x[:compliance].to_id, label: x[:compliance_label]}, typed_as: {id: x[:typed_as], label:x[:typed_as_label]}, classified_as: {id:x[:classification], label: x[:classification_label]} }
+      format: x[:format], description: x[:description], compliance: {id: x[:compliance].to_id, label: x[:compliance_label]}, typed_as: {id: x[:typed_as].to_id, label:x[:typed_as_label]}, classified_as: {id:x[:classification].to_id, label: x[:classification_label]} }
     end
     results
   end
