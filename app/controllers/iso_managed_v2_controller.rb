8c1455539f3bb9f6e6704f79053c4e2da5fc8a24
Samuel Banas <sab@s-cubed.dk>
2020-12-17 12:52:52 +0100

    GLAN-1409
    [1] Added Impact path to node data in IsoManagedV2 Controller

--- app/controllers/iso_managed_v2_controller.rb
+++ app/controllers/iso_managed_v2_controller.rb
@@ -165,7 +165,8 @@ private
     nodes.each {|x| result[:links] << { source: item.id, target: x.id } } 
     nodes.insert(0, item) 
     result[:nodes] = nodes.map {|x| { uri: x.uri.to_s, id: x.uri.to_id, identifier: x.scoped_identifier, semantic_version: x.semantic_version, 
-        version_label: x.version_label, label: x.label, rdf_type: x.rdf_type.to_s, history_path: TypePathManagement.history_url_v2(x, true)}}
+        version_label: x.version_label, label: x.label, rdf_type: x.rdf_type.to_s, history_path: TypePathManagement.history_url_v2(x, true),
+        impact_path: impact_iso_managed_v2_path(x)}}
     result
   end
 end
