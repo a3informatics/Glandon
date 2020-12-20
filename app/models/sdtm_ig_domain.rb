137d1b2aacadb4728576cdbcfe86e41a35c55820
Clarisa <car@s-cubed.dk>
2020-12-20 21:14:08 +0100

    GLAN-1457
    [1] Used and Standard to bool
    [2] Fix tests

--- app/models/sdtm_ig_domain.rb
+++ app/models/sdtm_ig_domain.rb
@@ -38,8 +38,8 @@ class SdtmIgDomain < Tabulation
     }
     query_results = Sparql::Query.new.query(query_string, "", [:isoC, :bd])
     query_results.by_object_set([:ordinal, :c, :type, :label, :name, :format, :description, :used, :compliance, :compliance_label, :typed_as, :typed_as_label, :classification, :classification_label, :standard]).each do |x|
-      results << {id: x[:c].to_id ,uri: x[:c].to_s, ordinal: x[:ordinal].to_i, rdf_type: x[:type].to_s, standard: x[:standard], label: x[:label], name: x[:name],
-      format: x[:format], description: x[:description], used: x[:used], compliance: {id: x[:compliance].to_id, label: x[:compliance_label]}, typed_as: {id: x[:typed_as].to_id, label:x[:typed_as_label]}, classified_as: {id:x[:classification].to_id, label: x[:classification_label]} }
+      results << {id: x[:c].to_id ,uri: x[:c].to_s, ordinal: x[:ordinal].to_i, rdf_type: x[:type].to_s, standard: x[:standard].to_bool, label: x[:label], name: x[:name],
+      format: x[:format], description: x[:description], used: x[:used].to_bool, compliance: {id: x[:compliance].to_id, label: x[:compliance_label]}, typed_as: {id: x[:typed_as].to_id, label:x[:typed_as_label]}, classified_as: {id:x[:classification].to_id, label: x[:classification_label]} }
     end
     results
   end
