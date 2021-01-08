cf65d82162bcd44374cf09b22752e07913964f77
Samuel Banas <sab@s-cubed.dk>
2021-01-08 11:03:47 +0100

    GLAN-1457
    [1] New SDTM SD modal + code + icon + integration
    TODO: Needs updated path once done in the controller

--- app/javascript/packs/sdtm_sponsor_domains/index.js
+++ app/javascript/packs/sdtm_sponsor_domains/index.js
@@ -1,5 +1,6 @@
 import IndexPanel from 'shared/custom/iso_managed/index_panel'
 import { expandColumn } from 'shared/helpers/dt/utils'
+import CreateSDTMSDView from 'shared/custom/sdtm_sponsor_domains/sdtm_sd_create'
 
 $(document).ready( () => {
 
@@ -10,5 +11,7 @@ $(document).ready( () => {
 
   // Expand column width to fit contents better
   expandColumn("Identifier", "#index");
+
+  new CreateSDTMSDView();
 
 });
--- app/javascript/shared/custom/sdtm_sponsor_domains/sdtm_sd_create.mjs
+++ app/javascript/shared/custom/sdtm_sponsor_domains/sdtm_sd_create.mjs
@@ -4,44 +4,54 @@ import ItemsPicker from 'shared/ui/items_picker/items_picker'
 import { managedConceptRef } from 'shared/ui/strings'
 
 /**
- * Create Biomedical Concept Modal View
- * @description Allows to create a new Biomedical Concept from parameters with custom callback
- * @requires shared/biomedical_concept_instances/create_modal.html.erb partial
+ * Create SDM Sponsor Domain Modal View
+ * @description Allows to create a new SDTM Sponsor Domain from parameters with custom callback
+ * @requires shared/sdtm_sponsor_domains/create_modal.html.erb partial
  * @extends CreateItemView base Create Item View class
  * @author Samuel Banas <sab@s-cubed.dk>
  */
-export default class CreateBCView extends CreateItemView {
+export default class CreateSDTMSDView extends CreateItemView {
 
   /**
    * Create a Modal View
    * @param {Object} params Instance parameters
    * @param {string} params.selector JQuery selector of the modal element
-   * @param {function} params.onCreated Callback executed when BC gets created, result passed as function argument
+   * @param {function} params.onCreated Callback executed when SDTM gets created, result passed as function argument
    * @param {function} params.onShow Callback executed when modal shown
    * @param {function} params.onHide Callback executed when modal hidden
    */
    constructor({
-     selector = '#new-bc-modal',
+     selector = '#new-sdtm-sd-modal',
      onCreated,
      onShow = () => { },
      onHide = () => { }
    } = {}) {
-    super({ selector, onCreated, param: 'biomedical_concept_instance', createItemUrl: createBCUrl });
+
+    super({ 
+      selector, onCreated, 
+      param: 'sdtm_sponsor_domain', 
+      createItemUrl: createSDTMSDUrl 
+    });
 
     Object.assign(this, {
-      templatePicker: this._initPicker(),
+      itemPicker: this._initPicker(),
       onShow,
       onHide
     });
+
   }
 
   /**
    * Clears the Modal
    */
   clear() {
+
     super.clear();
+
     // Clear extra form fields
-    this.form.find('input').removeAttr('data-id');
+    this.form.find('input')
+             .removeAttr('data-id');
+
   }
 
 
@@ -52,10 +62,14 @@ export default class CreateBCView extends CreateItemView {
    * Set event listeners and handlers
    */
   _setListeners() {
+
     super._setListeners();
 
-    // On Template selector click event
-    this.form.find('#new-item-template').on('click', () => this.templatePicker.show() );
+    // On 'Base on' selector click event
+    this.form.find('#new-item-base-on').on('click', () => 
+      this.itemPicker.show() 
+    );
+
   }
 
   /**
@@ -75,52 +89,60 @@ export default class CreateBCView extends CreateItemView {
   }
 
   /**
-   * Adds template into the Modal
+   * Adds Base on selection into the Modal
    * @param {Object} template selected template data object
    */
-  _onSelectTemplate(template) {
-    this.form.find('#new-item-template').val( managedConceptRef(template) )
-                                      .attr('data-id', template.id);
+  _onSelectBaseOn(item) {
+
+    this.form.find('#new-item-base-on').val( managedConceptRef(item) )
+                                       .attr('data-id', item.id);
+
   }
 
   /**
-   * Get the user-specified values for a new BC from the form
+   * Get the user-specified values for a new SDTM from the form
    * @return {Object} key-vaue pairs of parameters and values
    */
   get _formData() {
+
     let formData = super._formData;
 
     return Object.assign( formData, {
-      template_id: this.form.find('#new-item-template').attr('data-id'),
+      base_on_id: this.form.find('#new-item-base-on').attr('data-id'),
     } );
+
   }
 
   /**
-   * Initialize an ItemsPicker instance for BC Template selection
+   * Initialize an ItemsPicker instance for SDTM Base selection
    * Call only once
    * @return {ItemsPicker} new instance of ItemsPicker
    */
   _initPicker() {
+
     return new ItemsPicker({
-      id: 'new-bc',
-      types: ['biomedical_concept_template'],
-      onSubmit: (selection) => this._onSelectTemplate( selection.asObjectsArray()[0] )
+      id: 'new-sdtm-sd',
+      types: ['sdtm_ig_domain', 'sdtm_class'],
+      onSubmit: selection => this._onSelectBaseOn( selection.asObjectsArray()[0] )
     })
+
   }
 
   /**
-   * Get validation rules for all fields in the Create BC form
+   * Get validation rules for all fields in the Create SDTM SD form
    * @return {Object} validation rules compatible with Validator
    */
   get _validationRules() {
+
     let validationRules = super._validationRules;
 
     return Object.assign( validationRules, {
-      template: {
+      'base-on': {
         'value': 'not-empty',
         'data-id': 'not-empty'
       }
     } );
+
   }
 
 }
