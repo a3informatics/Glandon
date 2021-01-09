705fd8be1959e6df3c0c874ac37bad2c79201dcc
Dave IH <dih@s-cubed.dk>
2021-01-09 15:15:32 +0100

    GLAN-1467
    [1] Fix blank tag issue

--- spec/models/import/sdtm_model_spec.rb
+++ spec/models/import/sdtm_model_spec.rb
@@ -85,7 +85,7 @@ describe Import::SdtmModel do
     delete_data_file(sub_dir, filename)
   end
 
-  it "import bug fix"  do
+  it "import variable qualifier bug fix"  do
     full_path = test_file_path(sub_dir, "import_input_3.xlsx")
     params = {version: "1", date: "2016-11-22", files: [full_path], version_label: "1.1.1", label: "SDTM Model", semantic_version: "1.1.1", job: @job}
     result = @object.import(params)
@@ -96,6 +96,21 @@ describe Import::SdtmModel do
     copy_file_from_public_files("test", filename, sub_dir)
   #Xcopy_file_from_public_files_rename("test", filename, sub_dir, "import_expected_3.ttl")
     check_ttl(filename, "import_expected_3.ttl")
+    expect(@job.status).to eq("Complete")
+    delete_data_file(sub_dir, filename)
+  end
+
+  it "import no role bug fix"  do
+    full_path = test_file_path(sub_dir, "import_input_4.xlsx")
+    params = {version: "1", date: "2016-11-22", files: [full_path], version_label: "1.1.1", label: "SDTM Model", semantic_version: "1.1.1", job: @job}
+    result = @object.import(params)
+    filename = "cdisc_sdtm_model_#{@object.id}_errors.yml"
+    public_file_does_not_exist?("test", filename)
+    filename = "cdisc_sdtm_model_#{@object.id}_load.ttl"
+    expect(public_file_exists?("test", filename)).to eq(true)
+    copy_file_from_public_files("test", filename, sub_dir)
+  #Xcopy_file_from_public_files_rename("test", filename, sub_dir, "import_expected_4.ttl")
+    check_ttl(filename, "import_expected_4.ttl")
     expect(@job.status).to eq("Complete")
     delete_data_file(sub_dir, filename)
   end
