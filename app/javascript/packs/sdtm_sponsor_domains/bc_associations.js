36590df9b584e7e1a0bab24a54e297ad2f2abde9
Samuel Banas <sab@s-cubed.dk>
2021-01-19 16:29:32 +0100

    GLAN-1473
    [1] Managed Collections Panel module (unstable, with test data)
    [2] BC Associations page updates in view + added a JS pack

---
+++ app/javascript/packs/sdtm_sponsor_domains/bc_associations.js
@@ -0,0 +1,17 @@
+import ManagedCollectionPanel from 'shared/custom/iso_managed/managed_collection_panel'
+import TokenTimer from 'shared/custom/tokens/token_timer'
+
+$(document).ready( () => {
+
+  let tt = new TokenTimer({
+    tokenId: tokenTimerId,
+    warningTime: tokenTimerWarning
+  });
+
+  let mcp = new ManagedCollectionPanel({
+    urls: { data: 'aaa', add: 'bbb', remove: 'ccc', removeAll: 'ddd' },
+    param: 'sdtm_sponsor_domain',
+    allowedTypes: ['biomedical_concept_instance']
+  })
+
+});
---
+++ app/javascript/shared/custom/iso_managed/managed_collection_panel.mjs
@@ -0,0 +1,236 @@
+import SelectablePanel from 'shared/base/selectable_panel'
+
+import ItemsPicker from 'shared/ui/items_picker/items_picker'
+
+import { dtManagedItemsColumns } from 'shared/helpers/dt/dt_column_collections'
+import { hasColumn, selectAllBtn, deselectAllBtn } from 'shared/helpers/dt/utils'
+import { $confirm } from 'shared/helpers/confirmable'
+import { $ajax } from 'shared/helpers/ajax'
+
+import testData from './testdata'
+
+/**
+ * Managed Collection Panel
+ * @description Editable collection of version-based collection of Managed Items (adding, removing)
+ * @author Samuel Banas <sab@s-cubed.dk>
+ */
+export default class ManagedCollectionPanel {
+
+  /**
+   * Create a Managed Collection Panel instance
+   * @param {Object} params Instance parameters
+   * @param {string} params.selector Unique selector of the Managed Collection element
+   * @param {Object} params.urls Urls object containing the data, add, remove and removeAll action urls
+   * @param {string} params.param Strict parameter name required for the controller params
+   * @param {Array} params.allowedTypes Array of strings - param names of allowed item types that can be added to the Collection @see ItemsPicker module
+   */
+  constructor({
+    selector = "#managed-collection",
+    urls,
+    param,
+    allowedTypes = []
+  }) {
+
+    Object.assign( this, {
+      selector, urls, param, allowedTypes 
+    })
+
+    this.sp = this._initSelectablePanel()
+    this.picker = this._initPicker()
+
+    this._setListeners()
+    this._loadData()
+
+  }
+
+  /**
+   * Show the picker for adding items to the collection
+   */
+  add() {
+
+    if ( this.picker )
+      this.picker.show()
+
+  }
+
+  /**
+   * Remove the currently selected items from the collection
+   * @require user confirmation
+   */
+  removeSelected() {
+
+    const selectedCount = this.sp.selected.count()
+
+    if ( selectedCount < 1 )
+      return
+
+    $confirm({
+      subtitle: `${ selectedCount } item(s) will be removed from this collection.`,
+      dangerous: true,
+      callback: () => this._removeSelected()
+    })
+
+  }
+
+  /**
+   * Remove all items from the collection
+   * @require user confirmation
+   */
+  removeAll() {
+
+    const itemsCount = this.sp.table.rows().count()
+
+    if ( itemsCount < 1 )
+      return 
+
+    $confirm({
+      subtitle: `All items will be removed from this collection.`,
+      dangerous: true,
+      callback: () => this._removeAll()
+    })
+
+  }
+
+
+  _loadData() {
+
+    this.sp._render( testData )
+    this.sp._onDataLoaded( this.sp.table )
+    this.sp._loading( false );
+
+  }
+  
+
+  /** Private **/
+
+
+  /**
+   * Sets event listeners, handlers
+   * Used for non-table related listeners only!
+   */
+  _setListeners() {
+
+    const $panel = $( this.selector )
+
+    $panel.find( '#add-items' ).on( 'click', () => 
+      this.add()
+    )
+
+    $panel.find( '#remove-selected' ).on( 'click', () => 
+      this.removeSelected()
+    )
+
+    $panel.find( '#remove-all' ).on( 'click', () => 
+      this.removeAll() 
+    )
+
+  }
+
+  _add(ids) {
+
+    // Make request and update UI
+
+  }
+
+  _removeSelected() {
+
+    const selectedRows = this.sp.selected
+
+    // Make request and update UI  
+
+  }
+
+  _removeAll() {
+    
+  }
+
+  /**
+   * Execute server request (helper)
+   * @param {string} url Request URL
+   * @param {string} type Request type
+   * @param {any} data Data to pass in request
+   * @param {function} success Success callback, returned data passed are passed as first arg
+   */
+  _execRequest({
+    url,
+    type,
+    data,
+    success = () => {}
+  }) {
+
+    this._loading( true )
+    
+    $ajax({
+      url, type,
+      data: {
+        [ this.param ]: data 
+      },
+      done: data => success( data ),
+      always: () => this._loading( false )
+    })
+
+  }
+
+  /**
+   * Change panel's loading state
+   * @param {boolean} enable Value specifying to the desired loading state
+   */
+  _loading(enable) {
+
+    this.sp._loading( enable )
+    $( this.selector ).find( 'button, a' )
+                      .toggleClass( 'disabled', enable )
+
+  }
+
+  /**
+   * Initialize a new instance of SelectablePanel containing the collection items 
+   * @return {SelectablePanel} New SP instance
+   */
+  _initSelectablePanel() {
+
+    return new SelectablePanel({
+      tablePanelOptions: {
+        selector: `${ this.selector } #managed-items`,
+        url: this.urls.data,
+        param: this.param,
+        extraColumns: this._panelColumns,
+        buttons: [ selectAllBtn(), deselectAllBtn() ],
+        order: [[1, "asc"]],
+        paginated: false,
+        deferLoading: true,
+      },
+      multiple: true
+    })
+
+  }
+
+  /**
+   * Initialize a new instance of ItemsPicker for adding items to the collection
+   * @return {ItemsPicker} New Picker instance
+   */
+  _initPicker() {
+
+    return new ItemsPicker({
+      id: 'add-items',
+      types: this.allowedTypes,
+      multiple: true, 
+      onSubmit: selection => this._add( selection.asIdsArray() )
+    })
+
+  }
+
+  /**
+   * Get column definitions for Managed Collection table
+   * @return {Array} Array of DataTable column definitions
+   */
+  get _panelColumns() {
+
+    const withType = hasColumn( this.selector, 'Type' ),
+          withOwner = hasColumn( this.selector, 'Owner' )
+
+    return dtManagedItemsColumns( {}, withType, withOwner )
+
+  }
+
+}
