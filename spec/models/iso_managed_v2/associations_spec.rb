70606474f0d3fd82d740063f86c4e9aeb0d7a9bc
Clarisa <car@s-cubed.dk>
2021-01-19 14:05:00 +0100

    GLAN-1473
    [1] Add association? tests

--- spec/models/iso_managed_v2/associations_spec.rb
+++ spec/models/iso_managed_v2/associations_spec.rb
@@ -30,7 +30,7 @@ describe IsoManagedV2::Associations do
       check_file_actual_expected(result.to_h, sub_dir, "associate_expected_1.yaml", equate_method: :hash_equal)
     end
 
-    it "associate, mutliple BCs" do
+    it "associate, multiple BCs" do
       domain = create_sdtm_sponsor_domain("AAA", "SDTM Sponsor Domain", "AA")
       bc_1 = BiomedicalConceptInstance.find(Uri.new(uri: "http://www.s-cubed.dk/HEIGHT/V1#BCI"))
       bc_2 = BiomedicalConceptInstance.find(Uri.new(uri: "http://www.s-cubed.dk/WEIGHT/V1#BCI"))
@@ -138,6 +138,34 @@ describe IsoManagedV2::Associations do
       assoc = Association.find(assoc.uri)
       result = domain.diassociate_all
       expect{Association.find(assoc.uri)}.to raise_error(Errors::NotFoundError, "Failed to find http://www.assero.co.uk/ASSOC#1760cbb1-a370-41f6-a3b3-493c1d9c2238 in Association.")
+    end
+
+  end
+
+  describe "Association? tests" do
+
+    before :all do
+      load_files(schema_files, [])
+      load_data_file_into_triple_store("mdr_identification.ttl")
+      load_data_file_into_triple_store("biomedical_concept_templates.ttl")
+      load_data_file_into_triple_store("biomedical_concept_instances.ttl")
+    end
+
+    before :each do
+      allow(SecureRandom).to receive(:uuid).and_return(*SecureRandomHelpers.predictable)
+    end
+
+    it "association?, false" do
+      domain = create_sdtm_sponsor_domain("AAA", "SDTM Sponsor Domain", "AA")
+      expect(domain.association?).to eq(false)
+    end
+
+    it "association?, true" do
+      domain = create_sdtm_sponsor_domain("AAA", "SDTM Sponsor Domain", "AA")
+      bc_1 = BiomedicalConceptInstance.find(Uri.new(uri: "http://www.s-cubed.dk/HEIGHT/V1#BCI"))
+      bc_2 = BiomedicalConceptInstance.find(Uri.new(uri: "http://www.s-cubed.dk/WEIGHT/V1#BCI"))
+      result = domain.associate([bc_1.id, bc_2.id], "SDTM BC Association")
+      expect(domain.association?).to eq(true)
     end
 
   end
