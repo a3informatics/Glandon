5d99d1ce450255a509136d2d1e62ca190f242569
Dave IH <dih@s-cubed.dk>
2021-01-26 14:56:13 +0100

    GLAN-1476
    [1] Utility class to handle a set of tokens

---
+++ app/models/concerns/token_set.rb
@@ -0,0 +1,57 @@
+# Token Set. A set of tokens
+#
+# @author Dave Iberson-Hurst
+# @since 3.8.0
+class TokenSet
+
+  # Initialize
+  #
+  # @return [CustomPropertySet] the new object
+  def initialize(items, user)
+    @items = []
+    @user = user
+    items.each { |item| @items << { item: item, lock: Token.obtain(item, user) } }
+  end
+
+  # <<
+  #
+  # @param [Object] item the object to be added
+  # @return [Void] no return
+  def <<(item)
+    @items << { item: item, lock: Token.obtain(item, @user) }
+  end
+
+  # Locked? All items locked?
+  #
+  #
+  # @return [Boolean] true if all items locked
+  def locked?
+    !@items.any?{ |item| item[:lock].nil? }
+  end
+
+  # Each. Iterate over the items
+  #
+  # @return [Enumerator] returns the enumerator for the collection
+  def each
+    @items.each do |property| 
+      yield(property)
+    end
+  end
+
+  # Release the locks
+  #
+  # @return [Boolean] always true
+  def release
+    @items.each { |item| item[:lock].release }
+    @items = []
+    true
+  end
+
+  # Items
+  #
+  # @return [Array] the set of items as an array
+  def items
+    @items
+  end
+
+end
---
+++ spec/models/concerns/token_set_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+describe TokenSet do
+
+  include DataHelpers
+	include PauseHelpers
+  include UserAccountHelpers
+  include IsoManagedFactory
+
+  before :all do
+    data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl"]
+    load_files(schema_files, data_files)
+    @user = ua_add_user(email: "token_user_1@example.com")
+    @user_other = ua_add_user(email: "token_user_2@example.com")
+    Token.destroy_all
+  end
+
+  after :all do
+    ua_remove_user("token_user_1@example.com")
+    ua_remove_user("token_user_2@example.com")
+    Token.destroy_all
+  end
+
+	it "allows set to be set" do
+    items = []
+  	["1", "2", "3"].each { |x| items << create_iso_managed("ITEM #{x}", "Item #{x}") }
+  	token_set = TokenSet.new(items, @user)
+    expect(token_set.items.count).to eq(3)
+    expect(token_set.locked?).to eq(true)
+    expect(token_set.items[0][:item].uri.to_s).to eq("http://www.acme-pharma.com/ITEM_1/V1")
+    expect(token_set.items[1][:item].uri.to_s).to eq("http://www.acme-pharma.com/ITEM_2/V1")
+    expect(token_set.items[2][:item].uri.to_s).to eq("http://www.acme-pharma.com/ITEM_3/V1")
+    expect(Token.all.count).to eq(3)
+  end
+
+  it "allows set to be released" do
+    items = []
+    ["4", "5", "6"].each { |x| items << create_iso_managed("ITEM #{x}", "Item #{x}") }
+    token_set = TokenSet.new(items, @user)
+    expect(token_set.items.count).to eq(3)
+    expect(Token.all.count).to eq(3)
+    token_set.release
+    expect(token_set.items.count).to eq(0)
+    expect(Token.all.count).to eq(0)
+  end
+
+  it "enumerates" do
+    expected = ["http://www.acme-pharma.com/ITEM_7/V1", "http://www.acme-pharma.com/ITEM_8/V1", "http://www.acme-pharma.com/ITEM_9/V1"]
+    items = []
+    ["7", "8", "9"].each { |x| items << create_iso_managed("ITEM #{x}", "Item #{x}") }
+    token_set = TokenSet.new(items, @user)
+    index = 0
+    token_set.each { |item| expect(item[:item].uri.to_s).to eq(expected[index]); index += 1}
+  end
+
+  it "add item" do
+    expected = ["http://www.acme-pharma.com/ITEM_10/V1", "http://www.acme-pharma.com/ITEM_11/V1", "http://www.acme-pharma.com/ITEM_12/V1"]
+    items = []
+    ["10", "11", "12"].each { |x| items << create_iso_managed("ITEM #{x}", "Item #{x}") }
+    token_set = TokenSet.new(items[0..1], @user)
+    expect(token_set.items.count).to eq(2)
+    expect(token_set.locked?).to eq(true)
+    token_set << items[2]
+    expect(token_set.items.count).to eq(3)
+    expect(token_set.locked?).to eq(true)
+    index = 0
+    token_set.each { |item| expect(item[:item].uri.to_s).to eq(expected[index]); index += 1}
+  end
+
+  it "determine if everything is locked" do
+    items = []
+    ["20", "21"].each { |x| items << create_iso_managed("ITEM #{x}", "Item #{x}") }
+    Token.obtain(items[0], @user_other)
+    token_set = TokenSet.new(items, @user)
+    expect(token_set.items.count).to eq(2)
+    expect(token_set.locked?).to eq(false)
+  end
+
+end
