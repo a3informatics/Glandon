4c8cb2970d7a6578b124ca2454e37ed7d911cff7
Clarisa <car@s-cubed.dk>
2021-01-27 13:33:17 +0100

    GLAN-1482
    [1] Add add item method
    [2] Add tests

--- app/models/managed_collection.rb
+++ app/models/managed_collection.rb
@@ -25,4 +25,45 @@ class ManagedCollection <  IsoManagedV2
     object
   end
 
+  def add_item(id_set)
+    ordinal = next_ordinal(:has_managed)
+    transaction = transaction_begin
+    id_set.map{|x| Uri.new(id: x)}.each do |uri|
+      ref = OperationalReferenceV3.create({ordinal: ordinal, reference: uri, transaction: transaction}, self)
+      self.add_link(:has_managed, ref.uri)
+      ordinal += 1
+    end
+    transaction_execute
+    self
+  end
+
+  # def remove_item(id_set)
+  # end
+
+  # Managed. List the objects that belong to the Collection.
+  #
+  # @param 
+  # @return
+  # def managed
+  #   results = []
+  #   query_string = %Q{
+  #     SELECT DISTINCT ?s ?i ?l ?sv ?vl ?owner ?rdf_type WHERE {
+  #       #{self.uri.to_ref} bo:hasManaged ?op_ref .
+  #       ?op_ref bo:reference ?s .
+  #       ?s isoT:hasIdentifier/isoI:identifier ?i .
+  #       ?s isoC:label ?l .
+  #       ?s isoT:hasIdentifier/isoI:semanticVersion ?sv .
+  #       ?s isoT:hasIdentifier/isoI:versionLabel ?vl .
+  #       ?s isoT:hasIdentifier/isoI:hasScope/isoI:shortName ?owner .
+  #       ?s rdf:type ?rdf_type
+  #     }
+  #   }
+  #   query_results = Sparql::Query.new.query(query_string, "", [:bo, :isoT, :isoC, :isoI])
+  #   triples = query_results.by_object_set([:s, :i, :l, :sv, :vl, :owner, :rdf_type])
+  #   triples.each do |x|
+  #     results << {uri: x[:s].to_s, id: x[:s].to_id, identifier: x[:i], label: x[:l], semantic_version: x[:sv], version_label: x[:vl], owner: x[:owner], rdf_type: x[:rdf_type].to_s}
+  #   end
+  #   results
+  # end
+
 end
