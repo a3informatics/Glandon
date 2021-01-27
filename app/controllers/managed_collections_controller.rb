92f5f3550d387f57c0e870340d0f9a6fe7762246
Clarisa <car@s-cubed.dk>
2021-01-27 11:16:28 +0100

    GLAN-1482
    [1] Add actions

--- app/controllers/managed_collections_controller.rb
+++ app/controllers/managed_collections_controller.rb
@@ -22,38 +22,85 @@ class ManagedCollectionsController < ManagedItemsController
     @close_path = history_managed_collections_path(managed_collection: { identifier: @mc.has_identifier.identifier, scope_id: @mc.scope })
   end
 
-  # def show_data
-  #   @mc = ManagedCollection.find_minimum(protect_from_bad_id(params))
-  #   render json: {data: @mc.managed}, status: 200
-  # end
+  def show_data
+    @mc = ManagedCollection.find_minimum(protect_from_bad_id(params))
+    render json: {data: @mc.managed}, status: 200
+  end
 
-  # def create
-  #   mc = ManagedCollection.create(the_params)
-  #   if mc.errors.empty?
-  #     AuditTrail.create_item_event(current_user, mc, mc.audit_message(:created))
-  #     path = history_managed_collections_path({managed_collection: {identifier: mc.scoped_identifier, scope_id: mc.scope.id}})
-  #     render :json => {data: {history_path: path, id: mc.id}}, :status => 200
-  #   else
-  #     render :json => {errors: mc.errors.full_messages}, :status => 422
-  #   end
-  # end
+  def create
+    mc = ManagedCollection.create(the_params)
+    if mc.errors.empty?
+      AuditTrail.create_item_event(current_user, mc, mc.audit_message(:created))
+      path = history_managed_collections_path({managed_collection: {identifier: mc.scoped_identifier, scope_id: mc.scope.id}})
+      render :json => {data: {history_path: path, id: mc.id}}, :status => 200
+    else
+      render :json => {errors: mc.errors.full_messages}, :status => 422
+    end
+  end
+
+  def edit
+    @mc = ManagedCollection.find_with_properties(protect_from_bad_id(params))
+    respond_to do |format|
+      format.html do
+        return true unless edit_lock(@mc)
+        @mc = @edit.item
+        @edit_tags_path = path_for(:edit_tags, @mc)
+        @close_path = history_managed_collections_path({ managed_collection:
+            { identifier: @mc.scoped_identifier, scope_id: @mc.scope } })
+      end
+      format.json do
+        @mc = ManagedCollection.find_full(@mc.id)
+        return true unless check_lock_for_item(@mc)
+        render :json => { data: @mc.managed }, :status => 200
+      end
+    end
+  end
+
+  def add
+    mc = ManagedCollection.find_full(protect_from_bad_id(params))
+    return true unless check_lock_for_item(mc)
+    set_before = mc.managed
+    item = mc.add(items_params[:id_set])
+    if item.errors.empty?
+      AuditTrail.create_item_event(current_user, mc, "Item(s) added to Managed Collection.")
+      set_after = mc.managed
+      result = set_after - set_before
+      render :json => {data: result}, :status => 200
+    else
+      render :json => {errors: item.errors.full_messages}, :status => 422
+    end
+  end
+
+  def remove
+    mc = ManagedCollection.find_full(protect_from_bad_id(params))
+    return true unless check_lock_for_item(mc)
+    result = mc.remove(items_params[:id_set])
+    if result.errors.empty?
+      AuditTrail.create_item_event(current_user, sdtm_sponsor_domain, "Item(s) removed from Managed Collection.")
+      render :json => {data: []}, :status => 200
+    else
+      render :json => {errors: result.errors.full_messages}, :status => 422
+    end
+  end
 
 private
-
 
   # Strong parameters, general
   def the_params
     params.require(:managed_collection).permit(:identifier, :label, :offset, :count, :scope_id)
   end
 
+  def items_params
+    params.require(:managed_collection).permit(:id_set => [])
+  end
 
   # Path for given action
   def path_for(action, object)
     case action
       when :show
         return managed_collection_path(object)
-      #when :edit
-        #return edit_managed_collection_path(id: object.id)
+      when :edit
+        return edit_managed_collection_path(id: object.id)
       #when :destroy
         #return managed_collection_path(object)
       when :edit_tags
