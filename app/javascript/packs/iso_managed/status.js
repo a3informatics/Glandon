bbce3ced0aab03513341c784f01652b2781b76b9
Samuel Banas <sab@s-cubed.dk>
2021-01-27 09:15:02 +0100

    GLAN-1476
    [1] Basic structure of Status page view + JS pack

---
+++ app/javascript/packs/iso_managed/status.js
@@ -0,0 +1,10 @@
+import TokenTimer from 'shared/custom/tokens/token_timer'
+
+$(document).ready(() => {
+
+  const tt = new TokenTimer({
+    tokenId: tokenTimerId,
+    warningTime: tokenTimerWarning
+  });
+
+});
