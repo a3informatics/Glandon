42de3c5d0da76a0be1c04d8d38458647428e61e6
Samuel Banas <sab@s-cubed.dk>
2021-01-30 18:19:01 +0100

    GLAN-1476
    [1] Iso Managed Document Control page feature tests

--- spec/features/iso_managed_js_spec.rb
+++ spec/features/iso_managed_js_spec.rb
@@ -3,150 +3,218 @@ require 'rails_helper'
 describe "ISO Managed JS", :type => :feature do
 
   include DataHelpers
-  include PauseHelpers
-  include UiHelpers
-  include WaitForAjaxHelper
-  include UserAccountHelpers
-  include PublicFileHelpers
   include DownloadHelpers
-  include SparqlHelpers
+  include UserAccountHelpers
+  include UiHelpers
+  include NameValueHelpers
+  include WaitForAjaxHelper
+  include TokenHelpers
 
   def sub_dir
     return "features/iso_managed"
   end
 
   before :all do
-    ua_create
-    data_files = ["iso_namespace_real.ttl", "iso_registration_authority_real.ttl", 
-      "CT_ACME_TEST.ttl", 
-      "form_example_vs_baseline.ttl"]
+    data_files = []
     load_files(schema_files, data_files)
-    load_cdisc_term_versions(1..43)
-    clear_iso_concept_object
+    load_data_file_into_triple_store("mdr_identification.ttl")
+    nv_destroy
+    nv_create({parent: '10', child: '999'})
+    ua_create
+    set_transactional_tests false 
   end
 
   after :all do
     ua_destroy
-    delete_all_public_test_files
+    nv_destroy
+    set_transactional_tests true 
   end
 
   before :each do
-    delete_all_public_test_files
-    clear_downloads
+    ua_curator_login
   end
 
   after :each do
     ua_logoff
   end
 
-  describe "Curator User", :type => :feature do
+  describe "Document Control, Code List, Curator User", type: :feature, js: true do
 
-        it "allows the comments to be updated" #, js: true do
-    #   # ua_curator_login
-    #   # click_navbar_forms
-    #   # expect(page).to have_content 'Index: Forms'
-    #   #pause
-    #   # find(:xpath, "//tr[contains(.,'VS BASELINE')]/td/a", :text => 'History').click
-    #   # #pause
-    #   # find(:xpath, "//table[@id='secondary']/tbody/tr/td/a", :text => 'Edit').click
-    #   # expect(page).to have_content 'Comments:'
-    #   # #pause
-    #   # fill_in "iso_managed_changeDescription", with: "Hello world. This is a change description"
-    #   # click_button "Preview"
-    #   # div = page.find("#generic_markdown")
-    #   # expect(div.text(:all)).to eq("Hello world. This is a change description")
-    #   # fill_in "iso_managed_explanatoryComment", with: "I am a comment"
-    #   # click_button "Preview"
-    #   # #pause
-    #   # div = page.find("#generic_markdown")
-    #   # expect(div.text(:all)).to eq("I am a comment")
-    #   # fill_in "iso_managed_origin", with: "I am the origin"
-    #   # click_button "Preview"
-    #   # div = page.find("#generic_markdown")
-    #   # expect(div.text(:all)).to eq("I am the origin")
-    #   # fill_in "iso_managed_origin", with: "£±£±"
-    #   # expect(page).to have_content 'Please enter valid markdown.'
-    #   # ui_set_focus("iso_managed_explanatoryComment")
-    #   # wait_for_ajax
-    #   # div = page.find("#generic_markdown")
-    #   # expect(div.text(:all)).to eq("I am a comment")
-    #   # ui_set_focus("iso_managed_changeDescription")
-    #   # div = page.find("#generic_markdown")
-    #   # wait_for_ajax
-    #   # expect(div.text(:all)).to eq("Hello world. This is a change description")
-    #   # fill_in "iso_managed_origin", with: "Origin"
-    #   # click_button 'Submit'
-    #   # expect(page).to have_content 'Hello world. This is a change description'
-    #   # expect(page).to have_content 'I am a comment'
-    #   # expect(page).to have_content 'Origin'
-    # end
+    it "allows to view the Document Control page, initial state" do
+      new_cl_and_dc
 
-    it "allows the status to be viewed", js: true do
-      ua_curator_login
-      click_navbar_cdisc_terminology
-      expect(page).to have_content 'Controlled Terminology'
+      expect(page).to have_content "Document Control"
+      dc_check_version '0.1.0'
+      dc_check_version_label 'None'
+      dc_check_current :not_standard
+      dc_check_statuses('Incomplete', 'Candidate')
     end
 
-    it "allows the semantic version to be updated", js: true do
-      ua_curator_login
-      click_navbar_terminology
-      click_link 'New Terminology'
-      sleep 1
-      fill_in 'thesauri_identifier', with: 'TEST test'
-      fill_in 'thesauri_label', with: 'Test Terminology'
-      click_button 'Submit'
-      sleep 1
-      wait_for_ajax(10)
-      find(:xpath, "//tr[contains(.,'Test Terminology')]/td/a").click
-      wait_for_ajax(10)
-      # context_menu_element('history', 4, 'Test Terminology', :edit)
-      # wait_for_ajax(10)
-      # click_link 'Return'
-      context_menu_element('history', 4, 'Test Terminology', :document_control)
-      wait_for_ajax(10)
-      expect(page).to have_content 'Manage Status'
-      expect(page).to have_content 'Version Control'
-      expect(page).to have_content("Current Status:")
-      expect(page).to have_content("Incomplete")
-      click_button "state_submit"
-      expect(page).to have_content("Candidate")
-      click_button "state_submit"
-      expect(page).to have_content("Recorded")
-      click_button "state_submit"
-      expect(page).to have_content("Qualified")
-      find(:xpath, "//*[@id='version-edit']").click
-      find(:xpath, "//*[@id='select-release']/option[3]").click
-      find(:xpath, "//*[@id='version-edit-submit']").click
-      ui_check_table_row("version_info", 1, ["Version:", "0.1.0"])
+    it "allows to update version information" do
+      new_cl_and_dc
+
+      dc_forward_to 'Candidate'
+      wait_for_ajax 10
+      
+      # Semantic version
+      find('#version .bg-label').click 
+      select 'major'
+      click_on('sp-submit')
+      wait_for_ajax 10 
+      dc_check_version '1.0.0'
+
+      # Dismiss Edit 
+      find('#version .bg-label').click 
+      click_on('sp-dismiss')
+      dc_check_version '1.0.0'
+
+      # Version label 
+      find('#version-label .bg-label').click
+     
+      # Validation 
+      fill_in 'Version label', with: 'Test version æ'
+      click_on('sp-submit')
+      wait_for_ajax 10 
+      expect(page).to have_content('contains invalid characters')
+
+      fill_in 'Version label', with: 'Test version'
+      click_on('sp-submit')
+      wait_for_ajax 10 
+      dc_check_version_label 'Test version'
     end
 
-    it "allows items to be exported" #, js: true do
-    #   ua_content_admin_login
-    #   click_navbar_export
-    #   expect(page).to have_content 'Export Centre'
-    #   click_link 'Export Forms'
-    #   wait_for_ajax(10)
-    #   expect(page).to have_content 'Exports'
-    #   expect(page).to have_content 'Showing 1 to 2 of 2 entries' # New form added in previous test
-    #   public_file_exists?("test", "ACME_VS BASELINE_1.ttl")
-    # #Xcopy_file_from_public_files("test", "ACME_VS BASELINE_1.ttl", sub_dir) # Setup results.
-    #   find(:xpath, "//tr[contains(.,'VS BASELINE')]/td/a", :text => 'Download File').click
-    #   file = download_content
-    #   write_text_file_2(file, sub_dir, "form_export_results.ttl")
-    #   check_triples_fix("form_export_results.ttl", "ACME_VS BASELINE_1.ttl", {last_change_date: true})
-    #   delete_data_file(sub_dir, "form_export_results.ttl")
-    #   click_link 'Close'
-    #   expect(page).to have_content 'Export Centre'
-    #   click_link 'Export Terminologies'
-    #   expect(page).to have_content 'Exports'
-    #   expect(page).to have_content 'Showing 1 to 1 of 1 entries'
-    #   click_link 'Close'
-    #   expect(page).to have_content 'Export Centre'
-    #   click_link 'Export Biomedical Concepts'
-    #   wait_for_ajax(10)
-    #   expect(page).to have_content 'Showing 1 to 10 of 13 entries'
-    #   click_link 'Close'
-    # end
+    it "allows to make item current" do
+      new_cl_and_dc
+
+      dc_forward_to 'Standard'
+      wait_for_ajax 10
+
+      dc_check_current :not_current
+
+      click_on 'Make Current'
+      wait_for_ajax 10 
+      dc_check_current :current 
+    end
+    
+    it "allows to forward item through states" do
+      states = ['Candidate', 'Recorded', 'Qualified', 'Standard', 'Superseded']
+
+      new_cl_and_dc
+
+      states.each_with_index do |state, i|
+        click_on 'Forward to Next'
+        ui_confirmation_dialog true if state.eql?('Superseded')
+        wait_for_ajax 10 
+        expect(page).to have_content("Changed Status to #{ state }")
+        dc_check_statuses(state, states[i+1])
+      end 
+
+      # Check Superseded disables UI
+      expect(page).not_to have_selector ('#details-wrap')
+      expect(page).not_to have_selector ('#state-next')
+      expect( find('#next-status')[:class] ).to include ('disabled')
+    end
+
+    it "allows to add notes when updating item state" do
+      new_cl_and_dc
+      
+      fill_in 'Administrative note', with: 'Test Admin Note ææ'
+      fill_in 'Unresolved issue', with: 'Issue'
+
+      # Validation
+      click_on 'Forward to Next'
+      wait_for_ajax 10
+      expect(page).to have_content('Administrative note contains invalid characters')
+      fill_in 'Administrative note', with: 'Test Admin Note'
+      
+      dc_forward_to 'Candidate'
+      dc_check_statuses('Candidate', 'Recorded')
+    end
+    
+    it "allows to fast forward item state to Release and rewind to Draft"
+
+    it "allows to fast forward and rewind item with dependencies"
+
+    it "allows to rewind item back to Draft state"
+
+    it "token timers, warnings, extension and expiration" do
+
+      token_ui_check(@user_c) do
+        new_cl_and_dc
+      end 
+
+    end
+
+    it "token timer, expires edit lock, prevents changes" do
+
+      go_to_edit = proc { new_cl_and_dc }
+      do_an_edit = proc do 
+        click_on 'Forward to Next'
+        wait_for_ajax 10
+      end 
+
+      token_expired_check(go_to_edit, do_an_edit)
+ 
+    end
+
+    it "releases edit lock on page leave" do
+
+      token_clear_check do 
+        new_cl_and_dc
+      end
+
+    end
+    
+    # Helpers 
+
+    def new_cl_and_dc
+      click_navbar_code_lists
+      wait_for_ajax 10 
+      ui_new_code_list
+      context_menu_element_v2('history', 'Incomplete', :document_control)
+      wait_for_ajax 10
+    end
+
+    def dc_check_version(version)
+      expect( find('#version') ).to have_content(version)
+      expect( find('.semantic-version') ).to have_content(version) 
+    end
+
+    def dc_check_version_label(version_label)
+      expect( find('#version-label') ).to have_content(version_label) 
+      expect( find('#imh_header .version-label') ).to have_content(version_label) unless version_label.eql?('None')
+    end
+
+    def dc_check_statuses(current_status, next_status = nil)
+      expect( find('#status .status') ).to have_content(current_status) 
+      expect( find('#imh_header .state') ).to have_content(current_status)
+      expect( find('#status-next .status') ).to have_content(next_status) unless next_status.nil? 
+    end
+
+    def dc_check_current(type)
+      current = find('#current')
+
+      case type 
+      when :is_current 
+        expect( current ).to have_selector('.icon-sel-filled')
+      when :not_standard
+        expect( current ).to have_content('Item status is not Standard')
+      when :can_be_current  
+        expect( current ).to have_button('Make Current')
+      end 
+    end
+
+    def get_current_state
+      find('#status .status').text
+    end 
+
+    def dc_forward_to(state)
+      while not get_current_state.eql?(state) do 
+        click_on 'Forward to Next'
+        wait_for_ajax 10 
+        expect(page).to have_content('Changed Status to')
+      end
+    end
 
   end
 
