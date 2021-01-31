0cc10c199e2e134502a10637282785a8bda13d8b
Dave Iberson-Hurst <dave.iberson-hurst@assero.co.uk>
2021-01-31 17:30:56 +0100

    GLAN-1476
    [1] Update controller tests

--- spec/controllers/forms/items/questions_controller_spec.rb
+++ spec/controllers/forms/items/questions_controller_spec.rb
@@ -7,12 +7,10 @@ describe Forms::Items::QuestionsController do
   include UserAccountHelpers
   include IsoHelpers
   include ControllerHelpers
+  include IsoManagedHelpers
 
   def make_standard(item)
-    params = {}
-    params[:registration_status] = "Standard"
-    params[:previous_state] = "Incomplete"
-    item.update_status(params)
+    IsoManagedHelpers.make_item_standard(item)
   end
   
   describe "Update" do
