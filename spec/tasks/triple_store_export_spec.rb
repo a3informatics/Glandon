efed32bdb5dbbf0c16e2497b9278b70c06be6805
Dave Iberson-Hurst <dave.iberson-hurst@assero.co.uk>
2021-01-31 13:23:29 +0100

    GLAN-1483
    [1] Fix tests

--- spec/tasks/triple_store_export_spec.rb
+++ spec/tasks/triple_store_export_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require 'rake'
 
 describe 'triple store export' do
+
+  include SdtmSponsorDomainFactory
   
   before :all do
     Rake.application.rake_require "tasks/triple_store_export"
@@ -12,38 +14,44 @@ describe 'triple store export' do
     return "tasks/triple_store/exort"
   end
 
-  def run_and_capture_output(*params)  
+  def run_and_capture_output  
     stdout = StringIO.new
     $stdout = stdout
-    Rake::Task["triple_store:export"].invoke(params)
+    Rake::Task["triple_store:export"].invoke
     $stdout = STDOUT
     Rake::Task["triple_store:export"].reenable
     return stdout.string
   end
 
-  describe 'basic tests' do
-    
-    before :each do
-      load_files(schema_files, [])
-    end
-
-    it "no params" do
-      expect{run_and_capture_output("")}.to raise_error(SystemExit, /A single URI should be supplied/)
-    end
-
-    it "multiple params" do
-      expect{run_and_capture_output("xxx", "yyy")}.to raise_error(SystemExit, /Only a single parameter (a URI) should be supplied/)
-    end
-
-    it "invalid URI" do
-      expect{run_and_capture_output("xxx")}.to raise_error(SystemExit, /URI does not look valid: xxx/)
-    end
-
-    it "no errors" do
-      result = run_and_capture_output("http://www.example.com#exit").split("\n")
-      expect(result).to eq(["Triple count: 1696"])
-    end
-
+  before :each do
+    load_files(schema_files, [])
+    load_data_file_into_triple_store("mdr_identification.ttl")
   end
 
+  after :each do
+    Rake::Task["triple_store:export"].reenable
+  end
+
+  it "no params" do
+    ARGV.replace ['command']
+    expect{run_and_capture_output}.to raise_error(SystemExit, /A single URI should be supplied/)
+  end
+
+  it "invalid URI" do
+    ARGV.replace ['command', 'wwww']
+    expect{run_and_capture_output}.to raise_error(SystemExit, "URI does not look valid: wwww")
+  end
+
+  it "no errors" do
+    item = create_sdtm_sponsor_domain("AAA", "SDTM Sponsor Domain", "AA")
+    ARGV.replace ['command', "#{item.uri}"]
+    result = run_and_capture_output.split("\n")
+    expect(result[0]).to start_with("Exported http://www.s-cubed.dk/AAA/V1#SPD to /Users/daveih/Documents/rails/Glandon/public/test/SPARQL")
+  end
+
+  it "multiple params" do
+    ARGV.replace ['command', 'xxx', 'yyy']
+    expect{run_and_capture_output}.to raise_error(SystemExit, "Only a single parameter (a URI) should be supplied")
+  end
+  
 end
