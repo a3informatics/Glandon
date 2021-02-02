121c195842907e9b1cab6929cb32a0b357a4496f
Samuel Banas <sab@s-cubed.dk>
2021-02-02 15:43:52 +0100

    GLAN-1482
    [1] Added download buttons to MC show panel

--- app/javascript/packs/managed_collections/show.js
+++ app/javascript/packs/managed_collections/show.js
@@ -1,9 +1,14 @@
 import ManagedItemsPanel from 'shared/custom/iso_managed/managed_items_panel'
+import { csvExportBtn, excelExportBtn } from 'shared/helpers/dt/utils'
 
 $(document).ready( () => {
 
   let mip = new ManagedItemsPanel({
-    param: "managed_collection"
+    param: 'managed_collection',
+    buttons: [ 
+      csvExportBtn(), 
+      excelExportBtn() 
+    ]
   });
 
 });
