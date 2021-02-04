a2cd7d94764e5305c4ed63078e4cc732727a7468
Samuel Banas <sab@s-cubed.dk>
2021-02-04 09:41:29 +0100

    GLAN-1482
    [1] bugfix
    [2] Added Managed Collections to type path and breadcrumb by type helpers

--- app/models/concerns/type_path_management.rb
+++ app/models/concerns/type_path_management.rb
@@ -32,7 +32,8 @@ module TypePathManagement
       SdtmClass.rdf_type.to_s => { path: Rails.application.routes.url_helpers.history_sdtm_classes_path, strong: "sdtm_class" },
       AdamIg.rdf_type.to_s => { path: Rails.application.routes.url_helpers.history_adam_igs_path, strong: "adam_ig" },
       AdamIgDataset.rdf_type.to_s => { path: Rails.application.routes.url_helpers.history_adam_ig_datasets_path, strong: "adam_ig_dataset" },
-      SdtmSponsorDomain.rdf_type.to_s => { path: Rails.application.routes.url_helpers.history_sdtm_sponsor_domains_path, strong: "sdtm_sponsor_domain" }
+      SdtmSponsorDomain.rdf_type.to_s => { path: Rails.application.routes.url_helpers.history_sdtm_sponsor_domains_path, strong: "sdtm_sponsor_domain" },
+      ManagedCollection.rdf_type.to_s => { path: Rails.application.routes.url_helpers.history_managed_collections_path, strong: "managed_collection" }
     }
 
   # Method returns class for a given rdf type
