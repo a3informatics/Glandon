0e0167c17cf0eb1710506a0c8c3e26b25c90b0bc
Samuel Banas <sab@s-cubed.dk>
2021-02-10 14:05:37 +0100

    GLAN-1506
    [1] Upgrade handler partial + JS drafts

---
+++ app/javascript/shared/custom/thesauri/managed_concepts/upgrade/upgrade_handler.js
@@ -0,0 +1,65 @@
+import { $post } from 'shared/helpers/ajax'
+
+/**
+ * Upgrade Handler
+ * @description Allows to upgrade an item (Subset / Extension)
+ * @requires _upgrade_button Partial file to be included in the page 
+ * @author Samuel Banas <sab@s-cubed.dk>
+ */
+export default class UpgradeHandler {
+
+  /**
+   * Create an Upgrade Handler
+   */
+  constructor() {
+
+    const $button = $('#upgrade-button')
+
+    // Upgrade not possible when button not rendered
+    if ( !$button.length )
+      return
+
+    Object.assign( this, {
+      $button, 
+      url: upgradeItemUrl
+    })
+    
+    this._setListeners()
+
+  }
+
+  /**
+   * Make upgrade request to the server, refresh page on success
+   */
+  upgrade() {
+
+    this._loading( true )
+
+    $post({
+      url: this.url,
+      done: () => location.reload(),
+      always: () => this._loading( false )
+    })
+
+  }
+
+
+  /*** Private ***/
+
+
+  /**
+   * The event listeners 
+   */
+  _setListeners() {
+    this.$button.click( () => this.upgrade() )
+  }
+
+  /**
+   * Toggle loading state on the Upgrade button
+   * @param {boolean} enable Target loading state
+   */
+  _loading(enable) {
+    this.$button.toggleClass( 'el-loading', enable )
+  }
+
+}
