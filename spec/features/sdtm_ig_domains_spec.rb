096f799fc565dda38e75617a18b6a84fd5160b3f
Samuel Banas <sab@s-cubed.dk>
2021-02-18 09:38:17 +0100

    GLAN-1514
    [1] Removed old SDTM IG Domain spec Export tests as JSON and TTL export no longer available.
    [2] Added a CSV export feature test instead

--- spec/features/sdtm_ig_domains_spec.rb
+++ spec/features/sdtm_ig_domains_spec.rb
@@ -8,6 +8,10 @@ describe "SDTM IG Domains", :type => :feature do
   include UiHelpers
   include WaitForAjaxHelper
 
+  def sub_dir 
+    "features/sdtm_ig_domains"
+  end 
+
   describe "Basic Operations, curator", :type => :feature, js:true do
 
     before :all do
@@ -45,7 +49,7 @@ describe "SDTM IG Domains", :type => :feature do
       ua_logoff
     end
 
-    it "allows access to index page (REQ-MDR-MIT-015)", js:true do
+    it "allows access to index page (REQ-MDR-MIT-015)" do
       click_navbar_ig_domain
       wait_for_ajax 20
       expect(page).to have_content 'Index: SDTM IG Domains'
@@ -55,7 +59,7 @@ describe "SDTM IG Domains", :type => :feature do
       ui_check_table_cell("index", 1, 3, "Adverse Events")
     end
 
-    it "allows the history page to be viewed", js:true do
+    it "allows the history page to be viewed" do
       click_navbar_ig_domain
       wait_for_ajax 10
       ui_table_search('index', 'SDTM IG AE')
@@ -67,7 +71,7 @@ describe "SDTM IG Domains", :type => :feature do
       ui_check_table_cell("history", 1, 7, "Standard")
     end
 
-    it "history allows the show page to be viewed (REQ-MDR-BC-010)", js:true do
+    it "history allows the show page to be viewed (REQ-MDR-BC-010)" do
       click_navbar_ig_domain
       wait_for_ajax 10
       ui_table_search('index', 'SDTM IG AE')
@@ -84,6 +88,23 @@ describe "SDTM IG Domains", :type => :feature do
       ui_check_table_cell("show", 5, 2, "AEGRPID")
       ui_check_table_cell("show", 5, 3, "Group ID")
     end
+
+    it "show page allows to download SDTM IG Domain as CSV" do
+      click_navbar_ig_domain
+      wait_for_ajax 10
+      ui_table_search('index', 'SDTM IG AE')
+      find(:xpath, "//tr[contains(.,'SDTM IG AE')]/td/a", :text => 'History').click
+      wait_for_ajax 10
+      expect(page).to have_content 'Version History of \'SDTM IG AE\''
+      context_menu_element('history', 1, '3.1.3', :show)
+      wait_for_ajax 10
+      click_on 'CSV'
+
+      file = download_content
+    # write_text_file_2(file, sub_dir, "sdtm_ig_domain_csv_expected.csv")
+      expected = read_text_file_2(sub_dir, "sdtm_ig_domain_csv_expected.csv")
+    end
+
 
   end
 
