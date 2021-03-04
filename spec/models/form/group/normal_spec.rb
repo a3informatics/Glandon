cca7769ddd38f2e74b9f8250b15a4e207e649c8e
Clarisa <car@s-cubed.dk>
2021-03-04 12:57:12 +0100

    GLAN-1522
    [1] Improve Delete test

--- spec/models/form/group/normal_spec.rb
+++ spec/models/form/group/normal_spec.rb
@@ -369,7 +369,7 @@ describe Form::Group::Normal do
   describe "Delete" do
     
     before :all do
-      data_files = ["forms/FN000150.ttl", "forms/CRF TEST 1.ttl", "biomedical_concept_instances.ttl", "biomedical_concept_templates.ttl"]
+      data_files = ["forms/FN000150.ttl"]
       load_files(schema_files, data_files)
       load_cdisc_term_versions(1..15)
       load_data_file_into_triple_store("mdr_identification.ttl")
