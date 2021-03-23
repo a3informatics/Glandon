f9cfae59282490e979a283912e9f982a9d1de90a
Dave Iberson-Hurst <dih@s-cubed.dk>
2021-03-23 07:43:01 +0100

    R4.0.0
    [1] Fix file write

--- spec/models/biomedical_concept/data_spec.rb
+++ spec/models/biomedical_concept/data_spec.rb
@@ -168,7 +168,7 @@ describe BiomedicalConcept do
     end
 
     it "create templates" do
-      write_file = true
+      write_file = false
       results = []
       filenames = local_file_list("#{sub_dir}/templates", "*.yaml")
       filenames.each do |f|
