a4b17cfa8583c867a65bc72087cf3b3b76040b09
Clarisa <car@s-cubed.dk>
2021-03-29 10:40:32 +0200

    GLAN-1532
    [1] Add endpoints.ttl

--- spec/models/endpoint/data_spec.rb
+++ spec/models/endpoint/data_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
-describe Objective do
+describe Endpoint do
 
   include DataHelpers
   include PublicFileHelpers
   include SparqlHelpers
   include IsoHelpers
-  include ValidationHelpers
 
   def sub_dir
-    return "models/objective/data"
+    return "models/endpoint/data"
   end
 
-  describe "Create Objective" do
+  describe "Create Endpoint" do
     
     before :all do
-      load_files(schema_files, [])
+      data_files = ["parameter.ttl"]
+      load_files(schema_files, data_files)
       load_data_file_into_triple_store("mdr_identification.ttl")     
     end
 
@@ -31,22 +31,79 @@ describe Objective do
       item.to_ttl
     end
 
-    it "create Objective" do
-      objective = Objective.create(identifier: "OBJ1", label: "Objective 1")
-      objective = Objective.find_minimum(objective.uri)
-      iso_concept = Intervention.new()
-      parameter1 = Parameter.create(label: "Intervention", parameter_rdf_type: iso_concept.rdf_type )
-      objective.has_parameter = [parameter1]
-      enumerated = Enumerated.create(label: "Primary")
-      objective.objective_type = enumerated
-      endpoint1 = Endpoint.create(label: "Endpoint1", full_text: "")
-      objective.is_assessed_by = [endpoint1]
-      objective.full_text = "To show the contribution of [[[Intervention]]] to the clinical and parasiticidal effect of <interventionA/interventionB> 
-      combination by analyzing exposure-response of <interventionA> measured by <Timepoint> <Param>for the effect and the area under the concentration time curve up to infinity (AUC) of <interventionA> as PK predictor"
-      objective.save
-      full_path = item_to_ttl(objective)
-      full_path = objective.to_ttl
-  copy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "Objective.ttl")
+  #   it "create Endpoint" do
+  #     endpoint = Endpoint.create(identifier: "END1", label: "Endpoint 1", full_text: "[[[Timepoint]]][[[Param]]]")
+  #     endpoint = Endpoint.find_minimum(endpoint.uri)
+  #     parameter1 = Parameter.
+  #     parameter2 = Parameter.
+  #     endpoint.has_parameter = [parameter1, parameter2]
+  #     endpoint.save
+  #     full_path = item_to_ttl(endpoint)
+  #     full_path = endpoint.to_ttl
+  # copy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "endpoint.ttl")
+  #   end
+
+    it "End Points" do
+      parameter1 = Parameter.where(label: "BC").first
+      parameter2 = Parameter.where(label: "Intervention").first
+      parameter3 = Parameter.where(label: "Timepoint").first
+      parameter4 = Parameter.where(label: "Assessment").first
+  
+      endpoints =
+      [
+        {
+          label: "Endpoint 1",
+          full_text: "[[[Timepoint]]][[[AnalysisConcept]]]",
+          has_parameter: [parameter3]
+        },
+        {
+          label: "Endpoint 2",
+          full_text: "[[[Timepoint]]][[[AnalysisConcept]]]",
+          has_parameter: [parameter3]
+        },
+        {
+          label: "Endpoint 3",
+          full_text: "[[[Timepoint]]][[[AnalysisConcept]]]",
+          has_parameter: [parameter3]
+        },
+        {
+          label: "Endpoint 4",
+          full_text: "[[[BC]]] at baseline then every [[[Timepoint-freq]]] during the first [[[Timepoint-span]]] then at [[[Timepoint]]] and every [[[Timepoint-freq]]] until [[[Timepoint]]]",
+          has_parameter: [parameter1, parameter3]
+        }
+        #,
+        # {
+        #   label: "Endpoint 5",
+        #   full_text: "Clinical laboratory tests (including [[[Assessment]]]), [[[Assessment]]], and [[[Assessment]]]"
+        # },
+        # {
+        #   label: "Endpoint 6",
+        #   full_text: "The change from baseline to [[[Timepoint]]] in continuous laboratory tests: Hepatic Function Panel"
+        # },
+        # {
+        #   label: "Endpoint 7",
+        #   full_text: "The proportion of participants with abnormal (high or low) laboratory measures (urinalysis) during the postrandomization phase"
+        # },
+        # {
+        #   label: "Endpoint 8",
+        #   full_text: "The change from baseline to [[[Timepoint]]] in ECG parameter: QTcF"
+        # },
+        # {
+        #   label: "Endpoint 9",
+        #   full_text: "The change from baseline to [[[Timepoint]]] in the [[[Assessment]]]"
+        # }
+      ]
+      items = []
+      endpoints.each_with_index do |ep, index|
+        item = Endpoint.new(ep)
+        item.set_initial("EP #{index+1}")
+        items << item
+      end
+      sparql = Sparql::Update.new
+      sparql.default_namespace(items.first.uri.namespace)
+      items.each {|x| x.to_sparql(sparql, true)}
+      full_path = sparql.to_file
+    #Xcopy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "endpoints.ttl")
     end
   
   end
