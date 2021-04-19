2919737b2a13a61457facc68e5678b86f1cc312e
Samuel Banas <sab@s-cubed.dk>
2021-04-19 15:38:40 +0200

    GLAN-1533
    [1] Study timeline custom styles
    [2] Study Timeline module - added Arm label

--- app/javascript/shared/custom/studies/build/study_timeline.js
+++ app/javascript/shared/custom/studies/build/study_timeline.js
@@ -8,15 +8,26 @@ import Timeline from 'shared/base/d3/timeline/timeline'
  */
 export default class StudyTimeline extends Timeline {
 
+  /**
+   * Create a StudyTimeline instance
+   * @param {Object} params Instance parameters
+   * @param {string} params.selector Unique selector of the timeline wrapper
+   * @param {string} params.dataUrl Url to fetch the timeline data from
+   * @param {string} params.armLabel Label of the Arm instance which the Timeline displays
+   * @param {function} params.onDataLoaded Data load completed callback, receives raw data as first argument, optional
+   */
   constructor({
     selector,
     dataUrl,
+    armLabel = 'High Dose',
     onDataLoaded = () => {}
   } = {}) {
 
     super({
       selector, dataUrl, onDataLoaded,
-      deferLoading: true
+      deferLoading: true // Do not load data initially 
+    }, {
+      armLabel
     })
 
   }
@@ -26,7 +37,7 @@ export default class StudyTimeline extends Timeline {
 
 
   /**
-   * Create instance graph object, set listeners, load data
+   * Initializes instance deps
    */
   _init() {
 
@@ -34,7 +45,37 @@ export default class StudyTimeline extends Timeline {
     this.render() // Only to display timeline for dev purposes, normally will be rendered after data fetched / attached 
     
   }
-  
+
+  /**
+   * Initialize & render Timeline and additional elements 
+   */
+  _renderTimeline() {
+
+    super._renderTimeline()
+
+    // Render Arm label 
+    this._xAxis.append( 'text' )
+               .attr( 'class', 'arm-label' )
+               .attr( 'dy', -20 )
+               .text( this.armLabel )
+
+  }
+
+  /**
+   * Graph properties definitions
+   * @return {Object} Graph properties for svg, zoom, baseline date
+   */
+   get _props() {
+
+    const props = super._props
+
+    // Offset the left side due to Arm label being rendered
+    props.svg.margin.left = 40
+
+    return props
+
+  }
+
 
 }
   
