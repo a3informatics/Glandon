f011fd9cfa5a6e77d7c1734474b3267c91d8f32f
risacla <car@s-cubed.dk>
2021-04-22 12:31:36 +0200

    GLAN-1537
    [1] Fix test

--- spec/models/reports/cdisc_changes_report_spec.rb
+++ spec/models/reports/cdisc_changes_report_spec.rb
@@ -22,16 +22,16 @@ describe Reports::CdiscChangesReport do
   it "creates the report" do
     results = read_yaml_file(sub_dir, "input_1.yaml")
     report = Reports::CdiscChangesReport.new
-    pdf = report.create(results, @user)
+    pdf = report.create(results, @user, "http://localhost:3000")
     html = report.html
   #Xwrite_text_file_2(html, sub_dir, "report_1.txt")
     expected = read_text_file_2(sub_dir, "report_1.txt")
     run_at_1 = extract_run_at(expected)
     run_at_2 = extract_run_at(html)
     html.sub!(run_at_2, run_at_1) # Need to fix the run at date and time for the comparison
-    path_to_proj_1 = extract_path(expected)
-    path_to_proj_2 = Rails.root.to_s
-    expected.sub!(path_to_proj_1, path_to_proj_2)
+    #path_to_proj_1 = extract_path(expected)
+    #path_to_proj_2 = Rails.root.to_s
+    #expected.sub!(path_to_proj_1, path_to_proj_2)
     expect(html).to eq(expected)
   end
 
