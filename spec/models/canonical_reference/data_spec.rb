529e18a6cdb9672a5c732fdfaaa0d932e3a9f506
risacla <car@s-cubed.dk>
2021-04-23 10:14:02 +0200

    GLAN-1540
    [1] Add canonical references migration 2

--- spec/models/canonical_reference/data_spec.rb
+++ spec/models/canonical_reference/data_spec.rb
@@ -63,6 +63,21 @@ describe CanonicalReference do
     #Xcopy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "canonical_references_migration_1.ttl")
     end
 
+    it "migrations 2" do
+      results = []
+      refs = read_yaml_file(sub_dir, "canonical_references_migration_2.yaml")
+      refs.each do |ref|
+        item = CanonicalReference.new(label: ref[:label], sdtm: ref[:sdtm], bridg: ref[:bridg], definition: "Not currently defined.")
+        item.uri = item.create_uri(item.class.base_uri)
+        results << item
+      end
+      sparql = Sparql::Update.new
+      sparql.default_namespace(results.first.uri.namespace)
+      results.each{|x| x.to_sparql(sparql, true)}
+      full_path = sparql.to_file
+    #Xcopy_file_from_public_files_rename("test", File.basename(full_path), sub_dir, "canonical_references_migration_2.ttl")
+    end
+
   end
 
 end
