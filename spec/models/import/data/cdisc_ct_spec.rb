1786f673cbd14fe099601427cb77b439c642e6fb
Dave IH <dih@s-cubed.dk>
2021-09-27 09:03:21 +0200

    GLAN-1546
    [1] Initial test updates

--- spec/models/import/data/cdisc_ct_spec.rb
+++ spec/models/import/data/cdisc_ct_spec.rb
@@ -36,7 +36,7 @@ describe "Import::CdiscTerm CT Data" do
   # ---------- IMPORTANT SWITCHES ----------
   
   def set_write_file
-    false
+    true
   end
 
   def use_api
@@ -1976,6 +1976,42 @@ SELECT DISTINCT ?s ?p ?o WHERE {
 
     it "Create 2021-06-25", :import_data => 'slow' do
       release_date = "2021-06-25"
+      results = execute_import(release_date, {sdtm: release_date, cdash: release_date, adam: "2020-11-06", send: release_date, protocol: release_date, define: release_date}, set_write_file, use_api)
+      expected = [
+        {cl: :C65047,  status: :updated},       # LBTESTCD
+        {cl: :C66741,  status: :no_change},     # VSTESTCD
+        {cl: :C67153,  status: :no_change},     # VSTEST
+        {cl: :C66737,  status: :no_change},     # TPHASE
+        {cl: :C66738,  status: :no_change},     # TSPARMCD
+        {cl: :C66785,  status: :no_change},     # TCNTRL
+        {cl: :C66790,  status: :no_change},     # ETHNIC
+        {cl: :C67152,  status: :no_change},     # TSPARM
+        {cl: :C67153,  status: :no_change},     # VSTEST
+        {cl: :C67154,  status: :updated},       # LBTEST
+        {cl: :C71153,  status: :no_change},     # EGTESTCD
+        {cl: :C71620,  status: :no_change},     # UNIT
+        {cl: :C74456,  status: :updated},       # LOC
+        {cl: :C74559,  status: :updated},       # SCTESTCD
+        {cl: :C76351,  status: :no_change},     # SKINCLAS
+        {cl: :C78431,  status: :no_change},     # VSPOS
+        {cl: :C78735,  status: :no_change},     # EVAL
+        {cl: :C85494,  status: :updated},       # PKUNIT
+        {cl: :C99079,  status: :no_change},     # EPOCH
+        {cl: :C118971, status: :updated},       # CCCAT
+        {cl: :C128689, status: :updated},       # RACEC
+        {cl: :C103330, status: :updated},       # SCTEST
+        {cl: :C147069, status: :no_change},     # Randomization Type Value Set
+        {cl: :C160930, status: :no_change},     # CHAGNAMR
+        {cl: :C163026, status: :no_change},     # Study Monitoring Attribute Terminology
+        {cl: :C163028, status: :updated}        # D1FATS
+      ]
+      check_cl_results(results, expected) 
+      check_count(release_date)
+      check_tags(release_date)
+    end
+
+    it "Create 2021-09-24", :import_data => 'slow' do
+      release_date = "2021-09-24"
       results = execute_import(release_date, {sdtm: release_date, cdash: release_date, adam: "2020-11-06", send: release_date, protocol: release_date, define: release_date}, set_write_file, use_api)
       expected = [
         {cl: :C65047,  status: :updated},       # LBTESTCD
